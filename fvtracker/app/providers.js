cef30ff725db157cfade40d195ccfc2f5b63407b
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-20 20:05:21 +0100

    useproviders

--- fvtracker/app/providers.js
+++ fvtracker/app/providers.js
@@ -2,15 +2,16 @@
 
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { Provider as ReduxProvider } from "react-redux";
+import { Provider as ReduxProvider, useSelector } from "react-redux";
 import store from "@/store";
+import UserProviders from "@/lib/providers/user";
 
 export default function Providers({ children }) {
   return (
     <SessionProvider>
       <ReduxProvider store={store}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
+          <UserProviders>{children}</UserProviders>
         </ThemeProvider>
       </ReduxProvider>
     </SessionProvider>
---
+++ fvtracker/lib/providers/user.js
@@ -0,0 +1,36 @@
+"use client";
+import { logIn, logOut } from "@/store/userSlice";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function UserProviders({ children }) {
+  return <AppUserProvider>{children}</AppUserProvider>;
+}
+
+function AppUserProvider({ children }) {
+  const appUser = useSelector((state) => state.user?.appUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!appUser) setAppUserFromApiRedux(dispatch);
+  }, []);
+
+  return children;
+}
+
+const setAppUserFromApiRedux = async (dispatch) => {
+  try {
+    const response = await axios.get("/api/auth/redux/user");
+    console.log("Fetched app user from API response:", response.data);
+
+    const appUser = response.data.appUser;
+    if (!appUser) {
+      dispatch(logOut());
+      return;
+    }
+    dispatch(logIn({ appUser }));
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error fetching app user from API");
+  }
+};

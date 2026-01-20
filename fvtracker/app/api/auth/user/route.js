ec034401802e63bcf75dfb7195cfc859f156d793
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-20 20:10:57 +0100

    move next auth handlers to different path

--- fvtracker/app/api/auth/user/route.js
+++ fvtracker/app/api/auth/user/route.js
@@ -1,4 +1,5 @@
 export function GET(req) {
+  console.log("User route GET called");
   return new Response(JSON.stringify({ message: "User route GET" }), {
     status: 200,
     headers: { "Content-Type": "application/json" },

6c7a7de942cd0e08097d0331ee82489c263d5f04
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-21 20:47:35 +0100

    general manager export model

--- fvtracker/models/user/roles/GeneralManager.js
+++ fvtracker/models/user/roles/GeneralManager.js
@@ -8,3 +8,11 @@ const generalManagerSchema = {
   },
   managers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Manager" }],
 };
+
+
+export const GeneralManager =
+  mongoose.models.GeneralManager ||
+  mongoose.model(
+    "GeneralManager",
+    new mongoose.Schema(generalManagerSchema)
+  );

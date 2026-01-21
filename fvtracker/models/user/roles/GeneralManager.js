5b16f0df67568d3cb379adbd1b441aa46f0d8703
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-21 21:00:25 +0100

    gen manager without manager

--- fvtracker/models/user/roles/GeneralManager.js
+++ fvtracker/models/user/roles/GeneralManager.js
@@ -1,11 +1,6 @@
 import mongoose from "mongoose";
 
 const generalManagerSchema = {
-  manager: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Manager",
-    required: true,
-  },
   managers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Manager" }],
 };
 

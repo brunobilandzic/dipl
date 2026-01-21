99d99c6086dcd2da1dba9bf6874a667bbd0f695f
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-21 21:03:13 +0100

    add app user to gen manager

--- fvtracker/models/user/roles/GeneralManager.js
+++ fvtracker/models/user/roles/GeneralManager.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const generalManagerSchema = {
+  appUser: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "AppUser",
+    required: true,
+  },
   managers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Manager" }],
 };
 
 export const GeneralManager =
   mongoose.models.GeneralManager ||
-  mongoose.model(
-    "GeneralManager",
-    new mongoose.Schema(generalManagerSchema)
-  );
+  mongoose.model("GeneralManager", new mongoose.Schema(generalManagerSchema));

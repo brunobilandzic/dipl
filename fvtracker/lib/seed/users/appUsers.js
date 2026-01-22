281f8d5e4e8f6b18f18c7382c3f2cfd01cdecefe
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-22 17:43:53 +0100

    delete all app users and roles before seed

--- fvtracker/lib/seed/users/appUsers.js
+++ fvtracker/lib/seed/users/appUsers.js
@@ -2,9 +2,32 @@
 import { SEED_ERROR } from "@/lib/constants/errors/db/seed";
 import appUsersJsonArray from "@/lib/seed/data/appUsers";
 import { AppUser } from "@/models/user/AppUser";
+import dbConnect from "@/lib/db/mongooseConnect";
+import { Admin } from "@/models/user/roles/Admin";
+import { GeneralManager } from "@/models/user/roles/GeneralManager";
+import { CultivationManager } from "@/models/user/roles/CultivationManager";
+import { ProductionManager } from "@/models/user/roles/ProductionManager";
+import { StorageManager } from "@/models/user/roles/StorageManager";
+import { FinancialManager } from "@/models/user/roles/FinancialManager";
 
-export default async function seedAppUsers() {
+const check = async () => {
+  await dbConnect();
+  const userCount = await AppUser.countDocuments();
+  if (userCount > 0) {
+    await AppUser.deleteMany({});
+    await Admin.deleteMany({});
+    await GeneralManager.deleteMany({});
+    await CultivationManager.deleteMany({});
+    await ProductionManager.deleteMany({});
+    await StorageManager.deleteMany({});
+    await FinancialManager.deleteMany({});
+    console.log("Deleted all users");
+  }
+};
+
+export const seed = async () => {
   console.log("Seeding appUsers...");
+  await check();
 
   if (!appUsersJsonArray) {
     console.log("No appUsers to seed.");
@@ -13,7 +36,7 @@ export default async function seedAppUsers() {
 
   const promiese = [];
   for (const appUserData of appUsersJsonArray) {
-    promiese.push(createAppUser(appUserData));
+    promiese.push(create(appUserData));
   }
   const appUsers = await Promise.all(promiese);
 
@@ -23,9 +46,9 @@ export default async function seedAppUsers() {
   }
 
   return appUsers;
-}
+};
 
-const createAppUser = async (appUserData) => {
+export const create = async (appUserData) => {
   const appUser = new AppUser(appUserData);
   await appUser.save();
   return appUser;

e34c77d5c8e71e75cb1a4930b0562c66a4fa4975
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-23 19:16:54 +0100

    different manager constants structure

--- fvtracker/lib/constants/users/usernames.js
+++
@@ -1,8 +0,0 @@
-export default {
-  ADMIN: "admin.admin",
-  GENERAL_MANAGER: "general.manager",
-  CULTIVATION_MANAGER: "cultivation.manager",
-  FINANCIAL_MANAGER: "financial.manager",
-  PRODUCTION_MANAGER: "production.manager",
-  STORAGE_MANAGER: "storage.manager",
-};

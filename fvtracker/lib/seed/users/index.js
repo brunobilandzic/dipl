b9da9077d71827694000dd035ebbc5a684d4f8b4
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-23 19:28:50 +0100

    create manager, root then specific

--- fvtracker/lib/seed/users/index.js
+++ fvtracker/lib/seed/users/index.js
@@ -1,26 +1,5 @@
-import { seed as seedAppUsers, create as createAppUser } from "./appUsers";
-import { create as createAdmin } from "./admin";
+import seedAppUsers from "./appUsers";
 
 export default {
-  allUsers: {
-    seed: async () => {
-      const appUsers = await seedAppUsers();
-      const adminUser = await createAdmin();
-      if (!adminUser) {
-        throw new Error("Admin user creation failed");
-      }
-      if (!appUsers || appUsers.length === 0) {
-        throw new Error("App users seeding failed");
-      }
-      
-      return { appUsers, adminUserId: adminUser.id };
-    },
-  },
-  appUsers: {
-    seed: seedAppUsers,
-    create: createAppUser,
-  },
-  admin: {
-    create: createAdmin,
-  },
+  all: seedAppUsers,
 };

17e4ccf2553fd78542f7c2bf435290b326b2d37c
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-25 10:06:55 +0100

    index data, folder structure

---
+++ fvtracker/models/sectors/cultivation/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { CultivationArea, Cultivation } from "./Cultivation";
+import { Field, FieldGridCell } from "./Field";
+import { CropGeneralType, CropType, CropVariety } from "./Crops";
+import { SoilType } from "./Soil";
+
+export default {
+  CultivationArea,
+  Cultivation,
+  Field,
+  FieldGridCell,
+  CropGeneralType,
+  CropType,
+  CropVariety,
+  SoilType,
+};

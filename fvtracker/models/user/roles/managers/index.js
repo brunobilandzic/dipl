03d8f989d6f8e19654b9a1ba65d5623c2ee6007d
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-25 10:18:37 +0100

    index managers

---
+++ fvtracker/models/user/roles/managers/index.js
@@ -0,0 +1,17 @@
+import { Manager } from "./Manager";
+import { Admin } from "@/models/user/Admin";
+import { CultivationManager } from "./CultivationManager";
+import { WarehouseManager } from "./WarehouseManager";
+import { GeneralManager } from "./GeneralManager";
+import { FinancialManager } from "./FinancialManager";
+import { ProductionManager } from "./ProductionManager";
+
+export default {
+  Manager,
+  Admin,
+  CultivationManager,
+  WarehouseManager,
+  GeneralManager,
+  FinancialManager,
+  ProductionManager,
+};

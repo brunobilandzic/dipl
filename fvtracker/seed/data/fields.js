9df4e494d2fde28f0ffb10fb6b122c0c8449720f
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-30 21:28:37 +0100

    best optimizedParams

--- fvtracker/seed/data/fields.js
+++ fvtracker/seed/data/fields.js
@@ -28,7 +28,7 @@ export const fieldParams = [
 export const optimizedParams = {
   name: "SEEDED FIELD",
   description: "Field optimized for seeders testing",
-  width: 100,
+  width: 150,
   length: 100,
   min_ca_dim: 15,
   max_ca_dim: 50,

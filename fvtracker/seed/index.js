ada311824c0506ac098e241e27ec15730cf1dc69
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-30 16:33:51 +0100

    create field seed type

--- fvtracker/seed/index.js
+++ fvtracker/seed/index.js
@@ -9,8 +9,8 @@ async function handleAPIRequest(seedType) {
     case SEED_TYPES.USERS:
       return await import("./users").then((module) => module.default.all());
 
-    case SEED_TYPES.FIELDS:
-      return await import("./fields").then((module) => module.default.all());
+    case SEED_TYPES.FIELD:
+      return await import("./fields").then((module) => module.default.create());
 
     default:
       throw new Error(`Unknown seed type: ${seedType}`);
--- fvtracker/seed/seedTypes.js
+++ fvtracker/seed/seedTypes.js
@@ -1,5 +1,5 @@
 export default {
   USERS: "Seed Users",
   FETCH_ALL: "Fetch All",
-  FIELDS: "Seed Fields",
+  FIELD: "Create Field",
 };

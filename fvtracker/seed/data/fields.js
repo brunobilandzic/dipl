83235d75db893c64cf34be5daa309c6cef2935bd
brunobilandzic <bruno.bilandzic@outlook.com>
2026-01-30 14:44:14 +0100

    add gap to examples

--- fvtracker/seed/data/fields.js
+++ fvtracker/seed/data/fields.js
@@ -5,6 +5,7 @@ const fields = [
     length: 200,
     min_ca_dim: 5,
     max_ca_dim: 20,
+    gap: 2,
   },
   {
     name: "Medium CAS",
@@ -12,6 +13,7 @@ const fields = [
     length: 500,
     min_ca_dim: 10,
     max_ca_dim: 50,
+    gap: 4,
   },
   {
     name: "Large CAS",
@@ -19,5 +21,6 @@ const fields = [
     length: 1000,
     min_ca_dim: 20,
     max_ca_dim: 100,
+    gap: 8,
   },
 ];

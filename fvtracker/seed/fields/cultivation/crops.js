edd736433d036b79e4692da1716f91712923cc04
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-02 19:36:52 +0100

    remove crops to separate folder

--- fvtracker/seed/fields/cultivation/crops.js
+++
@@ -1,7 +0,0 @@
-import {vegatablesCropTypes, fruitsCropTypes} from "@/lib/constants/cultivation/plants";
-
-
-export async function seedCropGeneralTypes() {
-    console.log("Seeding crop general types...");
-   
-}

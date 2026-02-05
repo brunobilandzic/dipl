0f35921d07094b54c88a6137efc01a6290766624
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-05 18:51:42 +0100

    connect database when importing index from managers model folder

--- fvtracker/models/user/managers/index.js
+++ fvtracker/models/user/managers/index.js
@@ -4,6 +4,9 @@ import { CultivationManager } from "./CultivationManager";
 import { WarehouseManager } from "./WarehouseManager";
 import { FinancialManager } from "./FinancialManager";
 import { ProductionManager } from "./ProductionManager";
+import dbConnect from "@/lib/db/mongooseConnect";
+
+await dbConnect();
 
 export default {
   Manager,

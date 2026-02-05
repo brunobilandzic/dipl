3017e7575c7c4a70a51a2f58dd3c7455cb19774f
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-05 16:59:53 +0100

    db connect on get by username

--- fvtracker/lib/db/fetch/users/appUser.js
+++ fvtracker/lib/db/fetch/users/appUser.js
@@ -2,6 +2,7 @@ import dbConnect from "@/lib/db/mongooseConnect";
 import { AppUser } from "@/models/user/AppUser";
 
 export const getByUsername = async (username) => {
+  await dbConnect();
   const appUser = await AppUser.findOne({ username });
   if (!appUser) {
     throw new Error(`AppUser with username ${username} not found`);

5176486aa139e2ccf479104ae22d8be3dba9819a
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-06 14:23:06 +0100

    add padding bottom to body element

--- fvtracker/app/layout.js
+++ fvtracker/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased pb-6`}
       >
         <Providers>
           <Navbar />

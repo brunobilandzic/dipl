ff5da025cc8301d947187d97f7ddd7b3885ab33d
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-07 20:21:13 +0100

    on auth log session data not appuser from redux

--- fvtracker/app/(auth)/auth/page.js
+++ fvtracker/app/(auth)/auth/page.js
@@ -1,19 +1,22 @@
+"use client"
+
 import { LoginButton, LogoutButton } from "@/components/auth/login";
 import React from "react";
 import { SignUpButton } from "@/components/auth/signup";
-import authLib from "@/lib/auth";
+import {useSession} from "next-auth/react"
 
-async function AuthorizePage() {
-  const appUser = await authLib.session.fetchSessionAppUser();
+function AuthorizePage() {
+  const {data: session} = useSession()
+  
   return (
     <div className="h-screen flex justify-center items-center">
       <div>
-        {appUser ? (
+        {(session != undefined) ? (
           <div>
             <h2 className="text-2xl mb-4 m-x-auto">
               You are logged in as{" "}
               <div className="break-words text-wrap row">
-                {JSON.stringify(appUser, null, 2)}
+                {JSON.stringify(session, null, 2)}
               </div>
             </h2>
 

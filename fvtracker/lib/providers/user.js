5f5a4865b9c150f83298760d68306962689a8b4a
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-07 20:44:17 +0100

    ditch userproviders api calls to redux, all important data in usesession now

--- fvtracker/lib/providers/user.js
+++ fvtracker/lib/providers/user.js
@@ -1,65 +1,35 @@
 "use client";
-import { logIn, logOut, setManager } from "@/store/userSlice";
-import axios from "axios";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 
 export default function UserProviders({ children }) {
-  return (
-    <AppUserProvider>
-      <ManagerProvider>{children}</ManagerProvider>
-    </AppUserProvider>
-  );
+  return <AppUserProvider>{children}</AppUserProvider>;
 }
 
 function AppUserProvider({ children }) {
-  const appUser = useSelector((state) => state.user?.appUser);
+/*   const appUser = useSelector((state) => state.user?.appUser);
   const dispatch = useDispatch();
   useEffect(() => {
-    if (!appUser) setAppUserFromApiRedux(dispatch);
+    const setAppUser = async () => {
+      if (!appUser) await setAppUserFromApiRedux(dispatch);
+    };
+    setAppUser();
   }, []);
-
+ */
   return children;
 }
 
-function ManagerProvider({ children }) {
-  const manager = useSelector((state) => state.user?.manager);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (!manager) fetchManagerFromApiRedux(dispatch);
-  }, []);
-  return children;
-}
-
-const setAppUserFromApiRedux = async (dispatch) => {
+/* const setAppUserFromApiRedux = async (dispatch) => {
   try {
     const response = await axios.get("/api/auth/user/redux");
     console.log("Fetched app user from API response:", response.data);
 
-    const appUser = response.data.appUser;
+    const { appUser, managerData } = response.data;
     if (!appUser) {
       dispatch(logOut());
       return;
     }
-    dispatch(logIn({ appUser }));
+    dispatch(logIn({ appUser, managerData }));
   } catch (error) {
     console.error(error);
     throw new Error("Error fetching app user from API");
   }
-};
-
-const fetchManagerFromApiRedux = async (dispatch) => {
-  try {
-    const response = await axios.get("/api/auth/user/redux", {
-      params: { includeManager: true },
-    });
-    if (!response.data.managerData) {
-      console.log("No manager found in API response");
-      return;
-    }
-    dispatch(setManager(response.data.managerData));
-  } catch (error) {
-    console.error(error);
-    throw new Error("Error fetching manager from API");
-  }
-};
+}; */
--- fvtracker/store/userSlice.js
+++ fvtracker/store/userSlice.js
@@ -1,7 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  appUser: null,
 };
 
 export const userSlice = createSlice({
@@ -9,23 +8,12 @@ export const userSlice = createSlice({
   initialState: initialState,
   reducers: {
     logIn: (state, action) => {
-      state.appUser = action.payload.appUser;
     },
-    setManager: (state, action) => {
-      console.log(
-        "Setting manager in user slice with payload:",
-        action.payload,
-      );
-      const { rootManager, specificManager } = action.payload;
-      state.specificManager = specificManager;
-      state.rootManager = rootManager;
-      state.managerModelName = rootManager.managerModelName;
-    },
+
     logOut: (state) => {
-      state = initialState;
     },
   },
 });
 
-export const { logIn, setManager, logOut } = userSlice.actions;
+export const { logIn, logOut } = userSlice.actions;
 export default userSlice.reducer;

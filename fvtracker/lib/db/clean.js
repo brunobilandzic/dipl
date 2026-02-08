905b6a51cd23b6da424acf132f8723054e736be2
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-08 18:09:02 +0100

    in clean, stringify ObjectId not ObjectID

--- fvtracker/lib/db/clean.js
+++ fvtracker/lib/db/clean.js
@@ -2,7 +2,7 @@ export default (data) => {
   return JSON.parse(
     JSON.stringify(data, (key, value) => {
       if (value instanceof Date) return value.toISOString();
-      if (value?._bsontype === "ObjectID") return value.toString();
+      if (value?._bsontype === "ObjectId") return value.toString();
       if (value instanceof Buffer) return value.toString();
       return value;
     })

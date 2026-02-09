73e6f18f2d63ee5649748fc12f6c2f66f53d1516
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-09 13:49:34 +0100

    field double import quickfix

--- fvtracker/models/sectors/cultivation/index.js
+++ fvtracker/models/sectors/cultivation/index.js
@@ -3,7 +3,6 @@ import { CultivationArea, Cultivation } from "./Cultivation";
 import { Field } from "./Field";
 import { CropGeneralType, CropType, CropVariety } from "./Crops";
 import { SoilType } from "./Soil";
-import { Field } from "./Field";
 
 export default {
   CultivationArea,

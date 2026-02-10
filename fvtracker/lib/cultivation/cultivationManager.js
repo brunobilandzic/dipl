47624ef688ba67ea1e7c509f5b48673426fd86cb
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-10 17:45:25 +0100

    use cache when fetching cultivation manager

--- fvtracker/lib/cultivation/cultivationManager.js
+++ fvtracker/lib/cultivation/cultivationManager.js
@@ -1,24 +1,31 @@
 import models from "@/models";
 import { fetchSessionAppUser } from "@/lib/auth/fetchSessionData";
-
+import { unstable_cache } from "next/cache";
 
 export async function getCultivationManager() {
-  const appUser = await fetchSessionAppUser();
-  if (!appUser) {
-    throw new Error("No app user found for the session.");
-  }
+   const appUser = await fetchSessionAppUser();
+      if (!appUser) {
+        throw new Error("No app user found for the session.");
+      }
+  const cacheFunction = unstable_cache(
+    async () => {
+     
+      const cultivationManager =
+        await appUser.getSpecificManager("CultivationManager");
+      if (!cultivationManager) {
+        return <div>No cultivation manager found for this user.</div>;
+      }
+      await cultivationManager.populate({
+        path: "fields",
+        populate: {
+          path: "cultivationAreas",
+        },
+      });
 
-  const cultivationManager =
-    await appUser.getSpecificManager("CultivationManager");
-  if (!cultivationManager) {
-    return <div>No cultivation manager found for this user.</div>;
-  }
-  await cultivationManager.populate({
-    path: "fields",
-    populate: {
-      path: "cultivationAreas",
+      return cultivationManager;
     },
-  });
-  
-  return cultivationManager;
+    ["CultivationManager"],
+    { tags: ["cm"] },
+  );
+  return await cacheFunction();
 }

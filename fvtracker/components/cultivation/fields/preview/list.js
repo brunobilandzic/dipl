898afba77a49517865ccc772c74e5d56781fafb8
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-10 12:45:04 +0100

    no cas detected in field list also

--- fvtracker/components/cultivation/fields/preview/list.js
+++ fvtracker/components/cultivation/fields/preview/list.js
@@ -14,6 +14,7 @@ import { makeUrlFriendly } from "@/lib/utils/strings";
 
 export function FieldsList({ fields }) {
   if (!fields || fields.length === 0) return <div>No fields found.</div>;
+  console.log("cas", fields[0]?.cultivationAreas);
   return (
     <>
       <List>

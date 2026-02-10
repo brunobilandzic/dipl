bd1a29b8149f83bf1106cef73da5e797bb335e4c
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-10 13:37:31 +0100

    seed/fields folder structure

--- fvtracker/seed/fields/create/createFields.js
+++ fvtracker/seed/fields/create/createFields.js
@@ -6,7 +6,7 @@ import {
   cultivationAreaNamesConstant,
   optimizedParams,
   optimizedParamsArray,
-} from "../data/fields.js";
+} from "../../data/fields.js";
 import dbConnect from "@/lib/db/mongooseConnect.js";
 import { CultivationArea } from "@/models/sectors/cultivation/Cultivation.js";
 
@@ -19,70 +19,6 @@ async function deleteFieldsWithDocs() {
   console.log(
     "Deleted existing fields, cultivation areas, and field grid cells.",
   );
-}
-
-function randomPoint(field) {
-  const { width, length, min_ca_dim, max_ca_dim } = field;
-
-  const length_options = [];
-  for (let i = min_ca_dim; i <= max_ca_dim; i++) {
-    length_options.push(i);
-  }
-
-  let x = Math.floor(Math.random() * width) + 1;
-  let y = Math.floor(Math.random() * length) + 1;
-  let dim_x = length_options[Math.floor(Math.random() * length_options.length)];
-  let dim_y = length_options[Math.floor(Math.random() * length_options.length)];
-
-  return { x, y, dim_x, dim_y };
-}
-
-function notValidPoint(field, x, y, dim_x, dim_y) {
-  let { width, length, cultivationAreas, gap } = field;
-  const plantedCells =
-    cultivationAreas
-      ?.map((ca) => ca.planted)
-      ?.reduce((acc, planted) => {
-        if (planted) {
-          for (let entry of planted.entries()) {
-            acc.push(entry);
-          }
-        }
-        return acc;
-      }, []) || [];
-
-  if (x < 0 || y < 0 || x + dim_x > width || y + dim_y > length) {
-    return true;
-  }
-
-  for (let xi = x; xi <= x + dim_x + gap; xi++) {
-    for (let yi = y; yi <= y + dim_y + gap; yi++) {
-      for (let plantedCell of plantedCells) {
-        if (plantedCell[0] === `${xi},${yi}`) {
-          return true;
-        }
-      }
-    }
-    for (let yi = y; yi >= y - gap; yi--) {
-      for (let plantedCell of plantedCells) {
-        if (plantedCell[0] === `${xi},${yi}`) {
-          return true;
-        }
-      }
-    }
-  }
-
-  for (let xi = x; xi >= x - gap; xi--) {
-    for (let yi = y; yi <= y + dim_y + gap; yi++) {
-      for (let plantedCell of plantedCells) {
-        if (plantedCell[0] === `${xi},${yi}`) {
-          return true;
-        }
-      }
-    }
-  }
-
-  return false;
 }
 
 async function createFieldObject(fieldParams, msWindow = createFieldTimeMs) {
--- fvtracker/seed/fields/index.js
+++ fvtracker/seed/fields/index.js
@@ -1,7 +1,8 @@
 import { createFieldTimeMs, optimizedParamsArray } from "../data/fields";
-import { createFields } from "./createFields";
+import { createFields } from "./create/createFields";
 
 export default {
-  create: (fieldsParams = optimizedParamsArray, msWindow = createFieldTimeMs) => createFields(fieldsParams, msWindow),
+  create: (fieldsParams = optimizedParamsArray, msWindow = createFieldTimeMs) =>
+    createFields(fieldsParams, msWindow),
   FIELD_TIME_WINDOW: 1000 * 10,
 };

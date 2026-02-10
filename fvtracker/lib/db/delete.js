879ec1af27cd1f4ba41bf394d505adf5254e5839
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-10 14:54:36 +0100

    delete crops before creation

--- fvtracker/lib/db/delete.js
+++ fvtracker/lib/db/delete.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 import Cultivation from "@/models/sectors/cultivation";
-const {Field, CultivationArea} = Cultivation;
+const { Field, CultivationArea } = Cultivation;
 import dbConnect from "./mongooseConnect.js";
 import { CultivationManager } from "@/models/user/managers/CultivationManager.js";
+import {
+  CropGeneralType,
+  CropMainType,
+  CropType,
+  CropVariety,
+} from "@/models/sectors/cultivation/Crops.js";
 
 export const deleteDB = async () => {
   await dbConnect();
@@ -26,5 +32,15 @@ export async function deleteFieldsWithDocs() {
   await CultivationManager.updateMany({}, { $set: { fields: [] } });
   console.log(
     "Deleted existing fields, cultivation areas, and field grid cells.",
+  );
+}
+
+export async function deleteCrops() {
+  await CropMainType.deleteMany({});
+  await CropGeneralType.deleteMany({});
+  await CropType.deleteMany({});
+  await CropVariety.deleteMany({});
+  console.log(
+    "Deleted existing crop main types, general types, types, and varieties.",
   );
 }

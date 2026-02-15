f097eed0771fedcf542b2171e2651eaae436dae9
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-15 20:50:10 +0100

    abolish server side fetch field list

--- fvtracker/app/(cultivation)/(fields)/upravljanje-poljima/pregled/page.js
+++ fvtracker/app/(cultivation)/(fields)/upravljanje-poljima/pregled/page.js
@@ -1,15 +1,12 @@
 import { FieldsList } from "@/components/cultivation/fields/preview/list";
-import clean from "@/lib/db/clean";
 import React from "react";
-import { getCultivationManager } from "@/lib/cultivation/cultivationManager";
 
 export default async function FieldListPage() {
-  const cultivationManager = await getCultivationManager();
   
   return (
     <>
       {/*  {JSON.stringify(cultivationManager)} */}
-      {<FieldsList fields={clean(cultivationManager.fields)} />}
+      {<FieldsList />}
     </>
   );
 }

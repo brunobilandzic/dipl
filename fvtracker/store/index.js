1bb4652e446883089cfe4cd800ac0782e400a202
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-15 13:55:25 +0100

    import errorReducer from "./error";

--- fvtracker/store/index.js
+++ fvtracker/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import fieldReducer from "./cultivation/fieldSlice";
+import errorReducer from "./error";
 
 export default configureStore({
   reducer: {

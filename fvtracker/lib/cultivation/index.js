b58e25d3ba819e95dfa257eeedd6e3990d2cb1cd
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-15 20:50:56 +0100

    cultivation lib index

---
+++ fvtracker/lib/cultivation/index.js
@@ -0,0 +1,11 @@
+import { fieldsList, fetchFieldBySlug } from "./fields";
+
+export default {
+    fields: {
+        fieldsList,
+        fetchFieldBySlug
+    },
+    cultivationManager: {
+        fetchCultivationManager
+    }
+}

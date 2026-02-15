bd6d874789e14a07ced8268b29942fc9f53c0be6
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-15 15:03:06 +0100

    edit spinner color

--- fvtracker/components/layout/loading.js
+++ fvtracker/components/layout/loading.js
@@ -3,8 +3,8 @@ import { ClipLoader } from "react-spinners";
 export function Loading() {
   return (
     <>
-      <div className="w-full h-full flex items-center justify-center">
-        <ClipLoader size={50} color="#000000" />
+      <div className="w-full h-full flex items-center justify-center  text-gray-600">
+        <ClipLoader size={50} color="currentColor" />
       </div>
     </>
   );

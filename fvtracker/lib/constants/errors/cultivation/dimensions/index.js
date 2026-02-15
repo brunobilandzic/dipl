bb2516a3d2da80127fb7e650335e5e2ac29a8251
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-15 13:52:46 +0100

    create cultivation dimensions errors

---
+++ fvtracker/lib/constants/errors/cultivation/dimensions/index.js
@@ -0,0 +1,4 @@
+export default {
+  CULTIVATION_AREA_DIMENSIONS: (cultivationAreaDimenions) =>
+    `Cultivation area dimensions must be at least ${cultivationAreaDimenions.min_ca_dim}x${cultivationAreaDimenions.min_ca_dim} and at most ${cultivationAreaDimenions.max_ca_dim}x${cultivationAreaDimenions.max_ca_dim}`,
+};

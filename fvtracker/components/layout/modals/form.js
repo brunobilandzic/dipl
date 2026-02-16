7e291587db55355a92e65572e217552976a61b50
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-16 17:57:53 +0100

    form modal footer

--- fvtracker/components/layout/modals/form.js
+++ fvtracker/components/layout/modals/form.js
@@ -1,10 +1,25 @@
-import Modal from "./backdrop";
+import Modal from "./modal";
 
 export function FormModal({ isOpen, onCancel, onSubmit, title, children }) {
   return (
-    <Modal isOpen={isOpen} onCancel={onCancel}>
+    <Modal isOpen={isOpen} onCancel={onCancel} title={title}>
       {" "}
-      <div>hey mia</div>
+      {children}
+      <FormModalFooter onCancel={onCancel} onSubmit={onSubmit} />
     </Modal>
   );
+}
+
+
+function FormModalFooter({ onCancel, onSubmit }) {
+    return (
+        <div className="flex justify-end gap-4 mt-4">
+            <div onClick={onCancel} className="px-4 py-2 bg-gray-300 rounded cursor-pointer">
+                Cancel
+            </div>
+            <div onClick={onSubmit} className="px-4 py-2 bg-blue-500 text-white rounded cursor-pointer">
+                Submit
+            </div>
+        </div>
+    );
 }

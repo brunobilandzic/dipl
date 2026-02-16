f4255d645eb20ee9b5a2704af6b82d1f849f7569
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-16 15:50:11 +0100

    abolish field slice

--- fvtracker/store/index.js
+++ fvtracker/store/index.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
-import fieldReducer from "./cultivation/fieldSlice";
 import errorReducer from "./error";
 import loadingReducer from "./loading";
 import cultivationReducer from "./cultivation";
@@ -8,7 +7,6 @@ import cultivationReducer from "./cultivation";
 export default configureStore({
   reducer: {
     user: userReducer,
-    fields: fieldReducer,
     error: errorReducer,
     loading: loadingReducer,
     cultivation: cultivationReducer

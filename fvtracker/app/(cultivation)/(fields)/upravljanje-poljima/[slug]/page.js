2ca6fb69c8a2e594189b2fee5594a542b1f5fd13
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-16 15:36:28 +0100

    prepare server page for redux

--- fvtracker/app/(cultivation)/(fields)/upravljanje-poljima/[slug]/page.js
+++ fvtracker/app/(cultivation)/(fields)/upravljanje-poljima/[slug]/page.js
@@ -1,15 +1,8 @@
 import FieldPageComponent from "@/components/cultivation/fields/field";
-import { fetchFieldBySlug } from "@/lib/cultivation/fields";
-import clean from "@/lib/db/clean";
 import React from "react";
 
 export default async function FieldPage({ params }) {
   const { slug } = await params;
-  const field = await fetchFieldBySlug(slug);
-  await field.populate({
-    path: "cultivationAreas",
-    populate: { path: "cultivations" },
-  });
 
   return (
     <>

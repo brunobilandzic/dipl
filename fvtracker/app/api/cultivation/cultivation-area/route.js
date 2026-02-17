8b7f7924df1957b22ababdca3a2c9af12fd95099
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-17 19:13:24 +0100

    simpler to send planted array so transform on backend

--- fvtracker/app/api/cultivation/cultivation-area/route.js
+++ fvtracker/app/api/cultivation/cultivation-area/route.js
@@ -2,6 +2,7 @@ import dbConnect from "@/lib/db/mongooseConnect";
 import auth from "@/lib/auth";
 import { CultivationArea } from "@/models/sectors/cultivation/Cultivation";
 import { Field } from "@/models/sectors/cultivation/Field";
+import { plantedArrayToMap } from "@/lib/utils/cultivationAreas";
 
 export async function POST(request) {
   try {
@@ -10,15 +11,19 @@ export async function POST(request) {
       await auth.session.fetchSessionSpecificManager("CultivationManager");
     const body = await request.json();
     const properties = transformBody(body);
-    console.log(cultivationManager?.fields, "fields of cultivation manager")
-    if (!cultivationManager.fields?.some((fid) => fid?.toString() === properties.field)) {
+    if (
+      !cultivationManager.fields?.some(
+        (fid) => fid?.toString() === properties.field,
+      )
+    ) {
       throw new Error(
         "Field with the provided ID does not belong to the user's cultivation manager.",
       );
     }
 
     const newCultivationArea = await createCultivationArea(properties);
-    return Response.json({  newCultivationArea }, { status: 201 });
+
+    return Response.json({ newCultivationArea }, { status: 201 });
   } catch (error) {
     console.error(error);
     return Response.json(
@@ -31,20 +36,20 @@ export async function POST(request) {
 }
 
 function transformBody(body) {
-  const { field, name, description, planted } = body;
+  const { field, name, description } = body;
+  const planted = plantedArrayToMap(body.planted);
   return { field, name, description, planted };
 }
 
-
 async function createCultivationArea(properties) {
-    const field = await Field.findById(properties.field);
-    if (!field) {
-      throw new Error("Field not found with the provided ID.");
-    }
+  const field = await Field.findById(properties.field);
+  if (!field) {
+    throw new Error("Field not found with the provided ID.");
+  }
 
-    const newCultivationArea = new CultivationArea(properties)
-    field.cultivationAreas.push(newCultivationArea._id);
-    await field.save();
-    await newCultivationArea.save();
-    return newCultivationArea;
+  const newCultivationArea = new CultivationArea(properties);
+  field.cultivationAreas.push(newCultivationArea._id);
+  await field.save();
+  await newCultivationArea.save();
+  return newCultivationArea;
 }

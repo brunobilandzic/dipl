ceaa8350461e7d126d601fd93f93aff1c8a0918e
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-17 16:10:38 +0100

    handleApiError general not fields

--- fvtracker/lib/constants/errors/client/api.js
+++ fvtracker/lib/constants/errors/client/api.js
@@ -1,6 +1,5 @@
 export const handleApiError = (error) => {
-  console.log("Error fetching fields:", error);
   const errorMessage =
-    error.response?.data?.message || error.message || "Unknown error";
-  alert(`Error fetching fields: ${errorMessage}`);
+    error.response?.data?.message || error.message || "Nepoznata greška";
+  alert(errorMessage);
 };

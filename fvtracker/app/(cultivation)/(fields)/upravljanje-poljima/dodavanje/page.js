e66e0f3f7e8bbb6e564d5897c2a6f2becd689065
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-18 11:47:53 +0100

    CreateFieldPage

---
+++ fvtracker/app/(cultivation)/(fields)/upravljanje-poljima/dodavanje/page.js
@@ -0,0 +1,12 @@
+import { CreateFieldPageComponent } from "@/components/cultivation/fields/field/create";
+import React from "react";
+
+export default function CreateFieldPage() {
+  return (
+    <>
+      <div>
+        <CreateFieldPageComponent />
+      </div>
+    </>
+  );
+};

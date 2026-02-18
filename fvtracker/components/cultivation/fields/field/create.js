be163d4ed4dccae6e2f08b45fd64023de266a410
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-18 11:52:20 +0100

    replace get with useSession()

--- fvtracker/components/cultivation/fields/field/create.js
+++ fvtracker/components/cultivation/fields/field/create.js
@@ -1,22 +1,14 @@
 "use client";
 
-import { setManagerModelName } from "@/store/userSlice";
-import { getSession } from "next-auth/react";
+import {  useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 export function CreateFieldPageComponent() {
-  const { data: session, status } = getSession();
+  const { data: session, status } = useSession();
   const managerModelName = useSelector(
     (state) => state.user.session?.managerModelName,
   );
-
-  useEffect(() => {
-    console.log("Session status:", status);
-    if (status === "authenticated" && session.user?.managerModelName) {
-      setManagerModelName(session.user?.managerModelName);
-    }
-  }, [status]);
 
   return (
     <>

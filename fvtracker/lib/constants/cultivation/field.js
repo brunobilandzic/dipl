28babc07cea12c505435c4b5a745f2e65dab3773
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-18 11:16:36 +0100

    field dimensions minmax constant

---
+++ fvtracker/lib/constants/cultivation/field.js
@@ -0,0 +1,4 @@
+export const fieldDimensions = {
+  MIN_FIELD_DIMENSION: 20,
+  MAX_FIELD_DIMENSION: 200,
+};
--- fvtracker/lib/constants/cultivation/index.js
+++ fvtracker/lib/constants/cultivation/index.js
@@ -1,7 +1,9 @@
 import colors from "./colors";
+import { fieldDimensions } from "./field";
 
 export default {
   colors: {
     selectedCABackGround: colors.selectedCABackGround,
   },
+  field: { fieldDimensions },
 };

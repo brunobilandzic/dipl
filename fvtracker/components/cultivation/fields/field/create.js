d22dc0f39731d8b67b0da020fb38e6d9f556c008
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-18 19:58:20 +0100

    onSubm field function call api, checks

--- fvtracker/components/cultivation/fields/field/create.js
+++ fvtracker/components/cultivation/fields/field/create.js
@@ -7,6 +7,8 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import styles from "@/components/form/form.module.css";
 import utils from "@/lib/utils";
+import api from "@/lib/api";
+import { handleApiError } from "@/lib/constants/errors/client/api";
 
 export function CreateFieldPageComponent() {
   const { data: session, status } = useSession();
@@ -41,17 +43,51 @@ export function CreateFieldPageComponent() {
     }));
   };
 
-  useEffect(() => {
-    console.log("Field data:", fieldData);
-  }, [fieldData]);
+  const onSubmit = () => {
+    if (utils.objects.checkEmpty(fieldData)) {
+      alert("Sva polja su obavezna");
+      return;
+    }
+    if (
+      !utils.formValidation.numberInRange(
+        fieldData.length,
+        cultivationConstants.field.fieldDimensions.MIN_FIELD_DIMENSION,
+        cultivationConstants.field.fieldDimensions.MAX_FIELD_DIMENSION,
+      )
+    ) {
+      alert(
+        `Dužina mora biti između ${cultivationConstants.field.fieldDimensions.MIN_FIELD_DIMENSION} i ${cultivationConstants.field.fieldDimensions.MAX_FIELD_DIMENSION}`,
+      );
+      return;
+    }
+    if (
+      !utils.formValidation.numberInRange(
+        fieldData.width,
+        cultivationConstants.field.fieldDimensions.MIN_FIELD_DIMENSION,
+        cultivationConstants.field.fieldDimensions.MAX_FIELD_DIMENSION,
+      )
+    ) {
+      alert(
+        `Širina mora biti između ${cultivationConstants.field.fieldDimensions.MIN_FIELD_DIMENSION} i ${cultivationConstants.field.fieldDimensions.MAX_FIELD_DIMENSION}`,
+      );
+      return;
+    }
+    try {
+      const res = api.post("/cultivation/field", fieldData);
+      console.log(res);
+    } catch (err) {
+      handleApiError({
+        ...err,
+        generalMessage: "Greška prilikom kreiranja parcele",
+      });
+    }
+  };
 
   return (
     <>
       <div className="form">
         {cultivationConstants.field?.formInputs.map((input) => {
-          let returnValue = null;
           if (input.type === "text" || input.type === "number") {
-            
             return (
               <AppInput
                 key={input.name}
@@ -78,7 +114,9 @@ export function CreateFieldPageComponent() {
           }
         })}
         <div className={`${styles.footer}`}>
-          <div className="btn submitButton">Kreiraj parcelu</div>
+          <div onClick={onSubmit} className="btn submitButton">
+            Kreiraj parcelu
+          </div>
         </div>
       </div>
     </>

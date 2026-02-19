d6772eae1389a0eb9d439fd9018c2d1318125786
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-19 14:35:44 +0100

    empty cultivation state action

--- fvtracker/store/cultivation/index.js
+++ fvtracker/store/cultivation/index.js
@@ -16,9 +16,10 @@ const cultivationSlice = createSlice({
     },
     selectField: (state, action) => {
       state.selectedField = action.payload;
-    }
+    },
+    emptyCultivation: () = ({...initialState}),
   },
 });
 
-export const { setFields, setInitialState, selectField } = cultivationSlice.actions;
+export const { setFields, setInitialState, selectField, emptyFields } = cultivationSlice.actions;
 export default cultivationSlice.reducer;

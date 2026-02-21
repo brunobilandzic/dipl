29ac7efad4873e8841fecaa88cac7d8ce082a42e
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-21 21:55:25 +0100

    plantCropVariety alinged with new schema structures

--- fvtracker/lib/cultivation/plant.js
+++ fvtracker/lib/cultivation/plant.js
@@ -56,9 +56,16 @@ export async function plantCropVariety({
     cellCoords,
   });
 
-  cultivationArea.planted.set(fieldCoords, cropVarietyId);
+  cultivationArea.planted.set(fieldCoords, {
+    cropVariety: cropVariety._id,
+    plantedAt: new Date(),
+  });
 
-  cropVariety.cultivationAreas.push(cultivationAreaId);
+  cropVariety.cultivationAreas.push({
+    cultivationArea: cultivationAreaId,
+    cellCoords,
+    fieldCoords,
+  });
   await cropVariety.save();
   await cultivationArea.save();
 
@@ -70,3 +77,5 @@ export async function plantCropVariety({
     message: `Planted ${cropVariety.name} in cultivation area ${cultivationArea.name} at cell ${cellCoords}, field cell ${fieldCoords}.`,
   };
 }
+
+

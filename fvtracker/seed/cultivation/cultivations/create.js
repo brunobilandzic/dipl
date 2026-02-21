cbcd7f2e80024f441e04237ee6881206f77bb0eb
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-21 10:53:03 +0100

    getFieldAndCropData

--- fvtracker/seed/cultivation/cultivations/create.js
+++ fvtracker/seed/cultivation/cultivations/create.js
@@ -1,15 +1,18 @@
 import cultivation from "@/lib/cultivation";
 
 export async function createCultivations() {
-  const fields = await cultivation.fields.list();
-  const cropData = await cultivation.crops.data();
+  const { fields, cropData } = await getFieldAndCropData();
 
   console.log(`Seeding cultivations for ${fields.length} fields...`);
-  Object.keys(cropData).forEach((key) => {
-    console.log(`  ${key}: ${cropData[key].length} items`);
-  });
 
   // Here you would implement the logic to create cultivation areas based on the fields and crop data.
   // This is a placeholder to indicate where that logic would go.
   console.log("Creating cultivation areas...");
+}
+
+
+async function getFieldAndCropData() {
+  const fields = await cultivation.fields.list();
+  const cropData = await cultivation.crops.data();
+  return { fields, cropData };
 }

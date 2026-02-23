8e593cbb217eaba3fa3ffed4e9022a7fb37fefb2
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-23 17:01:03 +0100

    ca actions constants

---
+++ fvtracker/components/cultivation/cultivationArea/actions.js
@@ -0,0 +1,13 @@
+import { MdArrowLeft } from "react-icons/md";
+
+export const actions = [
+  {
+    name: "Back to field",
+    onClick: () => {
+      console.log("back to field");
+    },
+    icon: <MdArrowLeft />,
+  },
+];
+
+

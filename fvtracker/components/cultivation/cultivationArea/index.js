4d53b3a23ec6128952cf6bc3af9b5d1630966b7c
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-23 16:04:48 +0100

    try catch in fil cult area fields case

--- fvtracker/components/cultivation/cultivationArea/index.js
+++ fvtracker/components/cultivation/cultivationArea/index.js
@@ -4,6 +4,7 @@ import { handleApiError } from "@/lib/constants/errors/client/api";
 import { useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectField, setFields } from "@/store/cultivation";
+import handleError from "@/lib/constants/errors/client/handleError";
 
 export function CultivationAreaPageComponent({ fieldSlug, caSlug }) {
   const selectedField = useSelector((state) => state.cultivation.selectedField);
@@ -16,14 +17,23 @@ export function CultivationAreaPageComponent({ fieldSlug, caSlug }) {
   useEffect(() => {
     const fillCultivationArea = async () => {
       if (!selectedField && fields && fields.length > 0) {
-        console.log(
-          "slected field is null, but fields are in store, finding field...",
-        );
-        const foundField = fields.find((f) => f.slug === fieldSlug);
-        if (foundField) {
-          dispatch(selectField(foundField));
-        } else {
-          console.log("Field not found in store, fetching from API...");
+        try {
+          console.log(
+            "slected field is null, but fields are in store, finding field...",
+          );
+          const foundField = fields.find((f) => f.slug === fieldSlug);
+          if (foundField) {
+            dispatch(selectField(foundField));
+            return;
+          } else {
+            throw new Error("Field not found in store but fields in store");
+          }
+        } catch (error) {
+          handleError({
+            ...error,
+            generalMessage: "Failed to load cultivation area",
+          });
+        }
         }
       }
     };

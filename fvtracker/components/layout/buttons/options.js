7a5fa079c167bc650d4564baffc87d3c9f49b802
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-23 21:15:53 +0100

    small prop in option buttons

--- fvtracker/components/layout/buttons/options.js
+++ fvtracker/components/layout/buttons/options.js
@@ -1,26 +1,26 @@
 import { v4 as uuid } from "uuid";
 
-export function OptionButtons({ options, row = true }) {
+export function OptionButtons({ options, row = true, small = false }) {
   return (
     <>
       <div
         className={`flex ${row ? "flex-row w-2/3 mx-auto h-fit items-stretch justify-center " : "flex-col justify-start h-full "}  gap-2`}
       >
         {options.map((option, index) => (
-          <OptionButton key={uuid()} option={option} />
+          <OptionButton key={uuid()} option={option} small={small} />
         ))}
       </div>
     </>
   );
 }
 
-export const OptionButton = ({ option }) => {
+export const OptionButton = ({ option, small }) => {
   const { label, onClick, icon } = option;
   return (
     <>
       <div
         onClick={onClick}
-        className="flex flex-col gap-2 items-center justify-center cursor-pointer min-w-12 overflow-visible  md:gap-4 btn w-24"
+        className={`flex flex-col gap-2 items-center justify-center cursor-pointer min-w-12 overflow-visible  md:gap-4 btn ${small ? "w-16 h-16 btnSm text-red-500" : ""}`}
       >
         <div className="text-3xl">{icon}</div>
         <div className="text-sm text-wrap text-center">{label}</div>

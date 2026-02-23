b3bc8ea505bff0f85c5af5e5e8b97018da9a5ebc
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-23 20:01:03 +0100

    map options to option Buttons seed

--- fvtracker/components/seed/options.js
+++ fvtracker/components/seed/options.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { FaUserPlus } from "react-icons/fa";
 import { MdAllInclusive, MdDeleteForever, MdFoodBank } from "react-icons/md";
 import SEED_TYPES from "@/seed/seedTypes";
+import { OptionButtons } from "../layout/buttons/options";
 
 export default function SeedOptions() {
   const deleteDB = async () => {
@@ -78,47 +79,8 @@ export default function SeedOptions() {
   return (
     <>
       <div className="flex justify-center items-stretch gap-6 w-2/3 mx-auto h-fit  ">
-        {/*  {Object.entries(_icons).map(
-          ([key, { icon, label, function: func }]) => (
-            <div
-              key={key}
-              className="flex flex-col gap-2 btn items-center justify-around p-2 w-32  cursor-pointer "
-              onClick={async () => API(label)}
-            >
-              <div className="text-3xl">{icon}</div>
-              <div className="text-sm text-wrap text-center">{label}</div>
-            </div>
-          ),
-        )} 
-        <DeleteDB />
-        */}
+        <OptionButtons options={options.map(mapOption)} />
       </div>
     </>
   );
 }
-
-/* export function DeleteDB() {
-  const deleteDB = async () => {
-    try {
-      const response = await axios.delete("/api/delete");
-      const { success } = response.data;
-      console.log("SUCCESS:", response.data);
-      if (success) alert("Database deleted successfully");
-    } catch (error) {
-      console.error("Error deleting database:", error);
-      throw new Error(SEED_ERROR);
-    }
-  };
-  return (
-    <button
-      onClick={deleteDB}
-      className="flex flex-col gap-2 btn items-center justify-around p-2 w-32 cursor-pointer"
-    >
-      <div className="text-3xl">
-        <MdDeleteForever />
-      </div>
-      Delete DB
-    </button>
-  );
-}
- */

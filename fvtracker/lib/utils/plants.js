56451cbb48f9ea0b6c51baba5cccacccfa9f6905
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-25 19:47:36 +0100

    checkDimension functions to see if all free

--- fvtracker/lib/utils/plants.js
+++ fvtracker/lib/utils/plants.js
@@ -9,4 +9,22 @@ export function relativeToFieldCoords({ planted, cellCoords }) {
   return `${minX + width},${minY + length}`;
 }
 
+function checkDimension({ start, end, plantedCropVarieties }) {
+  for (let y = start.length; y <= end.length; y++) {
+    for (let x = start.width; x <= end.width; x++) {
+      const variety = plantedCropVarieties.find(
+        (variety) =>
+          variety.relativeCoords === `${x},${y}` &&
+          !variety.harvestedAt &&
+          variety.plantedAt,
+      );
+      if (variety) {
+        return {
+          filled: true,
+          variety,
+        };
+      }
+    }
+  }
+  return null;
 }

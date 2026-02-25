fc8eb3534089b0ee000be3a36ce2de1560d7e873
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-25 17:55:22 +0100

    field not needed in cultivation

--- fvtracker/models/sectors/cultivation/Cultivation.js
+++ fvtracker/models/sectors/cultivation/Cultivation.js
@@ -44,11 +44,6 @@ const cultivationAreaSchema = new Schema({
 // also has start and end date, status so it maybe will be used for some planning and tracking... idk yet
 // harvest will probably be linked to cultivation
 const cultivationSchema = new Schema({
-  field: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Field",
-    required: true,
-  },
   cultivationArea: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "CultivationArea",

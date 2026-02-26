9eedd47bf10500ff5cfd2e927b03cdf1513c82b4
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-26 12:00:14 +0100

    dont send cas and setclick to field grid

--- fvtracker/components/cultivation/fields/field/index.js
+++ fvtracker/components/cultivation/fields/field/index.js
@@ -217,8 +217,6 @@ function FieldEditCASPanel({
           <FieldGrid
             width={width}
             length={length}
-            cultivationAreas={cultivationAreas}
-            setClickedCell={editCultivationAreas ? setClickedCell : null}
             plantedCells={plantedCells}
             selectedCultivationArea={selectedCultivationArea}
             newCACoordinates={newCACoordinates}

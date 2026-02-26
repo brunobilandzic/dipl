116679a987964a2d3bc299f76532ddae269abca3
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-26 18:07:33 +0100

    CultivateCells ca option file

---
+++ fvtracker/components/cultivation/cultivationArea/cultivate.js
@@ -0,0 +1,11 @@
+import Modals from "@/components/layout/modals";
+
+export function CultivateCells({ cultivationCoordinates, cultivationArea }) {
+
+  return (
+    <>
+      <Modals.FormModal title="Cultivate Cells"></Modals.FormModal>
+      <div></div>
+    </>
+  );
+}

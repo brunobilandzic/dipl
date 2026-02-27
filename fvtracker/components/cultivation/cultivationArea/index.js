92a99e2c0a2a7bdf3b3d706be11ff82fdc441582
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-27 14:32:19 +0100

    end return if not begin

--- fvtracker/components/cultivation/cultivationArea/index.js
+++ fvtracker/components/cultivation/cultivationArea/index.js
@@ -125,7 +125,7 @@ export function CultivationAreaPageComponent({ fieldSlug, caSlug }) {
   };
 
   const onEndCoordinates = (x, y) => {
-    if(!newCUDetails.begin) return;
+    if (!newCUDetails.begin) return;
     const potentialCUCells = utils.cultivation.cultivationAreas.getCellsInRect({
       beginX: newCUDetails.begin.x,
       beginY: newCUDetails.begin.y,

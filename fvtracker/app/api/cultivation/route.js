f7929ca8522797a04d4f37d7ef54be3fd3b1554e
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-27 15:31:59 +0100

    make sure its manager

--- fvtracker/app/api/cultivation/route.js
+++ fvtracker/app/api/cultivation/route.js
@@ -1,9 +1,11 @@
 import cultivation from "@/lib/cultivation";
 import dbConnect from "@/lib/db/mongooseConnect";
+import auth from "@/lib/auth";
 
 export async function POST(request) {
   try {
     await dbConnect();
+    await auth.session.fetchSessionSpecificManager("CultivationManager");
     const body = await request.json();
 
     const newCultivation =

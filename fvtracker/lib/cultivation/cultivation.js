e710e7874be698840a9d9966e3c84b3192fb2195
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-27 12:03:36 +0100

    create cult simple lib func

--- fvtracker/lib/cultivation/cultivation.js
+++ fvtracker/lib/cultivation/cultivation.js
@@ -1,3 +1,33 @@
-async function createCultivation({}) {
-    
+import Cultivation from "@/models/sectors/cultivation/Cultivation";
+import CultivationArea from "@/models/sectors/cultivation/CultivationArea";
+import utils from "@/utils";
+import mongoose from "mongoose";
+
+async function createCultivation({ cultivation }) {
+  const {
+    cultivationArea: caId,
+    name,
+    description,
+    plantedCropVarieties,
+    workHours,
+    status,
+    startDate,
+    endDate,
+  } = cultivation;
+
+  const newCultivation = new Cultivation({
+    cultivationArea: caId,
+    name,
+    description,
+  });
+
+  const cuArea = await CultivationArea.findById(caId);
+  if (!cuArea) {
+    throw new Error("Cultivation area not found");
+  }
+  cuArea.cultivations.push(newCultivation._id);
+  await cuArea.save();
+  await newCultivation.save();
+
+  return newCultivation;
 }

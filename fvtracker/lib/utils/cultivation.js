25025200d0f8ccf3a83d2e2fe2b6032eac7454c5
brunobilandzic <bruno.bilandzic@outlook.com>
2026-02-28 11:02:16 +0100

    prepareCultivationData exculname

--- fvtracker/lib/utils/cultivation.js
+++ fvtracker/lib/utils/cultivation.js
@@ -56,6 +56,7 @@ export const prepareCultivationData = (newCultivation) => {
     description: newCultivation.description,
     relativeCoords: newCultivation.potentialCUCells,
     cropVarietyId: newCultivation.cropVarietyId,
+    existingCulName = newCultivation.existingCulName
   };
   return cultivationData;
 };

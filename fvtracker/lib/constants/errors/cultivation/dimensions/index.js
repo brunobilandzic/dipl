fe0b54b48842f3756dc56523afcf6736cda3adf2
brunobilandzic <bruno.bilandzic@outlook.com>
2026-03-01 19:54:31 +0100

    CULTIVATION_OVERLAP

--- fvtracker/lib/constants/errors/cultivation/dimensions/index.js
+++ fvtracker/lib/constants/errors/cultivation/dimensions/index.js
@@ -3,4 +3,5 @@ export default {
     `Cultivation area dimensions must be at least ${cultivationAreaDimenions.min_ca_dim}x${cultivationAreaDimenions.min_ca_dim} and at most ${cultivationAreaDimenions.max_ca_dim}x${cultivationAreaDimenions.max_ca_dim}`,
   CULTIVATION_AREA_OVERLAP: "Cultivation areas cannot overlap",
   CULTIVATION_AREA_GAP: (gap) => `Cultivation areas must have a gap of at least ${gap} cells between them`,
+  CULTIVATION_OVERLAP: "Cultivations cannot overlap",
 };

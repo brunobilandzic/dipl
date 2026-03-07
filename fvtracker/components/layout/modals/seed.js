9677d13367b0c03a349b74794edaab205f5b94fe
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-07 18:59:14 +0100

    seeding modal info block

--- fvtracker/components/layout/modals/seed.js
+++ fvtracker/components/layout/modals/seed.js
@@ -2,7 +2,13 @@ import Modal from "./modal"
 
 export const SeedingModal = (isOpen, onCancel, cultivationArea, plants) => {
     return <Modal title="Seeding Modal" isOpen={isOpen} onCancel={onCancel}>
-        <p>Cultivation Area: {cultivationArea}</p>
+        <div className="flex flex-col gap-2">
+        <div className="font-bold text-xl">
+{cultivation?.name || "N/A"}
+</div>
+        <div>
+    {cultivation?.description}
+        </div>
         </div>
       <ModalFooter>
         <div className={`btn submitButton`}>Submit</div>

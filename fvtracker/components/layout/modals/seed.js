309ad13df8c3ff44e74d7be2c753ac221f0e002c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-07 18:57:40 +0100

    seed modal modal footer

--- fvtracker/components/layout/modals/seed.js
+++ fvtracker/components/layout/modals/seed.js
@@ -3,6 +3,11 @@ import Modal from "./modal"
 export const SeedingModal = (isOpen, onCancel, cultivationArea, plants) => {
     return <Modal title="Seeding Modal" isOpen={isOpen} onCancel={onCancel}>
         <p>Cultivation Area: {cultivationArea}</p>
-        <p>Plants: {plants.length}</p>
+        </div>
+      <ModalFooter>
+        <div className={`btn submitButton`}>Submit</div>
+        <div className="btn cancelButton">Cancel</div>
+      </ModalFooter>
     </Modal>
-}
+  );
+};

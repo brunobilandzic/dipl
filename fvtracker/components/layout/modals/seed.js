e548266c4c9e277f84b69b82b41eeed5ed1df5ad
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-07 19:01:16 +0100

    seeding modal rest unknown

--- fvtracker/components/layout/modals/seed.js
+++ fvtracker/components/layout/modals/seed.js
@@ -1,16 +1,20 @@
 "use client";
 
 import Modal, { ModalFooter } from "./modal";
+
+export const SeedingModal = ({ isOpen, onCancel, cultivation, crops }) => {
   console.log("isopnen:", isOpen);
   console.log("cultivation:", cultivation);
-        <div className="flex flex-col gap-2">
+  return (
+    <Modal title="Seeding Modal" isOpen={isOpen} onCancel={onCancel}>
+      <div className="flex flex-col gap-2">
         <div className="font-bold text-xl">
-{cultivation?.name || "N/A"}
-</div>
+          {cultivation?.name || "N/A"}
+        </div>
         <div>
     {cultivation?.description}
         </div>
-        </div>
+      </div>
       <ModalFooter>
         <div className={`btn submitButton`}>Submit</div>
         <div className="btn cancelButton">Cancel</div>

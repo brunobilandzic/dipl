0b0889803195962c7a8536d5085bf18fe4e19d40
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-10 18:20:48 +0100

    move seed modal to cultivation components

--- fvtracker/components/cultivation/cultivationArea/index.js
+++ fvtracker/components/cultivation/cultivationArea/index.js
@@ -17,7 +17,7 @@ import utils from "@/lib/utils";
 import { CreateCultivation } from "./createCultivation";
 import cultivation from "@/lib/constants/cultivation";
 import { EditCultivation } from "./editCultivation";
-import { SeedingModal } from "@/components/layout/modals/seed";
+import { SeedingModal } from "@/components/cultivation/cultivationArea/seeding/seed";
 
 export function CultivationAreaPageComponent({ fieldSlug, caSlug }) {
   const dispatch = useDispatch();

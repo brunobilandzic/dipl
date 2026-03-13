65a9594ce767f0b98c791a38d95cc1d2a7d51454
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-13 13:02:45 +0100

    missing: remove cultivateReducer import

--- fvtracker/store/index.js
+++ fvtracker/store/index.js
@@ -3,7 +3,6 @@ import userReducer from "./userSlice";
 import errorReducer from "./error";
 import loadingReducer from "./loading";
 import cultivationReducer from "./cultivation";
-import cultivateReducer from "./cultivation/cultivate";
 
 export default configureStore({
   reducer: {
@@ -11,6 +10,5 @@ export default configureStore({
     error: errorReducer,
     loading: loadingReducer,
     cultivation: cultivationReducer,
-    cultivate: cultivateReducer,
   },
 });

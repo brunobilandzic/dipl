83f4f52c351ebf6d9d98955ab42ba946b68c02cc
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-15 10:58:23 +0100

    creant plant plan select field shows fields len

--- fvtracker/components/cultivation/planning/create/plantingPlan.js
+++ fvtracker/components/cultivation/planning/create/plantingPlan.js
@@ -1,10 +1,17 @@
 "use client";
 
+import api from "@/lib/api";
+import { setFields } from "@/store/cultivation";
 import Link from "next/link";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function CreatePlantingPlanPageComonent() {
-  return <div>CreatePlantingPlanPageComonent</div>;
+  return (
+    <>
+      <SelectField />
+    </>
+  );
 }
 
 export default CreatePlantingPlanPageComonent;
@@ -13,12 +20,33 @@ export const CreatePlantagePlan = ({}) => {
   return (
     <>
       <Link href={"/plan-sadnje/izradi"}>
-        <div
-          className="btn"
-        >
-          Izradi plan sadnje
-        </div>
+        <div className="btn">Izradi plan sadnje</div>
       </Link>
     </>
   );
+};
+
+const SelectField = ({ selectedField, setSelectedField }) => {
+  const fields = useSelector((state) => state.cultivation.fields);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (fields && fields.length > 0) return;
+
+    (async () => {
+      try {
+        const res = await api.get("/cultivation/fields");
+        if (res.data && res.data.fields) {
+          dispatch(setFields(res.data.fields));
+        }
+      } catch (error) {
+        console.log("Error fetching fields:", error);
+        const errorMessage =
+          error.response?.data?.message || error.message || "Unknown error";
+        alert(`Error fetching fields: ${errorMessage}`);
+      }
+    })();
+  }, [fields]);
+
+  return <>{fields?.length}</>;
 };

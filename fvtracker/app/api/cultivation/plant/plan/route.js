a81242141bbea7fae4c779ff832a89aacb9f46e2
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-17 12:43:17 +0100

    import db and send plantingPlanData prop

--- fvtracker/app/api/cultivation/plant/plan/route.js
+++ fvtracker/app/api/cultivation/plant/plan/route.js
@@ -1,11 +1,12 @@
 import { createPlantingPlan } from "@/lib/cultivation/plant";
+import dbConnect from "@/lib/db/mongooseConnect";
 
 export async function POST(request) {
   try {
     await dbConnect();
     const body = await request.json();
-    const newPlantingPlan = await createPlantingPlan(body);
-
+    const newPlantingPlan = await createPlantingPlan({ plantingPlanData: body });
+    console.log("Created new planting plan:", newPlantingPlan);
     return Response.json(newPlantingPlan, { status: 201 });
   } catch (error) {
     console.error(error);

ca6dae942aba95c20dc2ddef72f1df774825657c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-18 18:55:02 +0100

    return { newPlantingPlan } in route

--- fvtracker/app/api/cultivation/plant/plan/route.js
+++ fvtracker/app/api/cultivation/plant/plan/route.js
@@ -5,9 +5,11 @@ export async function POST(request) {
   try {
     await dbConnect();
     const body = await request.json();
-    const newPlantingPlan = await createPlantingPlan({ plantingPlanData: body });
+    const newPlantingPlan = await createPlantingPlan({
+      plantingPlanData: body,
+    });
     console.log("Created new planting plan:", newPlantingPlan);
-    return Response.json(newPlantingPlan, { status: 201 });
+    return Response.json({ newPlantingPlan }, { status: 201 });
   } catch (error) {
     console.error(error);
     return Response.json({ error: error.message }, { status: 500 });

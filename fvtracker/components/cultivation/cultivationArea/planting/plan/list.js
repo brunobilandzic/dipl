8ad062d7d0a4e77433052f83d9006cc7413b9043
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-18 19:21:43 +0100

    serlect or fetch fields in planting plans list

--- fvtracker/components/cultivation/cultivationArea/planting/plan/list.js
+++ fvtracker/components/cultivation/cultivationArea/planting/plan/list.js
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
+
+import { Loading } from "@/components/layout/loading";
+import api from "@/lib/api";
+import { setFields } from "@/store/cultivation";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const PlantingPlanList = () => {
-  return <div>PlantingPlanList</div>;
+  const fields = useSelector((state) => state.cultivation.fields);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (fields && fields.length > 0) return;
+
+    (async () => {
+      try {
+        const res = await api.get("/cultivation/fields");
+        if (res.data && res.data.fields) {
+          dispatch(setFields(res.data.fields));
+        }
+      } catch (error) {
+        console.log("Error fetching fields:", error);
+        const errorMessage =
+          error.response?.data?.message || error.message || "Unknown error";
+        alert(`Error fetching fields: ${errorMessage}`);
+      }
+    })();
+  }, [fields]);
+
+  if (!fields || fields.length === 0)
+    return (
+      <div className="w-full py-4 flex items-center justify-center">
+        <Loading />
+      </div>
+    );
+  return <div>{fields?.length}</div>;
 };
 
 export default PlantingPlanList;

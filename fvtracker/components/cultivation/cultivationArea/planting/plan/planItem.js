f4317cf865db9e36896b5765f5c6dd2bcc2b6cd5
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-18 21:24:45 +0100

    render lanting plan list item

--- fvtracker/components/cultivation/cultivationArea/planting/plan/planItem.js
+++ fvtracker/components/cultivation/cultivationArea/planting/plan/planItem.js
@@ -1,3 +1,49 @@
-export const PlantingPlanItem = ({ plan }) => {
-  return <></>;
+export const PlantingPlanListItem = ({ plan }) => {
+  return (
+    <>
+      <div>
+        <div className="text-lg font-bold ">{plan.name}</div>
+        <PlanDates
+          plannedPlantingDate={plan.plannedPlantingDate}
+          plannedHarvestingDate={plan.plannedHarvestingDate}
+        />
+        <PlantingPlanItems items={plan.items} />
+      </div>
+    </>
+  );
 };
+
+const PlanDates = ({ plannedPlantingDate, plannedHarvestingDate }) => {
+  return (
+    <div className="text-sm text-gray-500">
+      {plannedPlantingDate && (
+        <div>
+          <span className="font-semibold">Planned Planting Date: </span>
+          {new Date(plannedPlantingDate).toLocaleDateString()}
+        </div>
+      )}
+      {plannedHarvestingDate && (
+        <div>
+          <span className="font-semibold">Planned Harvesting Date: </span>
+          {new Date(plannedHarvestingDate).toLocaleDateString()}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const PlantingPlanItems = ({ items }) => {
+  return (
+    <div className="mt-2">
+      <div>Items:</div>
+      <div className="flex flex-wrap gap-4 mt-1">
+        {items.map((item) => (
+          <div key={item._id} className="">
+            <PlantingPlanItem item={item} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+

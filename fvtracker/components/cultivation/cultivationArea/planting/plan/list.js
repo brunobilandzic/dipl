0b0f46abbeb074494d2e214bc91a431010c274b5
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-18 20:42:43 +0100

    delete plans btn

--- fvtracker/components/cultivation/cultivationArea/planting/plan/list.js
+++ fvtracker/components/cultivation/cultivationArea/planting/plan/list.js
@@ -2,6 +2,7 @@
 
 import { Loading } from "@/components/layout/loading";
 import api from "@/lib/api";
+import handleError from "@/lib/constants/errors/client/handleError";
 import utils from "@/lib/utils";
 import { setFields } from "@/store/cultivation";
 import React, { useEffect } from "react";
@@ -29,6 +30,17 @@ const PlantingPlanList = () => {
     })();
   }, [fields]);
 
+  const deletePlans = async () => {
+    try {
+      const res = await api.delete("/cultivation/plant/plan", {});
+    } catch (error) {
+      handleError({
+        ...error,
+        generalMessage: "Error deleting planting plans",
+      });
+    }
+  };
+
   if (!fields || fields.length === 0)
     return (
       <div className="w-full py-4 flex items-center justify-center">
@@ -37,7 +49,21 @@ const PlantingPlanList = () => {
     );
 
   const fieldsPlans = utils.plant.getFieldsPlans(fields);
-  return <div>{JSON.stringify(fieldsPlans)}</div>;
+  return (
+    <>
+      <div>
+        <div>{JSON.stringify(fieldsPlans, null, 2)}</div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <button
+          onClick={deletePlans}
+          className="bg-red-500 text-white py-2 px-4 rounded"
+        >
+          Delete Plans
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default PlantingPlanList;

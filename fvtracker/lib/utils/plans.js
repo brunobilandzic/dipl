a352724ea1ad4c87fbcd72a7528e2775959fae32
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-18 21:25:47 +0100

    getPlantedCropVarietesPerCultivation declaration and skeleton

---
+++ fvtracker/lib/utils/plans.js
@@ -0,0 +1,6 @@
+export const getPlantedCropVarietesPerCultivation = ({ items }) => {
+  const cultivationPlantedCropVarieties = {};
+  items.forEach((item) => {
+    const plantedCropVarieties = item.plantedCropVarieties || [];
+  });
+};

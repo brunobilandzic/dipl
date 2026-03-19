3721af02a40e2e105037b54d35edb1b66c192038
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-19 08:51:49 +0100

    getPlantedCropVarietesPerCultivation to map cultivation name to plvs

--- fvtracker/lib/utils/plans.js
+++ fvtracker/lib/utils/plans.js
@@ -1,6 +1,13 @@
-export const getPlantedCropVarietesPerCultivation = ({ items }) => {
+export const getPlantedCropVarietesPerCultivation = ({
+  plantedCropVarieties,
+}) => {
   const cultivationPlantedCropVarieties = {};
-  items.forEach((item) => {
-    const plantedCropVarieties = item.plantedCropVarieties || [];
+  plantedCropVarieties.forEach((plantedCropVariety) => {
+    const { cultivation, ...rest } = plantedCropVariety;
+    if (!cultivationPlantedCropVarieties[cultivation?.name]) {
+      cultivationPlantedCropVarieties[cultivation?.name] = [];
+    }
+    cultivationPlantedCropVarieties[cultivation?.name].push(rest);
   });
+  return cultivationPlantedCropVarieties;
 };

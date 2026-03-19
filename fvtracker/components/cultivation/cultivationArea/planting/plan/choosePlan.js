ff3c86db4d4bef3c21cd544c61aa65c24b560f8f
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-19 21:14:53 +0100

    adjust to whole plan in available anddeclare and show planting plan item

--- fvtracker/components/cultivation/cultivationArea/planting/plan/choosePlan.js
+++ fvtracker/components/cultivation/cultivationArea/planting/plan/choosePlan.js
@@ -21,16 +21,28 @@ export const ChoosePlan = ({
   );
   return (
     <div className="flex flex-wrap p-4 border">
-      {availablePlantingPlans.map((avPlan) => (
-        <div
-          key={avPlan._id}
-          className={`  p-2 m-2 border rounded cursor-pointer ${selectedPlan?._id === avPlan._id ? "border-green-200 ring-green-500 ring-2" : ""}`}
-          onClick={() => onChoosePlan(avPlan)}
-        >
-          <p>Plan sadnje: {avPlan.plantingPlanName}</p>
-          <p>Količina: {avPlan.quantity}</p>
-        </div>
-      ))}
+      {availablePlantingPlans.map((avPlan) => {
+        console.log("Rendering available plan:", avPlan);
+        const plantingPlanItem = avPlan.items.find(
+          (item) => item.cropVariety?._id === cropVarietyId,
+        );
+        console.log(
+          "Found planting plan item for cultivation:",
+          plantingPlanItem,
+        );
+        console.log("item:", plantingPlanItem);
+        return (
+          <div
+            key={avPlan._id}
+            className={`  p-2 m-2 border rounded cursor-pointer ${selectedPlan?._id === avPlan._id ? "border-green-200 ring-green-500 ring-2" : ""}`}
+            onClick={() => onChoosePlan(avPlan)}
+          >
+            <p>Plan sadnje: {avPlan.name}</p>
+            <p>Varijanta: {plantingPlanItem?.cropVariety?.name}</p>
+            <p>Planirana količina: {plantingPlanItem?.quantity}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };

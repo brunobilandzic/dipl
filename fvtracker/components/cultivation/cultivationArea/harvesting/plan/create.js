7817d48c00c1d4a03393da0fecad2271de2182e0
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-21 19:01:23 +0100

    creaste harv plan select field

--- fvtracker/components/cultivation/cultivationArea/harvesting/plan/create.js
+++ fvtracker/components/cultivation/cultivationArea/harvesting/plan/create.js
@@ -1,7 +1,19 @@
 "use client";
 
+import { SelectField } from "../../planting/plan/create";
+import { useState } from "react";
+
 export default function CreateHarvestingPlanPageComponent() {
-    const [selectedField, setSelectedField] = useState(null);
-    return <></>
+  const [selectedField, setSelectedField] = useState(null);
+  return (
+    <>
+    <div className="border p-4 rounded-lg">
+      <SelectField
+        selectedField={selectedField}
+        setSelectedField={setSelectedField}
+      />
+    </div>
+
+    </>
+  );
 }
- 

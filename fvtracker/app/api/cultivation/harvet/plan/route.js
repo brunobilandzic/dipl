964f7c395df0da5d4802d852b4830d8b25d11542
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-21 20:28:43 +0100

    post harvesting new plan route

---
+++ fvtracker/app/api/cultivation/harvet/plan/route.js
@@ -0,0 +1,64 @@
+import cultivation from "@/lib/cultivation";
+import dbConnect from "@/lib/db/mongooseConnect";
+
+export async function POST(request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+    const newHarvestingPlan = await cultivation.plans.createHarvestingPlan({
+      harvestingPlanData: body,
+    });
+    await newHarvestingPlan.populate([
+      {
+        path: "items",
+        populate: [
+          {
+            path: "cropVariety",
+            populate: {
+              path: "cropType",
+            },
+          },
+          {
+            path: "plantedCropVarieties",
+            populate: {
+              path: "cultivation",
+              select: "name",
+            },
+          },
+        ],
+      },
+      {
+        path: "field",
+        select: "name _id",
+      },
+    ]);
+    console.log("Created new harvesting plan:", newHarvestingPlan);
+    return Response.json({ newHarvestingPlan }, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: error.message }, { status: 500 });
+  }
+}
+
+export async function DELETE(request) {
+  try {
+    await dbConnect();
+
+    let fieldId, planId;
+    try {
+      const body = await request.json();
+      ({ fieldId, planId } = body);
+    } catch {
+      // no body - delete all
+    }
+
+    await cultivation.plans.deleteHarvestingPlans({ fieldId, planId });
+    return Response.json(
+      { message: "Harvesting plans deleted" },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: error.message }, { status: 500 });
+  }
+}

bf8bfb3378424537357b393b5f72d7f3dcd1ae8f
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-21 14:46:28 +0100

    select all from planting plans

--- fvtracker/app/api/cultivation/fields/route.js
+++ fvtracker/app/api/cultivation/fields/route.js
@@ -45,7 +45,6 @@ export async function GET(request) {
         },
         {
           path: "plantingPlans",
-          select: "field items slug",
           populate: [
             {
               path: "items",

281dfa652568f25ae5b9cb27be07ed5998c5df89
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-21 11:35:08 +0100

    harvesting and planingplan models structure

---
+++ fvtracker/models/documents/plans/HarvestingPlan.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import { Field } from "@/models/sectors/cultivation/Field";
+import { makeUrlFriendly } from "@/lib/utils/strings";
+
+const { Schema } = mongoose;
+
+const harvestingItemSchema = new Schema({
+  cropVariety: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "CropVariety",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    default: 0,
+  },
+  plantedCropVarieties: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "PlantedCropVariety",
+      default: [],
+    },
+  ],
+  harvestingPlan: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "HarvestingPlan",
+    required: true,
+  },
+});
+
+const harvestingPlanSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  field: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Field",
+    required: true,
+  },
+  items: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "HarvestingPlanItem",
+      default: [],
+    },
+  ],
+  plannedharvestingDate: {
+    type: Date,
+    default: null,
+  },
+  plannedHarvestingDate: {
+    type: Date,
+    default: null,
+  },
+  slug: { type: String, unique: true, index: true },
+});
+
+harvestingPlanSchema.pre("save", async function () {
+  if (this.isModified("name")) {
+    const field = await Field.findById(this.field);
+    this.slug = makeUrlFriendly(`${field?.name}-${this.name}`);
+  }
+});
+
+export const HarvestingPlan =
+  mongoose.models.HarvestingPlan ||
+  mongoose.model("HarvestingPlan", harvestingPlanSchema);
+
+export const HarvestingPlanItem =
+  mongoose.models.HarvestingPlanItem ||
+  mongoose.model("HarvestingPlanItem", harvestingItemSchema);
---
+++ fvtracker/models/documents/plans/index.js
@@ -0,0 +1,9 @@
+import { PlantingPlan } from "./PlantingPlan";
+import { HarvestingPlan } from "./HarvestingPlan";
+
+export default {
+  cultivation: {
+    PlantingPlan,
+    HarvestingPlan,
+  },
+};

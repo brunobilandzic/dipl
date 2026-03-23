1f28460cc5fbe41d4cfc06808bfa8819d779750a
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-23 20:44:21 +0100

    errors translate cro

--- fvtracker/lib/constants/errors/cultivation/dimensions/index.js
+++ fvtracker/lib/constants/errors/cultivation/dimensions/index.js
@@ -1,7 +1,8 @@
 export default {
   CULTIVATION_AREA_DIMENSIONS: (cultivationAreaDimenions) =>
-    `Cultivation area dimensions must be at least ${cultivationAreaDimenions.min_ca_dim}x${cultivationAreaDimenions.min_ca_dim} and at most ${cultivationAreaDimenions.max_ca_dim}x${cultivationAreaDimenions.max_ca_dim}`,
-  CULTIVATION_AREA_OVERLAP: "Cultivation areas cannot overlap",
-  CULTIVATION_AREA_GAP: (gap) => `Cultivation areas must have a gap of at least ${gap} cells between them`,
-  CULTIVATION_OVERLAP: "Cultivations cannot overlap",
+    `Dimenzije površine uzgoja moraju biti najmanje ${cultivationAreaDimenions.min_ca_dim}x${cultivationAreaDimenions.min_ca_dim} i najviše ${cultivationAreaDimenions.max_ca_dim}x${cultivationAreaDimenions.max_ca_dim}`,
+  CULTIVATION_AREA_OVERLAP: "Površine uzgoja ne smiju se preklapati",
+  CULTIVATION_AREA_GAP: (gap) =>
+    `Površine uzgoja moraju imati razmak od najmanje ${gap} ćelija između njih`,
+  CULTIVATION_OVERLAP: "Uzgoji se ne smiju preklapati",
 };

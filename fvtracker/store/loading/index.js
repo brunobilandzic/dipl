e001c8f098e0333f6101fd8c67ca07b9a25a4a36
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-23 19:24:34 +0100

    loading state just track loading not error

--- fvtracker/store/loading/index.js
+++ fvtracker/store/loading/index.js
@@ -1,25 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    isLoading: false,
-    error: null,
+  isLoading: false,
 };
 
 const loadingSlice = createSlice({
-    name: 'loading',
-    initialState,
-    reducers: {
-        setLoading: (state, action) => {
-            console.log("Setting loading state to", action.payload);
-            state.isLoading = action.payload;
-        },
-        setError: (state, action) => {
-            state.error = action.payload;
-        },
-        clearError: (state) => {
-            state.error = null;
-        },
+  name: "loading",
+  initialState,
+  reducers: {
+    setLoading: (state, action) => {
+      console.log("Setting loading state to", action.payload);
+      state.isLoading = action.payload;
     },
+  },
 });
 
 export const { setLoading, setError, clearError } = loadingSlice.actions;

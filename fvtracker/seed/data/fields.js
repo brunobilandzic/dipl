e2682e5ca125a98d05780eebbabcccccf3947e5b
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-23 11:05:20 +0100

    planInfo constant for seed

--- fvtracker/seed/data/fields.js
+++ fvtracker/seed/data/fields.js
@@ -25,7 +25,6 @@ export const fieldParams = [
   },
 ];
 
-
 export const optimizedParams = {
   name: "SEEDED FIELD",
   description: "Field optimized for seeders testing",
@@ -39,6 +38,28 @@ export const optimizedParams = {
     latitude: 43.67028,
     longitude: 16.70472,
   },
+};
+
+const planInfo = {
+  plantingPlan: {
+    name: "Plan sadnje 1",
+    description: "Plan sadnje za testiranje",
+    items: [
+      {
+        cropVariety: null, // Postavit ćemo ovo kasnije
+        quantity: 100,
+      },
+    ],
+  },
+  harvestingPlan: {
+    name: "Plan berbe 1",
+    description: "Plan berbe za testiranje",
+    items: [
+      {
+        cropVariety: null, // Postavit ćemo ovo kasnije
+        quantity: 100,
+      },
+    ],
 };
 
 const optimizedParams2 = {
@@ -86,45 +107,42 @@ const optimizedParams4 = {
   },
 };
 
-export const optimizedParamsArray = [
-  optimizedParams,
-                   
-];
+export const optimizedParamsArray = [optimizedParams];
 
 const createFieldTimeSeconds = 5;
 
 export const createFieldTimeMs = createFieldTimeSeconds * 1000;
 
 export const cultivationAreaNamesConstant = [
-    {
-      name: "Polje Anita",
-      description: "natkriveno polje pogodno za sadanju voca",
-    },
-    { name: "Polje Marko", description: "otvoreno polje idealno za povrće" },
-    { name: "Polje Ivana", description: "sunčano polje za cvijeće" },
-    { name: "Polje Jure", description: "plodno polje za žitarice" },
-    { name: "Polje Lucija", description: "vlažno polje za jagode" },
-    { name: "Polje Petar", description: "pješčano polje za lavandu" },
-    { name: "Polje Ana", description: "stjenovito polje za masline" },
-    { name: "Polje Tomislav", description: "hladno polje za kupus" },
-    { name: "Polje Dora", description: "toplo polje za rajčice" },
-    { name: "Polje Luka", description: "plodno polje za krumpir" },
-    { name: "Polje Nika", description: "sunčano polje za bundeve" },
-    { name: "Polje Ivan", description: "vlažno polje za salatu" },
-    { name: "Polje Zora", description: "stjenovito polje za začinsko bilje" },
-    { name: "Polje Toni", description: "hladno polje za mrkvu" },
-    { name: "Polje Milena", description: "sunčano polje za papriku" },
-    { name: "Polje Jela", description: "vlažno polje za borovnice" },
-    { name: "Polje Zlatko", description: "pješčano polje za smokve" },
-    { name: "Polje Dino", description: "plodno polje za jagode" },
-    { name: "Polje Maja", description: "toplo polje za limun" },
-    { name: "Polje Stefan", description: "sunčano polje za maslinu" },
-    { name: "Polje Vesna", description: "vlažno polje za pirinač" },
-    { name: "Polje Dragan", description: "pješčano polje za jagnje" },
-    { name: "Polje Petra", description: "stjenovito polje za kapuciniju" },
-    { name: "Polje Goran", description: "hladno polje za ciklamu" },
-    { name: "Polje Ema", description: "plodno polje za blitvu" },
-    { name: "Polje Marija", description: "sunčano polje za rozmarin" },
-    { name: "Polje Filip", description: "vlažno polje za zelenu salaticu" },
-    { name: "Polje Katarina", description: "toplo polje za batat" },
+  {
+    name: "Polje Anita",
+    description: "natkriveno polje pogodno za sadanju voca",
+  },
+  { name: "Polje Marko", description: "otvoreno polje idealno za povrće" },
+  { name: "Polje Ivana", description: "sunčano polje za cvijeće" },
+  { name: "Polje Jure", description: "plodno polje za žitarice" },
+  { name: "Polje Lucija", description: "vlažno polje za jagode" },
+  { name: "Polje Petar", description: "pješčano polje za lavandu" },
+  { name: "Polje Ana", description: "stjenovito polje za masline" },
+  { name: "Polje Tomislav", description: "hladno polje za kupus" },
+  { name: "Polje Dora", description: "toplo polje za rajčice" },
+  { name: "Polje Luka", description: "plodno polje za krumpir" },
+  { name: "Polje Nika", description: "sunčano polje za bundeve" },
+  { name: "Polje Ivan", description: "vlažno polje za salatu" },
+  { name: "Polje Zora", description: "stjenovito polje za začinsko bilje" },
+  { name: "Polje Toni", description: "hladno polje za mrkvu" },
+  { name: "Polje Milena", description: "sunčano polje za papriku" },
+  { name: "Polje Jela", description: "vlažno polje za borovnice" },
+  { name: "Polje Zlatko", description: "pješčano polje za smokve" },
+  { name: "Polje Dino", description: "plodno polje za jagode" },
+  { name: "Polje Maja", description: "toplo polje za limun" },
+  { name: "Polje Stefan", description: "sunčano polje za maslinu" },
+  { name: "Polje Vesna", description: "vlažno polje za pirinač" },
+  { name: "Polje Dragan", description: "pješčano polje za jagnje" },
+  { name: "Polje Petra", description: "stjenovito polje za kapuciniju" },
+  { name: "Polje Goran", description: "hladno polje za ciklamu" },
+  { name: "Polje Ema", description: "plodno polje za blitvu" },
+  { name: "Polje Marija", description: "sunčano polje za rozmarin" },
+  { name: "Polje Filip", description: "vlažno polje za zelenu salaticu" },
+  { name: "Polje Katarina", description: "toplo polje za batat" },
 ];

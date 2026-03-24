d43d77bd8e0101fd2f0d548ce9f94487ba31011e
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-24 18:55:12 +0100

    harvestCultivation modal skeeleton

---
+++ fvtracker/components/cultivation/cultivationArea/harvesting/harvestCultivation.js
@@ -0,0 +1,17 @@
+export const HarvestCultivation = ({
+  isOpen,
+  onCancel,
+  onSubmit,
+  newNewHarvest,
+  setNewHarvest,
+  availablePlans,
+  crops,
+  onChoosePlan,
+  submitDisabled,
+}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onCancel}>
+      berba brate
+    </Modal>
+  );
+};

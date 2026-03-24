91333aa378cbd1b1703ac38e3d613870a692386a
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-24 12:10:13 +0100

    remoce hplcv click grom seeedijg

--- fvtracker/components/cultivation/cultivationArea/planting/seedingModal.js
+++ fvtracker/components/cultivation/cultivationArea/planting/seedingModal.js
@@ -307,7 +307,6 @@ export const SeedingModal = ({
               seedMode={true}
               toPlantCells={newPlantage?.toPlantCells}
               handleNotPlanted={handleNotPlanted}
-              hanleCropVarietyClick={hanleCropVarietyClick}
             />
           </div>
         </div>

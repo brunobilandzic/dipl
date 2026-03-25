7cfec29fc9f1e438a22ff57c5d3bf04f42c9df13
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-25 19:08:57 +0100

    route t get gen man

---
+++ fvtracker/app/api/(generalManager)/general-manager/route.js
@@ -0,0 +1,19 @@
+import auth from "@/lib/auth";
+import { GENERAL_MANAGER } from "@/lib/constants/users/managerTypes";
+import dbConnect from "@/lib/db/mongooseConnect";
+import { getGeneralManager } from "@/lib/generalManager";
+
+export async function GET() {
+  await dbConnect();
+  try {
+    await auth.session.specificManager({ managerName: GENERAL_MANAGER });
+    const generalManager = await getGeneralManager();
+    return Response.json({ generalManager }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { error: "Greška pri dohvaćanju Generalnog Managera" },
+      { status: 500 },
+    );
+  }
+}

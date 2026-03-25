7dc4d0ee0d30be3044362ced4491663e0eaead06
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-25 09:31:09 +0100

    role request model,

---
+++ fvtracker/models/documents/requests/RoleRequest.js
@@ -0,0 +1,32 @@
+const { Schema } = require("mongoose");
+
+const roleRequestSchema = new Schema({
+  generalManager: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "GeneralManager",
+    required: true,
+  },
+  requestManager: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "RootManager",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "approved", "rejected"],
+    default: "pending",
+  },
+});
+
+roleRequestSchema.pre("save", async function () {
+    const generalManager = await mongoose.models.GeneralManager.findById(this.generalManager);
+    if (!generalManager) {
+      throw new Error("General Manager not found for Role Request.");
+    }
+    generalManager.roleRequests.push(this._id);
+    await generalManager.save();
+})
+
+export const RoleRequest =
+  mongoose.models.RoleRequest ||
+  mongoose.model("RoleRequest", roleRequestSchema);

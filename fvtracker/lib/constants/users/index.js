1079cff841622d2f0ce7e6e7bf8f604951f71491
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-25 09:00:01 +0100

    role statuses constants

--- fvtracker/lib/constants/users/index.js
+++ fvtracker/lib/constants/users/index.js
@@ -1,9 +1,13 @@
-import {
-  usernameToModel,
-  modelToUsername,
-} from "./managersUsernameModel";
+import { usernameToModel, modelToUsername } from "./managersUsernameModel";
+
+const ROLE_STATUSES = {
+  PENDING: "PENDING",
+  APPROVED: "APPROVED",
+  REJECTED: "REJECTED",
+};
 
 export default {
   usernameToModel,
   modelToUsername,
+  ROLE_STATUSES,
 };

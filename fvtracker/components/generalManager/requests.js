54fe3531c7e833107e838d7ff61acb92b3752cba
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-25 20:03:08 +0100

    role req item

--- fvtracker/components/generalManager/requests.js
+++ fvtracker/components/generalManager/requests.js
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { useSelector } from "react-redux";
-import Link from "next/link";
 
 export const RoleRequestList = () => {
   const generalManager = useSelector((state) => state.generalManager.manager);
@@ -27,14 +26,42 @@ const RoleRequestItem = ({ roleRequest }) => {
   const { managerModelName: requestedRoleName } = roleRequest.rootManager;
   return (
     <>
-      <Link href={`/menadzeri/zahtjevi/${roleRequest._id}`}>
-        <div className="p-4 border rounded-md shadow-md flex flex-col gap-2 relative">
+      <div className="flex justify-between items-center mb-2 p-4 border rounded-md shadow-md">
+        <div className="flex flex-col gap-2 ">
           <div>
             {name} {surname}
           </div>
           <div>Zahtjev za ulogu: {requestedRoleName}</div>
         </div>
-      </Link>
+        <div>
+          <RoleRequestStatus status={roleRequest.status} />
+        </div>
+      </div>
     </>
+  );
+};
+
+const RoleRequestStatus = ({ status }) => {
+  let statusBackground = "bg-gray-500";
+  let statusColor = "text-white";
+  switch (status) {
+    case "approved":
+      statusBackground = "bg-green-500";
+      break;
+    case "rejected":
+      statusBackground = "bg-red-500";
+      break;
+    case "pending":
+      statusBackground = "bg-yellow-500";
+      break;
+    default:
+      break;
+  }
+  return (
+    <span
+      className={`text-white w-20 text-center px-2 py-2 rounded-lg ${statusBackground} text-sm font-semibold ${statusColor}`}
+    >
+      {status}
+    </span>
   );
 };

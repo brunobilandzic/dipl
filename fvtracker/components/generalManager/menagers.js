d25ade52a19709996074e4d2d95ba506eda19d63
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-26 20:48:53 +0100

    everything readxy for populated managers

--- fvtracker/components/generalManager/menagers.js
+++ fvtracker/components/generalManager/menagers.js
@@ -1,9 +1,45 @@
 "use client";
 
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { LoadingFullScreen } from "../layout/loading";
+import { v4 as uuid } from "uuid";
 
 export const ManagerList = () => {
   const generalManager = useSelector((state) => state.generalManager?.manager);
 
-  return <>{JSON.stringify(generalManager, null, 2)}</>;
+  useEffect(() => {
+    console.log("General Manager:", generalManager);
+    if (!generalManager)
+      console.warn("General Manager data is not available in the Redux store.");
+  }, [generalManager]);
+
+  if (!generalManager) {
+    return <LoadingFullScreen />;
+  }
+
+  return (
+    <>
+      {JSON.stringify(generalManager, null, 2)}
+      <div>
+        {generalManager.managers.map((manager) => (
+          <ManagerListItem key={uuid()} manager={manager} />
+        ))}
+      </div>
+      sssssss
+    </>
+  );
+};
+
+const ManagerListItem = ({ manager }) => {
+  return (
+    <>
+      <div className="border rounded-lg p-4 flex justify-between items-center">
+        <div className="flex flex-col">
+          <h3 className="font-bold"></h3>
+          <p className="text-sm text-gray-500"></p>
+        </div>
+      </div>
+    </>
+  );
 };

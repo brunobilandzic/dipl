9881b28303abc712af902c7dfcc9e16989219679
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-26 11:10:03 +0100

    change role request status put route

---
+++ fvtracker/app/api/(generalManager)/general-manager/role-requests/route.js
@@ -0,0 +1,24 @@
+import dbConnect from "@/lib/db/mongooseConnect";
+import { RoleRequest } from "@/models/documents/requests/RoleRequest";
+
+export async function PUT(req) {
+  // do not mix req response with Next.js Response object
+  try {
+    await dbConnect();
+    const { requestId, response } = await req.json();
+    await RoleRequest.updateOne(
+      { _id: requestId },
+      { status: response, respondedAt: new Date() },
+    );
+    return Response.json(
+      { message: `Role request status updated to ${response}` },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Error processing role request:", error);
+    return Response.json(
+      { error: "Error processing role request." },
+      { status: 500 },
+    );
+  }
+}

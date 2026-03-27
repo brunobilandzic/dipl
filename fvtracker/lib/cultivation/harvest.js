40d18feeed58fc962d5874550fbf01657c7b3964
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-27 13:47:02 +0100

    get harv plan by id

--- fvtracker/lib/cultivation/harvest.js
+++ fvtracker/lib/cultivation/harvest.js
@@ -1,4 +1,12 @@
+import { HarvestingPlan } from "@/models/documents/plans/HarvestingPlan";
 import { getCultivationById } from "./cultivation";
+export async function getHarvestingPlanById(id) {
+  const harvestingPlan = await HarvestingPlan.findById(id);
+  if (!harvestingPlan) {
+    throw new Error("Harvesting plan not found.");
+  }
+  return harvestingPlan;
+}
 
 export async function harvestRelativeCells({
     cultivationId,

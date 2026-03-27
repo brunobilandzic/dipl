7ec56de59b3151f9495a052b262c1c91ad33b38d
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-27 13:49:33 +0100

    get planted crop varieaties with plan item with that cv

--- fvtracker/lib/cultivation/harvest.js
+++ fvtracker/lib/cultivation/harvest.js
@@ -1,5 +1,7 @@
 import { HarvestingPlan } from "@/models/documents/plans/HarvestingPlan";
 import { getCultivationById } from "./cultivation";
+import { PlantedCropVariety } from "@/models/sectors/cultivation/Crops";
+
 export async function getHarvestingPlanById(id) {
   const harvestingPlan = await HarvestingPlan.findById(id);
   if (!harvestingPlan) {
@@ -14,4 +16,16 @@ export async function harvestCells({
   toHarvestCells,
   harvestingPlanId,
 }) {
+  let plantedCropVarieties = await PlantedCropVariety.find({
+    cultivation: cultivationId,
+  }).populate("plantingPlanItem");
+  
+  plantedCropVarieties = plantedCropVarieties.filter(
+    (pcv) => pcv.plantingPlanItem?.cropVariety.toString() === cropVarietyId,
+  );
+  const harvestingPlan = await getHarvestingPlanById(harvestingPlanId);
+
+  console.log({ plantedCropVarieties, harvestingPlan, toHarvestCells });
+}
+
 const harvestCell = async ({ plantingPlanItem, relativeCoords }) => {};

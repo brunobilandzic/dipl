28b87c312a1b0c4621fe8d4bc92ceebf33203a03
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-27 13:46:15 +0100

    call harvest cells from route

--- fvtracker/app/api/cultivation/harvest/new-harvest/route.js
+++ fvtracker/app/api/cultivation/harvest/new-harvest/route.js
@@ -1,3 +1,18 @@
+import cultivation from "@/lib/cultivation";
+import { harvestCells } from "@/lib/cultivation/harvest";
+
 export async function POST(req) {
-  return Response.json({ message: "New harvest created" }, { status: 201 });
+  try {
+    const body = await req.json();
+    const newHarvest = body;
+    console.log({ newHarvest });
+    await harvestCells(newHarvest);
+    return Response.json({ message: "New harvest created" }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating new harvest:", error);
+    return Response.json(
+      { message: "Error creating new harvest" },
+      { status: 500 },
+    );
+  }
 }

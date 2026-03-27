1161e1cdb2767779e78b6ae71f1a1b88cdb354f0
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-27 09:24:08 +0100

    role status and statistics

--- fvtracker/components/generalManager/menagers.js
+++ fvtracker/components/generalManager/menagers.js
@@ -2,8 +2,9 @@
 
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { LoadingFullScreen } from "../layout/loading";
+import { Loading, LoadingFullScreen } from "../layout/loading";
 import { v4 as uuid } from "uuid";
+import { RoleRequestStatus } from "./requests";
 
 export const ManagerList = () => {
   const generalManager = useSelector((state) => state.generalManager?.manager);
@@ -26,7 +27,6 @@ export const ManagerList = () => {
           <ManagerListItem key={uuid()} manager={manager} />
         ))}
       </div>
-      sssssss
     </>
   );
 };
@@ -47,7 +47,32 @@ const ManagerListItem = ({ manager }) => {
             <p className="text-sm text-gray-500">{manager.appUser.email}</p>
             <p className="text-sm text-gray-500">{manager.managerModelName}</p>
           </div>
+          <ManagerStatistics manager={manager} />
         </div>
+        <RoleRequestStatus roleRequest={manager.roleRequest} />
+      </div>
+    </>
+  );
+};
+
+const ManagerStatistics = ({ manager }) => {
+  switch (manager.managerModelName) {
+    case "CultivationManager":
+      return (
+        <CultivationManagerStatistics
+          cultivationManager={manager.specificManager}
+        />
+      );
+    default:
+      return null;
+  }
+};
+
+export const CultivationManagerStatistics = ({ cultivationManager }) => {
+  return (
+    <>
+      <div>
+        <div>Fields created: {cultivationManager.fields.length}</div>
       </div>
     </>
   );

73df5b505514876de5a9751f88cebfeeadef7bb0
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-29 18:28:16 +0200

    make ICEBERG varuety plkans seed

--- fvtracker/seed/cultivation/fields/create/createFields.js
+++ fvtracker/seed/cultivation/fields/create/createFields.js
@@ -155,7 +155,7 @@ export async function createFieldRecord(fieldObject) {
   cultivationManager.fields.push(fieldRecord._id);
   fieldRecord.manager = cultivationManager._id;
 
-  const cropVariety = await CropVariety.findOne({ name: "Idared" });
+  const cropVariety = await CropVariety.findOne({ name: "Iceberg" });
 
   await cultivationManager.save();
   await fieldRecord.save();

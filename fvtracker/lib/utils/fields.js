a5148c18e552cd569da60630396c7126e57dbb0e
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-30 12:20:32 +0200

    in fields util fetcher func set loaging

--- fvtracker/lib/utils/fields.js
+++ fvtracker/lib/utils/fields.js
@@ -1,6 +1,7 @@
 import api from "@/lib/api";
 import handleError from "@/lib/constants/errors/client/handleError";
 import { setFields } from "@/store/cultivation";
+import { setLoading } from "@/store/loading";
 
 export const extractCoords = (cell) => {
   const [width, length] = cell.split(",").map(Number);
@@ -9,11 +10,14 @@ export const extractCoords = (cell) => {
 
 export const refreshFields = async ({ dispatch, router }) => {
   try {
+    dispatch(setLoading(true));
     const res = await api.get("/cultivation/fields");
     if (res.data && res.data.fields && Array.isArray(res.data.fields)) {
       dispatch(setFields(res.data.fields));
     }
+    dispatch(setLoading(false));
   } catch (error) {
+    dispatch(setLoading(false));
     if (error.response?.status === 403) {
       router.push("/uloga-nije-odobrena");
       return;

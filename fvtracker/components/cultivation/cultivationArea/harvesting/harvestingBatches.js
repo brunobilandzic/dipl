1c8fc48ab5f46235f9afe2e0f67322fc3eb66571
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-30 12:29:53 +0200

    get fields from redux in batches

--- fvtracker/components/cultivation/cultivationArea/harvesting/harvestingBatches.js
+++ fvtracker/components/cultivation/cultivationArea/harvesting/harvestingBatches.js
@@ -1,7 +1,13 @@
 "use client";
+import { LoadingFullScreen } from "@/components/layout/loading";
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function HarvestingBatches() {
+  const fields = useSelector((state) => state.cultivation.fields);
+
+  if (!fields) return <LoadingFullScreen />;
+
   return (
     <div>
       <h1>Harvesting Batches</h1>

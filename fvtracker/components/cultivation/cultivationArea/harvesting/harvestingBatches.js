7bdc0f00dbb00adc447480ab918cd44e11e5e312
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-30 13:00:49 +0200

    set harvesting abtches

--- fvtracker/components/cultivation/cultivationArea/harvesting/harvestingBatches.js
+++ fvtracker/components/cultivation/cultivationArea/harvesting/harvestingBatches.js
@@ -1,10 +1,18 @@
 "use client";
 import { LoadingFullScreen } from "@/components/layout/loading";
-import React from "react";
+import { getHarvestingBatches } from "@/lib/utils/harvest";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 export default function HarvestingBatches() {
   const fields = useSelector((state) => state.cultivation.fields);
+  const [harvestingBatches, setHarvestingBatches] = useState(null);
+
+  // set batches based on fields data
+  useEffect(() => {
+    if (!fields) return;
+    setHarvestingBatches(getHarvestingBatches({ fields }));
+  }, [fields]);
 
   if (!fields) return <LoadingFullScreen />;
 

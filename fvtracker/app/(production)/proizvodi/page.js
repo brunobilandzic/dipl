b993a8c72352499110f34e2cd7e7ce807ef38716
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-30 20:31:31 +0200

    product list page skeleton

---
+++ fvtracker/app/(production)/proizvodi/page.js
@@ -0,0 +1,12 @@
+import ProductList from "@/components/production/products/list";
+import React from "react";
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <ProductList />
+    </div>
+  );
+};
+
+export default ProductsPage;
---
+++ fvtracker/components/production/products/list.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <div>
+      <div className="font-bold text-3xl border-b-2">Lista proizvoda</div>
+    </div>
+  );
+};
+
+export default ProductList;

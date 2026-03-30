f2bb70b9134eb7ed292ce3d35b288b94f660d493
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-30 13:04:44 +0200

    display length if got hbatch

--- fvtracker/components/cultivation/cultivationArea/harvesting/harvestingBatches.js
+++ fvtracker/components/cultivation/cultivationArea/harvesting/harvestingBatches.js
@@ -19,6 +19,7 @@ export default function HarvestingBatches() {
   return (
     <div>
       <h1>Harvesting Batches</h1>
+      {harvestingBatches?.length}
       {/* Add your harvesting batches content here */}
     </div>
   );

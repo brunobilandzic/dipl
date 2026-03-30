7e1f8252f4d966b576e558ea17ef69e02c024bc8
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-30 11:12:12 +0200

    send all if gen man

--- fvtracker/app/api/cultivation/harvest/batches/route.js
+++ fvtracker/app/api/cultivation/harvest/batches/route.js
@@ -1,12 +1,21 @@
+import { GENERAL_MANAGER } from "@/lib/constants/users/managerTypes";
 import {
   getHarvestingBatches,
   populateBatches,
 } from "@/lib/cultivation/harvest";
+import { HarvestingBatch } from "@/models/sectors/interface/HarvestingBatch";
 
 export async function GET(req) {
   try {
     const { searchParams } = new URL(request.url);
     const managerName = searchParams.get("managerName");
+    if (managerName === GENERAL_MANAGER) {
+      let harvestingBatches = await HarvestingBatch.find();
+      harvestingBatches = await populateBatches({ harvestingBatches });
+      return Response.json({ harvestingBatches }, { status: 200 });
+    }
+    const harvestingBatches = await getHarvestingBatches({ managerName });
+    return Response.json({ harvestingBatches }, { status: 200 });
   } catch (error) {
     console.error("Error fetching harvesting batches:", error);
     return Response.json(

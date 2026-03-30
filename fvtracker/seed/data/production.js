57839f6a37fc311dc0d26eb69c3178b57ea46f5c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-30 15:07:53 +0200

    define product seed constants

---
+++ fvtracker/seed/data/production.js
@@ -0,0 +1,10 @@
+export const products = [
+  {
+    name: "Cherry rajčica Paket 10 kom",
+    cropVarieties: [{ name: "Cherry rajčica", cellProduces: 10 }],
+  },
+  {
+    name: "Kuhani špinat konzerva 400g",
+    cropVarieties: [{ name: "Špinat", quantity: 1, cellProduces: 5 }],
+  },
+];

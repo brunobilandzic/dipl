134a7fcf0b50bd89c1c38b799f84c194e5dbffa5
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-30 12:10:44 +0200

    handle error navigatte to not approved

--- fvtracker/lib/constants/errors/client/handleError.js
+++ fvtracker/lib/constants/errors/client/handleError.js
@@ -2,18 +2,21 @@ import { handleApiError } from "./api";
 
 export default function handleError(error) {
   if (error.name === "AxiosError") {
-    handleApiError({
-      ...error, 
+    return handleApiError({
+      ...error,
     });
-  } else {
-    console.error("Unexpected error:", error);
-    const errorMessage =
-      error.response?.data?.message ||
-      error.response?.data?.error ||
-      error.message ||
-      error.name ||
-      "Nepoznata greška";
-
-    alert(`${error.generalMessage}: ${errorMessage}`);
   }
+  if (error.response?.status === 403) {
+    router.push("/uloga-nije-odobrena");
+    return;
+  }
+  console.error("Unexpected error:", error);
+  const errorMessage =
+    error.response?.data?.message ||
+    error.response?.data?.error ||
+    error.message ||
+    error.name ||
+    "Nepoznata greška";
+
+  alert(`${error.generalMessage}: ${errorMessage}`);
 }

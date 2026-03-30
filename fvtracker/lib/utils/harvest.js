d07b63163708cbad655761203e799172bb2f7ec7
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-30 11:49:31 +0200

    harvesting batches refresh util func

--- fvtracker/lib/utils/harvest.js
+++ fvtracker/lib/utils/harvest.js
@@ -1,3 +1,5 @@
+import api from "@/lib/api";
+
 export const getHarvestCellsRect = ({
   beginX,
   beginY,
@@ -28,4 +30,16 @@ export const getHarvestCellsRect = ({
   return rectCells;
 };
 
-export async function refreshHarvestingBatches({ dispatch, router }) {}
+export async function refreshHarvestingBatches({ dispatch, router }) {
+  try {
+    const res = await api.get("/cultivation/harvest/batches");
+    if(res.data && res.data.harvestingBatches && Array.isArray(res.data.harvestingBatches)) {
+      dispatch(setHarvestingBatches(res.data.harvestingBatches));
+    }
+  } catch (error) {
+    handleError({
+      ...error,
+      generalMessage: "Došlo je do greške prilikom učitavanja serpantina.",
+    });
+  }
+}

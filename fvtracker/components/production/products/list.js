a3674bf91ddde9c12455fd98a3112389b85bf06a
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-03-31 11:12:31 +0200

    product list fetches from redux checks

--- fvtracker/components/production/products/list.js
+++ fvtracker/components/production/products/list.js
@@ -1,9 +1,18 @@
+"use client";
+
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ProductList = () => {
+  const products = useSelector((state) => state.products.items);
   return (
     <div>
       <div className="font-bold text-3xl border-b-2">Lista proizvoda</div>
+      <ul>
+        {products?.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };

ad57a42fbc54113d492211ac5ba2854f4fec2fb9
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-01 17:58:31 +0200

    refresh resource batches for production

--- fvtracker/lib/utils/harvestingBatches.js
+++ fvtracker/lib/utils/harvestingBatches.js
@@ -1,3 +1,6 @@
+import api from "../api";
+import handleError from "../constants/errors/client/handleError";
+
 export const harvestingBatchItemData = ({ batchItem }) => {
   const quantityPerCell = batchItem.cropVariety?.quantityPerCell || 0;
 
@@ -5,4 +8,20 @@ export const harvestingBatchItemData = ({ batchItem }) => {
   const plcvCount = batchItem.plantedCropVarieties?.length || 0;
   const quantity = quantityPerCell * plcvCount;
   return { quantity, cropVarietyName, plcvCount };
+};
+
+export const refreshResourceBatches = async ({ dispatch, router }) => {
+  try {
+    const res = await api.get("/harvest/batches");
+    return res.data.harvestingBatches;
+  } catch (error) {
+    console.error(error);
+    handleError(
+      {
+        ...error,
+        generalMessage: "Greška prilikom osvježavanja žetvenih serija",
+      },
+      router,
+    );
+  }
 };

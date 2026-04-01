38247665ea4750ca8e3e2938f1388e829b6a4396
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-01 18:45:58 +0200

    refresh prod mans util fgunc

---
+++ fvtracker/lib/utils/production/managers.js
@@ -0,0 +1,19 @@
+import api from "@/lib/api";
+import { setManagers } from "@/store/production/products";
+
+export const refreshManagers = async ({ dispatch, router }) => {
+  try {
+    const res = await api.get("/production-manager", { params: { all: true } });
+    dispatch(setManagers(res.data.productionManagers));
+  } catch (error) {
+    console.error("Error fetching production managers:", error);
+    handleError(
+      {
+        ...error,
+        generalMessage:
+          "Failed to fetch production managers. Please try again later.",
+      },
+      router,
+    );
+  }
+};

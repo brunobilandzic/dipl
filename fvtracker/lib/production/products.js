9c92fb6444ddca0cc85e09fcc36ada8e8ccbe367
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-02 14:11:31 +0200

    lib get prod by slug

--- fvtracker/lib/production/products.js
+++ fvtracker/lib/production/products.js
@@ -1,15 +1,29 @@
 import { Product } from "@/models/sectors/production/Products";
 
-export const getProducts = async () => {      
-    const products = await Product.find().populate([
-        {
-            path: "ingredients",
-            select: "cropVariety quantity",
-            populate: {
-                path: "cropVariety",
-                select: "name",
-            },
-        },
-    ]);
-    return products;
+export const getProducts = async () => {
+  const products = await Product.find().populate([
+    {
+      path: "ingredients",
+      select: "cropVariety quantity",
+      populate: {
+        path: "cropVariety",
+        select: "name",
+      },
+    },
+  ]);
+  return products;
+};
+
+export const getProductBySlug = async (slug) => {
+  const product = await Product.findOne({ slug }).populate([
+    {
+      path: "ingredients",
+      select: "cropVariety quantity",
+      populate: {
+        path: "cropVariety",
+        select: "name",
+      },
+    },
+  ]);
+  return product;
 };

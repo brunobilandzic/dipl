cfd1d165c1860b15e58402a4f9f9f8ad775215e2
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-02 12:07:37 +0200

    refresh products set loading

--- fvtracker/lib/utils/production/products.js
+++ fvtracker/lib/utils/production/products.js
@@ -1,14 +1,18 @@
 import api from "@/lib/api";
 import handleError from "@/lib/constants/errors/client/handleError";
+import { setLoading } from "@/store/loading";
 import { setProducts } from "@/store/production/products";
 
 export const refreshProducts = async ({ dispatch, router }) => {
   try {
+    dispatch(setLoading(true));
     const res = await api.get("/products");
     dispatch(setProducts(res.data.products));
     console.log("Products refreshed successfully:");
+    dispatch(setLoading(false));
   } catch (error) {
     console.error("Error fetching products:", error);
+    dispatch(setLoading(false));
     handleError(
       {
         ...error,

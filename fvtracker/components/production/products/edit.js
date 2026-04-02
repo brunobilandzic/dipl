f337664abe831b0afae751c747e8ed4716aa3848
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-02 13:46:24 +0200

    edit product page will use for create

---
+++ fvtracker/components/production/products/edit.js
@@ -0,0 +1,35 @@
+export const EditProduct = ({ product }) => {
+  const initialFormData = initialData({ product });
+
+  return (
+    <div>
+      <h2>Uredi proizvod</h2>
+      <pre>{JSON.stringify(initialFormData, null, 2)}</pre>
+    </div>
+  );
+};
+
+const initialData = ({ product }) => {
+  if (!product) return testProduct;
+  return {
+    name: product.name,
+    description: product.description,
+    price: product.price,
+    ingredients: product.ingredients.map((ing) => ({
+      cropVarietyName: ing.cropVariety.name,
+      quantity: ing.quantity,
+    })),
+  };
+};
+
+const testProduct = {
+  name: "Proizvod 1",
+  description: "Opis proizvoda 1",
+  price: 10,
+  ingredients: [
+    {
+      cropVarietyName: "Cherry rajčica",
+      quantity: 10,
+    },
+  ],
+};

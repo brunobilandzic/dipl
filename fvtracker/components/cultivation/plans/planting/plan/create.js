1a9fb551464fa69516be812a7356b34672025b2e
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-05 19:50:08 +0200

    prettify

--- fvtracker/components/cultivation/plans/planting/plan/create.js
+++ fvtracker/components/cultivation/plans/planting/plan/create.js
@@ -120,7 +120,11 @@ export const SelectField = ({
   );
 };
 
-export const FillPlanInfo = ({ selectedField, plant = true, itemsName="items" }) => {
+export const FillPlanInfo = ({
+  selectedField,
+  plant = true,
+  itemsName = "items",
+}) => {
   const crops = useSelector((state) => state.cultivation.crops);
   const {
     generalTypes = [],

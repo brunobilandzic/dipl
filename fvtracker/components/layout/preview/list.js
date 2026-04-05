45bc1059a71d5f47eeea591589ac57afeb23df05
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-05 18:56:36 +0200

    preview list unique delete addd

--- fvtracker/components/layout/preview/list.js
+++ fvtracker/components/layout/preview/list.js
@@ -1,6 +1,21 @@
-export function List({ children }) {
+export function List({ children, title, onDeleteList, onCreateItem }) {
   return (
     <>
+      <div className="list-header flex items-center justify-between mb-2 border-b-2 pb-1">
+        <div className="text-2xl font-bolder">{title}</div>
+        <div className="flex gap-2">
+          {onCreateItem && (
+            <div className="btn submitButton" onClick={onCreateItem}>
+              Dodaj
+            </div>
+          )}
+          {onDeleteList && (
+            <div className="btn cancelButton" onClick={onDeleteList}>
+              Obriši sve
+            </div>
+          )}
+        </div>
+      </div>
       <div className="flex flex-col gap-4 justify-start">{children}</div>
     </>
   );

93ef0670c9d807b385c5c10fa24b7b81f8a8cab7
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-06 15:03:22 +0200

    defaultSortOptions

--- fvtracker/components/layout/preview/sort.js
+++ fvtracker/components/layout/preview/sort.js
@@ -1,6 +1,10 @@
 import { AppSelect } from "@/components/form/inputs";
 
-export const SortList = ({ sortBy, setSortBy, sortOptions }) => {
+export const SortList = ({
+  sortBy,
+  setSortBy,
+  sortOptions = defaultSortOptions,
+}) => {
   return (
     <div className="flex items-center gap-2">
       <span className="text-sm font-medium">Sortiraj po:</span>
@@ -12,3 +16,12 @@ export const SortList = ({ sortBy, setSortBy, sortOptions }) => {
     </div>
   );
 };
+
+const defaultSortOptions = [
+  { value: "fieldNameAsc", label: "Naziv polja A-Z" },
+  { value: "fieldNameDesc", label: "Naziv polja Z-A" },
+  { value: "priceAsc", label: "Cijena rastuće" },
+  { value: "priceDesc", label: "Cijena padajuće" },
+  { value: "newest", label: "Najnovije" },
+  { value: "oldest", label: "Najstarije" },
+];

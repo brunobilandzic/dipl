36c2ad7543755721d21575d6cab8521223d631c6
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-06 15:20:10 +0200

    edit filter layout comp

--- fvtracker/components/layout/preview/filter.js
+++ fvtracker/components/layout/preview/filter.js
@@ -1,48 +1,35 @@
 import { AppInput } from "@/components/form/inputs";
 import { v4 as uuid } from "uuid";
 
-export const Filter = ({}) => {
+export const Filter = ({ filters, setFilters, onApply }) => {
   return (
     <>
-      <div>{JSON.stringify(filterData, null, 2)}</div>
-      {options.map((option, index) => {
-        let InputComponent;
-        switch (option.filter) {
-          case "stringContains":
-            InputComponent = FieldStringContains;
-            break;
-          default:
-            InputComponent = AppInput;
-        }
-        return (
-          <div key={option.name} className="mb-4">
-            <InputComponent
-              option={option}
-              onChange={(e) => {
-                setFilterData((prev) => {
-                  return prev.map((o, i) => {
-                    if (i === index) {
-                      return { ...o, [option.name]: e.target.value };
-                    }
-                    return o;
-                  });
-                });
-              }}
-            />
-          </div>
-        );
-      })}
+      <div className="flex flex-col gap-2">
+        {filters.map((option, index) => {
+          switch (option.type) {
+            case "search":
+              return (
+                <AppInput
+                  key={index}
+                  value={option.value}
+                  onChange={(e) => {
+                    setFilters((prev) => {
+                      return prev.map((o, i) => {
+                        if (i === index) {
+                          return { ...o, value: e.target.value };
+                        }
+                        return o;
+                      });
+                    });
+                  }}
+                />
+              );
+          }
+        })}
+        <div className="btn btnSm" onClick={onApply}>
+          Primjeni
+        </div>
+      </div>
     </>
-  );
-};
-
-export const FieldStringContains = ({ option, onChange }) => {
-  return (
-    <AppInput
-      fieldName={option.field}
-      label={option.label}
-      name={option.field}
-      onChange={onChange}
-    />
   );
 };

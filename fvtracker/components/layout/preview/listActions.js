ce2718eaafd181099a0fe3921f013d1552c3d969
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-07 11:26:39 +0200

    list header action buttons

---
+++ fvtracker/components/layout/preview/listActions.js
@@ -0,0 +1,39 @@
+import { v4 as uuid } from "uuid";
+
+export const ListHeaderShowButton = ({ label, isOpen, setOpen }) => {
+  return (
+    <div
+      className={`btn btnSm ${isOpen ? "cancelButton" : "submitButton"}`}
+      onClick={() => setOpen(!isOpen)}
+    >
+      {label}
+    </div>
+  );
+};
+
+export const ActionOptions = ({ options }) => {
+  return (
+    <div className="flex justify-end gap-2 mt-4">
+      {options.map((option, index) => {
+        return (
+          <div
+            className={`btn btnSm ${option.className}`}
+            onClick={option.onClick}
+            key={uuid()}
+          >
+            {option.label}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export const DeleteListButton = ({ onDelete }) => {
+  console.log("onDelete:", onDelete);
+  return (
+    <div className="btn cancelButton btnSm" onClick={onDelete}>
+      Obriši sve
+    </div>
+  );
+};

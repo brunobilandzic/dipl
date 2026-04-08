56d5607d1894c05da69c1dd70e85a96b0ae3e6e8
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-08 10:40:05 +0200

    production varieties page declare

---
+++ fvtracker/app/(production)/(batches)/sirovine/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductionVarieties = () => {
+  return <div>Production Varieties</div>;
+};
+
+export default ProductionVarieties;
--- fvtracker/lib/utils/list.js
+++ fvtracker/lib/utils/list.js
@@ -31,7 +31,6 @@ export const initFilters = (listType) => {
           value: "",
         },
       ];
-    
   }
 };
 

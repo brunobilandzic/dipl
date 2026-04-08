6ca45e5896f229330d5aaad7ee355ea74e5679b8
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-08 19:06:36 +0200

    materials list flat map batch items

--- fvtracker/components/production/materials/list.js
+++ fvtracker/components/production/materials/list.js
@@ -2,6 +2,7 @@
 
 import { LoadingFullScreen } from "@/components/layout/loading";
 import { List, ListItem } from "@/components/layout/preview/list";
+import { materialsList } from "@/lib/utils/production/materials";
 import { refreshMaterials } from "@/store/production";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
@@ -10,6 +11,7 @@ export const MaterialsList = ({}) => {
   const materials = useSelector(
     (state) => state.production.materials.filteredItems,
   );
+  const harvestingBatchItems = materials ? materialsList({ materials }) : [];
   console.log({ materials });
   const dispatch = useDispatch();
   useEffect(() => {
--- fvtracker/lib/utils/production/materials.js
+++ fvtracker/lib/utils/production/materials.js
@@ -1,3 +1,6 @@
-export const createMaterialList = ({materials}) => {
-    
-}
+export const materialsList = ({ materials }) => {
+  const harvestingBatchItems = materials.flatMap(
+    (material) => material.harvestingBatchItems,
+  );
+  console.log({ harvestingBatchItems });
+};

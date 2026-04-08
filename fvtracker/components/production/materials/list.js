d69b2237ad48f3788bc2584b85a151f8499b081e
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-08 19:13:14 +0200

    cropVarietyBatchQuantities util helper to get resources

--- fvtracker/components/production/materials/list.js
+++ fvtracker/components/production/materials/list.js
@@ -2,7 +2,10 @@
 
 import { LoadingFullScreen } from "@/components/layout/loading";
 import { List, ListItem } from "@/components/layout/preview/list";
-import { materialsList } from "@/lib/utils/production/materials";
+import {
+  cropVarietyBatchQuantities,
+  materialsList,
+} from "@/lib/utils/production/materials";
 import { refreshMaterials } from "@/store/production";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
@@ -11,8 +14,8 @@ export const MaterialsList = ({}) => {
   const materials = useSelector(
     (state) => state.production.materials.filteredItems,
   );
-  const harvestingBatchItems = materials ? materialsList({ materials }) : [];
-  console.log({ materials });
+  const resources = materials ? cropVarietyBatchQuantities({ materials }) : [];
+  console.log({ resources });
   const dispatch = useDispatch();
   useEffect(() => {
     if (!materials) {
--- fvtracker/lib/utils/production/materials.js
+++ fvtracker/lib/utils/production/materials.js
@@ -1,6 +1,27 @@
-export const materialsList = ({ materials }) => {
+export const cropVarietyBatchQuantities = ({ materials }) => {
+  const cropVarietyBatchQuantities = [];
   const harvestingBatchItems = materials.flatMap(
     (material) => material.harvestingBatchItems,
   );
-  console.log({ harvestingBatchItems });
+  harvestingBatchItems.forEach((item) => {
+    const cropVarietyName = item.cropVariety.name;
+    const cropTypeName = item.cropVariety.cropType.name;
+    const batchQuantity = item.batchQuantity;
+    const existingEntry = cropVarietyBatchQuantities.find(
+      (entry) =>
+        entry.cropVarietyName === cropVarietyName &&
+        entry.cropTypeName === cropTypeName,
+    );
+    if (existingEntry) {
+      existingEntry.batchQuantity += batchQuantity;
+    } else {
+      cropVarietyBatchQuantities.push({
+        cropVarietyName,
+        cropTypeName,
+        batchQuantity,
+      });
+    }
+  });
+
+  console.log({ cropVarietyBatchQuantities });
 };

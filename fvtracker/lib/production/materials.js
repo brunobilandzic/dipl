af4f680d3bb9b9702aaaa91e69c9d327e9ae3bfe
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-08 17:30:55 +0200

    populate harvesting batch for production manager interests

--- fvtracker/lib/production/materials.js
+++ fvtracker/lib/production/materials.js
@@ -1,6 +1,22 @@
 import { HarvestingBatch } from "@/models/sectors/interface/HarvestingBatch";
 
 export const getMaterials = async () => {
-  const batches = await HarvestingBatch.find({});
+  const batches = await HarvestingBatch.find({}).populate([
+    {
+      path: "harvestingBatchItems",
+      populate: [
+        {
+          path: "cropVariety",
+          select: "name cropType plantedCropVarieties",
+          populate: [
+            {
+              path: "cropType",
+              select: "name",
+            },
+          ],
+        },
+      ],
+    },
+  ]);
   return batches;
 };

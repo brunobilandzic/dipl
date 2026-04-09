a2e5aea6842d503c1d239610e322351d9738b7e8
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-09 18:19:16 +0200

    createStock lib func

--- fvtracker/lib/production/stocks/index.js
+++ fvtracker/lib/production/stocks/index.js
@@ -1,7 +1,12 @@
+import { getProductById } from "../product";
+
 export const createProductStock = async ({
   productId,
   quantity,
   harvestingBatchId,
 }) => {
   // Implement the logic to create a product stock entry
+  const product = await getProductById(productId);
+  const stock = await product.createStock({ harvestingBatchId, quantity });
+  return stock;
 };

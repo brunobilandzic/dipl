168d113ab54c4bf5c5122f6da6410f77414a52d8
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-09 13:25:44 +0200

    move unauth check to handle error begin

--- fvtracker/lib/constants/errors/client/handleError.js
+++ fvtracker/lib/constants/errors/client/handleError.js
@@ -1,14 +1,15 @@
 import { handleApiError } from "./api";
 
 export default function handleError(error, router) {
+  if (error.response?.status === 403) {
+    console.log("redirecting\n\n");
+    router?.push("/uloga-nije-odobrena");
+    return;
+  }
   if (error.name === "AxiosError") {
     return handleApiError({
       ...error,
     });
-  }
-  if (error.response?.status === 403) {
-    router?.push("/uloga-nije-odobrena");
-    return;
   }
   console.error("Unexpected error:", error);
   const errorMessage =

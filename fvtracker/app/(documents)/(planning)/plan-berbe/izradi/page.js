a81cdc9e6d942f8aad2c23257eba2f7284f7a055
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-09 13:27:04 +0200

    check man to render unauth if !

--- fvtracker/app/(documents)/(planning)/plan-berbe/izradi/page.js
+++ fvtracker/app/(documents)/(planning)/plan-berbe/izradi/page.js
@@ -2,9 +2,18 @@ import CreateHarvestingPlanPageComponent from "@/components/cultivation/plans/ha
 import { fetchManager } from "@/lib/auth/fetchSessionData";
 import { CULTIVATION_MANAGER } from "@/lib/constants/users/managerTypes";
 import React from "react";
+import { UnathorizedPage } from "@/components/auth/unAuthorized";
 
 async function CreateHarvestingPlanPage() {
-  await fetchManager({ managerNames: [CULTIVATION_MANAGER] });
+  const { specificManager: cultivationManager } = await fetchManager({
+    managerNames: [CULTIVATION_MANAGER],
+    throwError: false,
+  });
+  if (!cultivationManager) {
+    return (
+      <UnathorizedPage message="Nemate pristup ovoj stranici. Prijavite se kao Menadžer Proizvodnje." />
+    );
+  }
   return (
     <div>
       <CreateHarvestingPlanPageComponent />

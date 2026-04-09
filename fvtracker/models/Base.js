49f7796dc713061ccb2c0bcba090f2c5beea6ee2
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-09 17:33:46 +0200

    missing import mngoose

---
+++ fvtracker/models/Base.js
@@ -0,0 +1,27 @@
+import { Schema } from "mongoose";
+import mongoose from "mongoose";
+
+const baseSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+baseSchema.pre("save", function () {
+  this.updatedAt = new Date();
+});
+
+export const Base = mongoose.models.Base || mongoose.model("Base", baseSchema);

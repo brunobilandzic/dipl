147fa9bf874eca9c8df0c66cf9aade0d252f4c87
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-09 13:28:53 +0200

    note refresh when from navabr

--- fvtracker/components/layout/navbar/index.js
+++ fvtracker/components/layout/navbar/index.js
@@ -63,7 +63,7 @@ function NavItems() {
         !fieldsRedux &&
         session.user?.roleStatus === ROLE_STATUSES.APPROVED
       ) {
-        console.log("refreshing fields for cultivation manager");
+        console.log("refreshing fields for cultivation manager from navbar");
         refreshFields({ dispatch, router });
       }
       if (managerModelName === "GeneralManager" && !generalManagerRedux) {

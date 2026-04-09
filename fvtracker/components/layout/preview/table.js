41ca6b1bb7490202219bca399b63614d4e8bab73
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-09 11:13:39 +0200

    table no rows case

--- fvtracker/components/layout/preview/table.js
+++ fvtracker/components/layout/preview/table.js
@@ -1,26 +1,45 @@
+import { PiEmpty } from "react-icons/pi";
+
 export const AppTable = ({ headerItems, rows }) => {
+  const numCols = headerItems ? headerItems.length : 0;
   return (
-    <table className="w-full border-collapse">
-      <thead>
-        <tr>
-          {headerItems?.map((item, index) => (
-            <th key={index} className="border p-2 text-left">
-              {item.label}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {rows?.map((row, rowIndex) => (
-          <tr key={rowIndex} className="border-t">
-            {row.map((cell, cellIndex) => (
-              <td key={cellIndex} className="border p-2">
-                {cell}
-              </td>
+    <>
+      {rows?.length > 0 ? (
+        <table className="w-full border-collapse">
+          <thead>
+            <tr>
+              {headerItems?.map((item, index) => (
+                <th key={index} className="">
+                  {item.label}
+                </th>
+              ))}
+            </tr>
+          </thead>
+
+          <tbody>
+            {rows.map((row, rowIndex) => (
+              <tr key={rowIndex} className="border-t">
+                {row.map((cell, cellIndex) => (
+                  <td key={cellIndex} className="">
+                    {cell}
+                  </td>
+                ))}
+              </tr>
             ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+          </tbody>
+        </table>
+      ) : (
+        <NoTableRows numCols={numCols} />
+      )}
+    </>
+  );
+};
+
+const NoTableRows = ({ numCols }) => {
+  return (
+    <div className="flex items-center justify-center gap-2 text-gray-500 text-2xl py-4">
+      <PiEmpty />
+      <span>Nema podataka</span>
+    </div>
   );
 };

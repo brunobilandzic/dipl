4b0a72d5bef18ca7905e83f60e21831dd6056d21
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-10 18:53:51 +0200

    select redux and refresh products in stock list

--- fvtracker/components/production/products/stock/list.js
+++ fvtracker/components/production/products/stock/list.js
@@ -1,6 +1,19 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useRouter } from "next/navigation";
+import { refreshProducts } from "@/lib/utils/production/products";
 
 const StockList = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const products = useSelector((state) => state.production.products.items);
+  useEffect(() => {
+    if (!products) {
+      refreshProducts({ dispatch, router });
+    }
+  }, [products]);
   return <div>StockList</div>;
 };
 

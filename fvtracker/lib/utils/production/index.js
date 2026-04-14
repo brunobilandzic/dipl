0136898ddce02427e2dd0127050e9e2bf66b0ce3
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-14 14:33:47 +0200

    production dispatch payloads

--- fvtracker/lib/utils/production/index.js
+++ fvtracker/lib/utils/production/index.js
@@ -1,19 +1,29 @@
 import api from "@/lib/api";
 import handleError from "@/lib/constants/errors/client/handleError";
 import { setLoading } from "@/store/loading";
+import { setProducts } from "@/store/production";
 
 export default async function fillProductionRedux({ dispatch, router }) {
   try {
     dispatch(setLoading(true));
+    console.log("Fetching production manager data...");
     const res = await api.get("/productionManager");
-    const productionManager = res.data;
-    console.log("Fetched production manager data:", productionManager);
+    const productionManager = res.data.productionManager;
+    dispatchPayloads({ manager: productionManager, dispatch });
+    console.log(productionManager);
     dispatch(setLoading(false));
     return productionManager;
   } catch (error) {
+    console.error("Error fetching production manager data:", error);
     handleError({
       ...error,
       generalMessage: "Greška pri dohvaćanju podataka o proizvodnji",
     });
   }
 }
+
+const dispatchPayloads = ({ manager, dispatch }) => {
+  console.log("Dispatching production manager data to Redux...");
+  console.log( manager );
+  dispatch(setProducts(manager.products));
+};

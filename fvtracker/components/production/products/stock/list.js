fc6be36af9ba84196bc687a86bd4a0d2cdef8497
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-14 12:54:07 +0200

    show stocks list

--- fvtracker/components/production/products/stock/list.js
+++ fvtracker/components/production/products/stock/list.js
@@ -4,6 +4,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useRouter } from "next/navigation";
 import { refreshProductsStocks } from "@/store/production";
+import { List } from "@/components/layout/preview/list";
 
 const StockList = () => {
   const dispatch = useDispatch();
@@ -14,7 +15,26 @@ const StockList = () => {
       dispatch(refreshProductsStocks());
     }
   }, [stocks]);
-  return <div>StockList</div>;
+  return (
+    <div>
+      <List title="Zalihe proizvoda">
+        {stocks?.map((stock) => (
+          <StockItem key={stock._id} stock={stock} />
+        ))}
+      </List>
+    </div>
+  );
 };
 
 export default StockList;
+
+const StockItem = ({ stock }) => {
+  console.log({ stock });
+  return (
+    <div>
+      <h3>{stock.product.name}</h3>
+      <p>{stock.product.description}</p>
+      <p>Quantity: {stock.quantity}</p>
+    </div>
+  );
+};

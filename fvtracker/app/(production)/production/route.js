a39f85ae147b62836ca1ab3b14be183dffa2c811
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-14 14:10:03 +0200

    check auth production route

--- fvtracker/app/(production)/production/route.js
+++ fvtracker/app/(production)/production/route.js
@@ -2,5 +2,10 @@ import { fetchManager } from "@/lib/auth/fetchSessionData";
 import { PRODUCTION_MANAGER } from "@/lib/constants/users/managerTypes";
 
 export async function GET(request) {
-
+  const { generalManager, specificManager, unauthorized } = await fetchManager({
+    managerNames: [PRODUCTION_MANAGER],
+  });
+  if (unauthorized) {
+    return Response.json({ unauthorized: true }, { status: 403 });
+  }
 }

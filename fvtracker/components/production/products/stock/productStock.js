b5b0bc7a9a6d791c5f5b4f6124c654faf2ff2291
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-14 13:46:50 +0200

    product stock page component

--- fvtracker/components/production/products/stock/productStock.js
+++ fvtracker/components/production/products/stock/productStock.js
@@ -1,9 +1,15 @@
 "use client";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { refreshProductsStocks } from "@/store/production";
 
 export const ProductStock = ({ slug }) => {
+  const dispatch = useDispatch();
   const stocks = useSelector((state) => state.production.productStocks.items);
+  if (!stocks) {
+    dispatch(refreshProductsStocks());
+  }
   const stock = stocks?.find((s) => s.product.slug === slug);
+
   return <div>ProductStock: {JSON.stringify(stock)}</div>;
 };

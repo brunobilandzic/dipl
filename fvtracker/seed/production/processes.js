ae8d3ee6d844407ee72aae67d8f771158c6c10df
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-18 18:42:34 +0200

    getMachineName

--- fvtracker/seed/production/processes.js
+++ fvtracker/seed/production/processes.js
@@ -1,5 +1,17 @@
+import { productMachineNameMap } from "@/seed/data/constants";
+
 export const process = ({ productName }) => {};
 
 export const getMachineName = ({ productName }) => {
-    const productNameWords = productName.split(" ")
+  const productNameWords = productName.split(" ");
+  let machineName = null;
+  for (const [productNames, mapMachineName] of productMachineNameMap) {
+    const productNamesWords = productNames.split(" ");
+    for (const word in productNameWords) {
+      if (word in productNameWords) {
+        machineName = mapMachineName;
+      }
+    }
+  }
+  return machineName || `Stroj za proizvod ${productName}`;
 };

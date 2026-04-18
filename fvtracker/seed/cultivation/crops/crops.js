361288d3e76bdf3d6dddc0e651c567ca25775fc6
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-18 12:20:45 +0200

    debug log plantedMap

--- fvtracker/seed/cultivation/crops/crops.js
+++ fvtracker/seed/cultivation/crops/crops.js
@@ -293,6 +293,7 @@ export async function plantageHarvest({
       cultivation.plantedCropVarieties.map((p) => p.relativeCoords),
     ),
   });
+  console.log({plantedMap})
   await createNewHarvest({ harvestingPlan, cultivation, plantedMap });
 }
 

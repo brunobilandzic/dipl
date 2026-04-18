cc265ab82038340006f60c060d875353635266fb
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-18 19:33:54 +0200

    remove logs

--- fvtracker/seed/production/processes.js
+++ fvtracker/seed/production/processes.js
@@ -7,14 +7,10 @@ export const getMachineName = ({ productName }) => {
   const productNameWords = productName
     .split(" ")
     .map((word) => onlyLetters(word).toLowerCase());
-
-  console.log("Product name words:", productNameWords);
   let machineName = null;
   for (const [productNamesKey, mapMachineName] of productMachineNameMap) {
     const productNamesKeys = productNamesKey.split(" ");
-    console.log("Checking product names keys:", productNamesKeys);
     for (const word of productNameWords) {
-      console.log({ word, productNamesKeys });
       if (productNamesKeys.includes(word)) {
         machineName = mapMachineName;
         break;

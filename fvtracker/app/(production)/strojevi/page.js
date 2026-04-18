04d25c84665bf22b495bcd75b06ec6b1b30c2f72
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-18 20:41:09 +0200

    use machine list component in page

--- fvtracker/app/(production)/strojevi/page.js
+++ fvtracker/app/(production)/strojevi/page.js
@@ -1,7 +1,12 @@
+import MachinesList from "@/components/production/machines/list";
 import React from "react";
 
 const MachinesPage = () => {
-  return <div>MachinesPage</div>;
+  return (
+    <div>
+      <MachinesList />
+    </div>
+  );
 };
 
 export default MachinesPage;

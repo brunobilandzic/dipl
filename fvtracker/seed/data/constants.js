3121dafb3876e82a4ca73c1d27449d6c13782b7c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-18 19:24:49 +0200

    kg in package machine names categoriy

--- fvtracker/seed/data/constants.js
+++ fvtracker/seed/data/constants.js
@@ -3,7 +3,7 @@ const seededPassword = "Password123!";
 export { seededPassword };
 
 export const productMachineNameMap = new Map([
-  ["pakiranje paket vrećica", "stroj za pakiranje"],
+  ["pakiranje paket vrećica kg", "stroj za pakiranje"],
   ["konzerva konzerve", "stroj za konzerviranje"],
   ["sok", "stroj za cijeđenje"],
 ]);

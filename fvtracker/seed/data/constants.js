be9ea8b944a573e63629440e1fa4162bd5ff5a7a
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-18 18:42:46 +0200

    productMachineNameMap rename

--- fvtracker/seed/data/constants.js
+++ fvtracker/seed/data/constants.js
@@ -2,7 +2,7 @@ const seededPassword = "Password123!";
 
 export { seededPassword };
 
-export const productMachine = new Map([
+export const productMachineNameMap = new Map([
   ["pakiranje paket vrećica", "stroj za pakiranje"],
   ["konzerva konzerve", "stroj za konzerviranje"],
   ["sok", "stroj za cijeđenje"],

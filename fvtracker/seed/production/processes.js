e19f92d399312b7ca0b8fe36d214c729aaa11d22
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-18 19:33:36 +0200

    find word in product names keys

--- fvtracker/seed/production/processes.js
+++ fvtracker/seed/production/processes.js
@@ -1,15 +1,23 @@
+import { onlyLetters } from "@/lib/utils/strings";
 import { productMachineNameMap } from "@/seed/data/constants";
 
 export const process = ({ productName }) => {};
 
 export const getMachineName = ({ productName }) => {
-  const productNameWords = productName.split(" ");
+  const productNameWords = productName
+    .split(" ")
+    .map((word) => onlyLetters(word).toLowerCase());
+
+  console.log("Product name words:", productNameWords);
   let machineName = null;
   for (const [productNamesKey, mapMachineName] of productMachineNameMap) {
     const productNamesKeys = productNamesKey.split(" ");
+    console.log("Checking product names keys:", productNamesKeys);
     for (const word of productNameWords) {
-      if (word in productNamesKeys) {
+      console.log({ word, productNamesKeys });
+      if (productNamesKeys.includes(word)) {
         machineName = mapMachineName;
+        break;
       }
     }
   }

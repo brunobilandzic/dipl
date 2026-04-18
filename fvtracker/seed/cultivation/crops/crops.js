649894adf6e39bf45fdeb9218ac3999e807b102b
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-18 12:30:06 +0200

    ediut harvest log

--- fvtracker/seed/cultivation/crops/crops.js
+++ fvtracker/seed/cultivation/crops/crops.js
@@ -233,7 +233,7 @@ export const createNewHarvest = async ({ harvestingPlan, plantedMap }) => {
     }
     const cropVarietyId = harvestingPlanItem.cropVariety._id;
     const harvestCoords = plantedCoords.slice(0, 2);
-    console.log("going to harvest", harvestCoords, "of cv:", cvName);
+    console.log("harvesting:", harvestCoords.length, "cells of", cvName);
 
     const docs = await PlantedCropVariety.find(
       { relativeCoords: { $in: harvestCoords } },

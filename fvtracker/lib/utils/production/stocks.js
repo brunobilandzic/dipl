98794ba7ab45c37415ece34ec6c73dca87d7a996
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-20 19:01:47 +0200

    productionStoksFacilities

--- fvtracker/lib/utils/production/stocks.js
+++ fvtracker/lib/utils/production/stocks.js
@@ -4,3 +4,14 @@ export const productsAllProductionStocksSum = ({ productionStocks }) => {
     return acc;
   }, 0);
 };
+
+export const productionStoksFacilities = ({ productionStocks }) => {
+  return productionStocks.reduce((acc, stock) => {
+    const facilityName = stock.facility.name;
+    if (!acc[facilityName]) {
+      acc[facilityName] = 0;
+    }
+    acc[facilityName] += stock.quantity;
+    return acc;
+  }, {});
+};

c59233aaa40c2a4648a433321904928ac5b79ce5
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-20 13:20:57 +0200

    remove filed params

--- fvtracker/seed/data/fields.js
+++ fvtracker/seed/data/fields.js
@@ -1,32 +1,5 @@
 import { getRandomString } from "@/lib/utils/strings";
 
-export const fieldParams = [
-  {
-    name: "Small CAS",
-    width: 100,
-    length: 100,
-    min_ca_dim: 10,
-    max_ca_dim: 50,
-    gap: 4,
-  },
-  {
-    name: "Medium CAS",
-    width: 500,
-    length: 500,
-    min_ca_dim: 25,
-    max_ca_dim: 90,
-    gap: 4,
-  },
-  {
-    name: "Large CAS",
-    width: 1000,
-    length: 1000,
-    min_ca_dim: 50,
-    max_ca_dim: 180,
-    gap: 6,
-  },
-];
-
 export const optimizedParams = {
   name: "SEEDED FIELD",
   description: "Field optimized for seeders testing",

822c9ae247b160fd59791a2bbda92caf72fc59de
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-20 13:32:20 +0200

    machinesSeedData

---
+++ fvtracker/seed/data/production/machines.js
@@ -0,0 +1,14 @@
+const machinesSeedData = [
+  {
+    name: "Stroj za pakiranje",
+    description: "Stroj za pakiranje gotovih proizvoda u pakete",
+  },
+  {
+    name: "Stroj za konzerviranje",
+    description: "Stroj za konzerviranje gotovih proizvoda u konzerve",
+  },
+  {
+    name: "Stroj za cijeđenje",
+    description: "Stroj za cijeđenje gotovih proizvoda u sokove",
+  },
+];

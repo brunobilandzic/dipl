3da63827a85ea2ddd2782967947c9f5326886f3d
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-20 13:42:41 +0200

    deleteProduction

--- fvtracker/seed/production/index.js
+++ fvtracker/seed/production/index.js
@@ -1,6 +1,13 @@
+import { ProductionFacility } from "@/models/sectors/production/Facility";
 import { createProducts } from "./createProducts";
+
 export default {
   seedProduction: async () => {
+    await deleteProduction();
     await createProducts();
   },
+};
+
+const deleteProduction = async () => {
+  await ProductionFacility.deleteMany({});
 };

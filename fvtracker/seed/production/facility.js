a3e383ec6db99bcb6b439d91044978f0c8a2b815
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-20 17:04:20 +0200

    createFacility

--- fvtracker/seed/production/facility.js
+++ fvtracker/seed/production/facility.js
@@ -1,3 +1,10 @@
+import { getRandomString } from "@/lib/utils/strings";
+
 export const createFacility = async () => {
-    
-}
+  const facility = new ProductionFacility({
+    name: `Postrojenje ${getRandomString(5)}`,
+    description: "Postrojenje inicijalizirano za potrebe razvoja aplikacije.",
+  });
+  await facility.save();
+  return facility;
+};

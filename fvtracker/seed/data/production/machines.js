052cb1ad640a4222be407864d9194c6a75615daf
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-20 13:38:08 +0200

    machnames singuzkar vars

--- fvtracker/seed/data/production/machines.js
+++ fvtracker/seed/data/production/machines.js
@@ -1,22 +1,18 @@
 const machinesSeedData = [
   {
-    name: names.PACKAGING_MACHINE,
+    name: PACKAGING_MACHINE,
     description: "Stroj za pakiranje gotovih proizvoda u pakete",
   },
   {
-    name: names.CANNING_MACHINE,
+    name: CANNING_MACHINE,
     description: "Stroj za konzerviranje gotovih proizvoda u konzerve",
   },
   {
-    name: names.JUICE_PRESSING_MACHINE,
+    name: JUICE_PRESSING_MACHINE,
     description: "Stroj za cijeđenje gotovih proizvoda u sokove",
   },
 ];
 
-const names = {
-  PACKAGING_MACHINE: "Stroj za pakiranje",
-  CANNING_MACHINE: "Stroj za konzerviranje",
-  JUICE_PRESSING_MACHINE: "Stroj za cijeđenje",
-};
-
-export default names;
+export const PACKAGING_MACHINE = "Stroj za pakiranje";
+export const CANNING_MACHINE = "Stroj za konzerviranje";
+export const JUICE_PRESSING_MACHINE = "Stroj za cijeđenje";

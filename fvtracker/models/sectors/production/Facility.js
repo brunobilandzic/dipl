02d3977b463e863e64fdefbf45112504effcbbdb
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-20 17:39:19 +0200

    adjust facility delete many cascade

--- fvtracker/models/sectors/production/Facility.js
+++ fvtracker/models/sectors/production/Facility.js
@@ -28,13 +28,7 @@ const productionFacilitySchema = new Schema({
 
 productionFacilitySchema.pre("deleteMany", async function () {
   const facilities = await this.model.find(this.getFilter()).distinct("_id");
-  await mongoose.model("ProductionMachine").deleteMany({
-    productionFacility: { $in: facilities },
-  });
   await mongoose.model("ProductStock").deleteMany({
-    productionFacility: { $in: facilities },
-  });
-  await mongoose.model("ProductionProcess").deleteMany({
     productionFacility: { $in: facilities },
   });
 });

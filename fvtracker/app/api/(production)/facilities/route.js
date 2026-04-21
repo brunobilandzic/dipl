c452fc038929e6ad7fac8d70daefd23c38928327
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-21 16:27:38 +0200

    facilities route

--- fvtracker/app/api/(production)/facilities/route.js
+++ fvtracker/app/api/(production)/facilities/route.js
@@ -1,11 +1,14 @@
+import { fetchManager } from "@/lib/auth/fetchSessionData";
 import {
   PRODUCTION_MANAGER,
   WAREHOUSE_MANAGER,
 } from "@/lib/constants/users/managerTypes";
+import { getFacilities } from "@/lib/production/facilities/get";
 
 export async function GET(request) {
   const { specificManager: productionManager } = await fetchManager({
     managerNames: [PRODUCTION_MANAGER, WAREHOUSE_MANAGER],
   });
-  return Response.json({ message: "Facilities route" }, { status: 200 });
+  const facilities = await getFacilities();
+  return Response.json({ facilities }, { status: 200 });
 }

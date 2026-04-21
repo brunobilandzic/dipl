8a9b614e0e2084af3fc59c24ef7c9a595183164c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-21 16:48:05 +0200

    create stock, wrap routes in try catch

--- fvtracker/app/api/(production)/stocks/route.js
+++ fvtracker/app/api/(production)/stocks/route.js
@@ -1,13 +1,59 @@
-import { getStocks } from "@/lib/production/stocks";
+import { createProductStock, getStocks } from "@/lib/production/stocks";
+import dbConnect from "@/lib/db/mongooseConnect";
 
 export async function GET(request) {
-  const { searchParams } = new URL(request.url);
-  const productId = searchParams.get("productId");
-  if (!productId) {
-    const stocks = await getStocks();
+  try {
+    await dbConnect();
+    const { searchParams } = new URL(request.url);
+    const productId = searchParams.get("productId");
+    if (!productId) {
+      const stocks = await getStocks();
+      return Response.json(
+        { stocks },
+        {
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+    }
+  } catch (error) {
+    console.log("Error fetching stocks:", error);
     return Response.json(
-      { stocks },
+      { error: error.message },
       {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+}
+
+export async function POST(request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+    const { productId, quantity, batchName, comment, productionFacilityId } =
+      body;
+    const productionStock = await createProductStock({
+      productId,
+      batchName,
+      productionFacilityId,
+      quantity,
+      comment,
+    });
+
+    return Response.json(
+      { productionStock },
+      {
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  } catch (error) {
+    console.log("Error creating production stock:", error);
+
+    return Response.json(
+      { error: error.message },
+      {
+        status: 500,
         headers: { "Content-Type": "application/json" },
       },
     );

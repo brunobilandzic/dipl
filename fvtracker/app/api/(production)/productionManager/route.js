895a7b6bbdbb1808028c93dfe74b99c9d43a4388
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-21 15:47:31 +0200

    populate warehouseAcceptanceProcesses

--- fvtracker/app/api/(production)/productionManager/route.js
+++ fvtracker/app/api/(production)/productionManager/route.js
@@ -41,6 +41,9 @@ export async function GET(request) {
             {
               path: "warehouse",
             },
+            {
+              path: "warehouseAcceptanceProcesses",
+            },
           ],
         },
       ],

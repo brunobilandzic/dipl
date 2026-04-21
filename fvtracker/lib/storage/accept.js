ee3f84e29081ea76b8bad40ac56883580f6726cc
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-21 12:51:38 +0200

    find according warehouse stock

--- fvtracker/lib/storage/accept.js
+++ fvtracker/lib/storage/accept.js
@@ -1,9 +1,10 @@
 import { ProductionStock } from "@/models/sectors/production/Facility";
 
-export const createWarehouseStock = async ({
+export const acceptWarehouseStock = async ({
   productId,
   quantity,
   productionFacilityId,
+  warehouseId,
 }) => {
   const productionStock = await ProductionStock.findOne({
     product: productId,
@@ -19,4 +20,17 @@ export const createWarehouseStock = async ({
       `Not enough quantity in production stock. Required: ${quantity}, Available: ${productionStock.quantity}`,
     );
   }
+  let stock = await WarehouseStock.findOne({
+    product: productId,
+    warehouse: warehouseId,
+  });
+  if (!stock) {
+    stock = new WarehouseStock({
+      product: productId,
+      warehouse: warehouseId,
+      quantity: 0,
+    });
+  }
+  stock.quantity += quantity
+  await stock.save();
 };

4afd004fe1868afd0ff8a6ae26b69972be23c0d8
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-25 20:11:22 +0200

    submit warehouse stock util function

--- fvtracker/lib/utils/storage/warehouse.js
+++ fvtracker/lib/utils/storage/warehouse.js
@@ -1,6 +1,23 @@
+import { WAREHOUSE_STOCK } from "@/lib/constants/warehouse";
+
 export const totalWarehouseStockQuantity = ({ warehouseStocks }) => {
   return warehouseStocks.reduce((acc, stock) => {
     acc += stock.quantity;
     return acc;
   }, 0);
+};
+
+export const submitWarehouseStock = async ({ warehouseStockData }) => {
+  const res = await api.post(
+    "/stocks",
+    {
+      warehouseStockData,
+    },
+    {
+      params: {
+        stockType: WAREHOUSE_STOCK,
+      },
+    },
+  );
+  return res.data.newWarehouseStock;
 };

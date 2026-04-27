072004ea675d829dc84de18617aecfb25119712b
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-27 16:05:56 +0200

    unlean for populate

--- fvtracker/lib/production/facilities/get.js
+++ fvtracker/lib/production/facilities/get.js
@@ -2,9 +2,12 @@ import { ProductionFacility } from "@/models/sectors/production/Facility";
 
 export const getFacilities = async ({ slug }) => {
   if (slug) {
-    const facility = await ProductionFacility.findOne({ slug }).lean();
+    const facility = await ProductionFacility.findOne({ slug })
     return facility;
   }
-  const facilities = await ProductionFacility.find({}).lean();
+  const facilities = await ProductionFacility.find({})
   return facilities;
 };
+
+
+export const populateFacilities = async (facilities) => { }

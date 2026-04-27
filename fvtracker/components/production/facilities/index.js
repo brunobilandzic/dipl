a653684755277c1eb2fbe1b9e6470f610207d4ed
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-27 15:51:44 +0200

    dispatch select facility

--- fvtracker/components/production/facilities/index.js
+++ fvtracker/components/production/facilities/index.js
@@ -1,25 +1,28 @@
 "use client";
 
 import { LoadingFullScreen } from "@/components/layout/loading";
+import { setSelectedFacility } from "@/store/production";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function Facility({ slug }) {
+  const dispatch = useDispatch();
   const facility = useSelector((state) => state.production.facilities.selected);
-  if (!facility) return <LoadingFullScreen />;
+  if (!facility) {
+    dispatch(setSelectedFacility(slug));
+  }
   return (
     <>
-      <div>Facility {facility.name}</div>
+      <div>Facility {facility?.name}</div>
       <div>
-        <FacilityStocks stocks={facility.stocks} />
+        <FacilityStocks stocks={facility?.stocks} />
       </div>
     </>
   );
 }
 
 const FacilityStocks = ({ stocks }) => {
-  if (!stocks) return <LoadingFullScreen />;
-  return <div>FacilityStocks {stocks.length}</div>;
+  return <div>FacilityStocks {stocks?.length}</div>;
 };
 
 export default Facility;

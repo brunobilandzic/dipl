699a235ebf3a0dfdf95ce071070415aa58d3391e
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-27 15:29:09 +0200

    getFacilities with slug

--- fvtracker/lib/production/facilities/get.js
+++ fvtracker/lib/production/facilities/get.js
@@ -1,6 +1,8 @@
 import { ProductionFacility } from "@/models/sectors/production/Facility";
 
-export const getFacilities = async () => {
-  const facilities = await ProductionFacility.find().populate([{ path: "stocks" }]);
-  return facilities;
+export const getFacilities = async ({ slug }) => {
+  const facilities = await ProductionFacility.find({ slug }).populate([
+    { path: "stocks" },
+  ]);
+  return slug ? facilities[0] : facilities;
 };

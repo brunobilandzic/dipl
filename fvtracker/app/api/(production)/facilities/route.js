7f86f5b7ed0af726b5c9d82916415c1599912f41
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-27 13:39:00 +0200

    get facility list route

--- fvtracker/app/api/(production)/facilities/route.js
+++ fvtracker/app/api/(production)/facilities/route.js
@@ -3,12 +3,25 @@ import {
   PRODUCTION_MANAGER,
   WAREHOUSE_MANAGER,
 } from "@/lib/constants/users/managerTypes";
+import dbConnect from "@/lib/db/mongooseConnect";
 import { getFacilities } from "@/lib/production/facilities/get";
+export const GET = async (req) => {
+  try {
+    await dbConnect();
+    const { unauthorized } = await fetchManager({
+      managerNames: [PRODUCTION_MANAGER, WAREHOUSE_MANAGER],
+    });
+    if (unauthorized) {
+      return Response.json({ unauthorized: true }, { status: 403 });
+    }
+    const facilities = await getFacilities();
+    return Response.json({ facilities }, { status: 200 });
+  } catch (error) {
+    console.error("Greška pri dohvaćanju postrojenja:", error);
+    return Response.json(
+      { error: "Greška pri dohvaćanju postrojenja" },
+      { status: 500 },
+    );
+  }
+};
 
-export async function GET(request) {
-  const { specificManager: productionManager } = await fetchManager({
-    managerNames: [PRODUCTION_MANAGER, WAREHOUSE_MANAGER],
-  });
-  const facilities = await getFacilities();
-  return Response.json({ facilities }, { status: 200 });
-}

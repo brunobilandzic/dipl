72f935bd94158ec477cd6f40381c3a9332350079
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-28 12:33:56 +0200

    response.json not new response

--- fvtracker/app/api/warehouse/route.js
+++ fvtracker/app/api/warehouse/route.js
@@ -3,6 +3,7 @@ export async function GET(request, { params }) {
   if (warehouseId) {
     const { getWarehouse } = await import("@/lib/warehouses/get");
     const warehouse = await getWarehouse({ warehouseId });
+    return Response.json({ warehouse });
   } else {
     const { getWarehouses } = await import("@/lib/warehouses/get");
     const warehouses = await getWarehouses();

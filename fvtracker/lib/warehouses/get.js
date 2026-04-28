aa47aeda833a52da10c5a1a8c14090f1a954694d
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-28 12:30:55 +0200

    use warehousePopulateConfig

--- fvtracker/lib/warehouses/get.js
+++ fvtracker/lib/warehouses/get.js
@@ -1,10 +1,7 @@
 import { Warehouse } from "@/models/sectors/storage/Warehouse";
+import warehousePopulateConfig from "./populateConfig";
 
 export async function getWarehouse({ warehouseId }) {
-  const warehouse = await Warehouse.findById(warehouseId).populate([
-    {
-      path: "stocks",
-    },
-  ]);
+  const warehouse = await Warehouse.findById(warehouseId).populate(warehousePopulateConfig);
   return warehouse;
 }

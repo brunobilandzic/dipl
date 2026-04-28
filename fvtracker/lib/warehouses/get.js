26453e7f4474d7e4fd576dcc1686837a0ce3a6b9
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-28 12:50:25 +0200

    lib get id param rename

--- fvtracker/lib/warehouses/get.js
+++ fvtracker/lib/warehouses/get.js
@@ -1,8 +1,8 @@
 import { Warehouse } from "@/models/sectors/storage/Warehouse";
 import warehousePopulateConfig from "./populateConfig";
 
-export async function getWarehouse({ warehouseId }) {
-  const warehouse = await Warehouse.findById(warehouseId).populate(
+export async function getWarehouse({ id }) {
+  const warehouse = await Warehouse.findById(id).populate(
     warehousePopulateConfig,
   );
   return warehouse;

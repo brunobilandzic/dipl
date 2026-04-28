fb7725c91db1b74b7f6431183e4acad6d64c5d3d
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-28 12:49:10 +0200

    fix get war params route

--- fvtracker/app/api/warehouses/route.js
+++ fvtracker/app/api/warehouses/route.js
@@ -1,8 +1,9 @@
-export async function GET(request, { params }) {
-  const { warehouseId } = params;
-  if (warehouseId) {
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+  if (id) {
     const { getWarehouse } = await import("@/lib/warehouses/get");
-    const warehouse = await getWarehouse({ warehouseId });
+    const warehouse = await getWarehouse({ id });
     return Response.json({ warehouse });
   } else {
     const { getWarehouses } = await import("@/lib/warehouses/get");

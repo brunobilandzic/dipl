c6dbe18c9a670399f48b351d806ec2c8b8c45726
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-04-28 12:32:30 +0200

    getWarehouses lib

--- fvtracker/lib/warehouses/get.js
+++ fvtracker/lib/warehouses/get.js
@@ -2,6 +2,13 @@ import { Warehouse } from "@/models/sectors/storage/Warehouse";
 import warehousePopulateConfig from "./populateConfig";
 
 export async function getWarehouse({ warehouseId }) {
-  const warehouse = await Warehouse.findById(warehouseId).populate(warehousePopulateConfig);
+  const warehouse = await Warehouse.findById(warehouseId).populate(
+    warehousePopulateConfig,
+  );
   return warehouse;
+}
+
+export async function getWarehouses() {
+  const warehouses = await Warehouse.find().populate(warehousePopulateConfig);
+  return warehouses;
 }

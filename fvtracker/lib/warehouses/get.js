d7c3197499fdcdad0fc84f9e6e8359c279c580d1
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-06 19:26:18 +0200

    wh getters throw errs

--- fvtracker/lib/warehouses/get.js
+++ fvtracker/lib/warehouses/get.js
@@ -5,10 +5,16 @@ export async function getWarehouse({ id }) {
   const warehouse = await Warehouse.findById(id).populate(
     warehousePopulateConfig,
   );
+  if (!warehouse) {
+    throw new Error("Warehouse not found with the provided ID.");
+  }
   return warehouse;
 }
 
 export async function getWarehouses() {
   const warehouses = await Warehouse.find().populate(warehousePopulateConfig);
+  if (!warehouses) {
+    throw new Error("No warehouses found");
+  }
   return warehouses;
 }

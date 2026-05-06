1b4c41c40e04f852b8523264c7e88fccb44dce54
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-06 17:55:38 +0200

    WarehouseListPage

---
+++ fvtracker/app/(wareouse)/skladisne-jedinice/page.js
@@ -0,0 +1,12 @@
+import { WarehouseList } from "@/components/warehouse/objects/list";
+import React from "react";
+
+function WarehouseListPage() {
+  return (
+    <div>
+      <WarehouseList />
+    </div>
+  );
+}
+
+export default WarehouseListPage;

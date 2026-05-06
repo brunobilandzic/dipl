1b432f965cc684711ccc62766874773ad251ac3a
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-06 19:24:24 +0200

    simple warehpuse list

--- fvtracker/components/warehouse/objects/list.js
+++ fvtracker/components/warehouse/objects/list.js
@@ -1,15 +1,30 @@
 "use client";
+import { ListItem, List } from "@/components/layout/preview/list";
 import { useDispatch, useSelector } from "react-redux";
 
 export const WarehouseList = () => {
-  const warehouses = useSelector((state) => state.warehouses);
+  const warehouses = useSelector(
+    (state) => state.warehouse.warehouses.filteredItems,
+  );
   const dispatch = useDispatch();
 
   console.log({ warehouses });
 
   return (
     <>
-      <div>skladista</div>
+      <List title="Skladišta">
+        {warehouses.map((wh) => (
+          <WarehouseListItem key={wh._id} warehouse={wh} />
+        ))}
+      </List>
     </>
+  );
+};
+
+const WarehouseListItem = ({ warehouse }) => {
+  return (
+    <div>
+      <ListItem title={warehouse.name} subtitle={warehouse.location} />
+    </div>
   );
 };

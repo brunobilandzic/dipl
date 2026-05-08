56cc7ab55f975c413b3f2e21bb7c62cd9abf70e7
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-08 13:48:16 +0200

    update warehouse info

--- fvtracker/app/api/warehouses/route.js
+++ fvtracker/app/api/warehouses/route.js
@@ -1,3 +1,4 @@
+import { deleteWarehouse, updateWarehouse } from "@/lib/warehouses";
 import { getWarehouses, getWarehouse } from "@/lib/warehouses/get";
 import warehousePopulateConfig from "@/lib/warehouses/populateConfig";
 
@@ -13,5 +14,19 @@ export async function GET(request) {
       await wh.populate(warehousePopulateConfig);
     }
     return Response.json({ warehouses });
+  }
+}
+export async function PUT(request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+    const data = await request.json();
+    await updateWarehouse({ id, data });
+    return Response.json({ success: true }, { status: 200 });
+  } catch (err) {
+    return Response.json(
+      { error: err?.message ?? String(err) },
+      { status: 500 },
+    );
   }
 }

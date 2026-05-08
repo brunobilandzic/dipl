b7f9eafe03aefea8fdbd34b94df653f96f6f031b
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-08 14:13:44 +0200

    createwh page

---
+++ fvtracker/app/(wareouse)/skladisne-jedinice/dodavanje/page.js
@@ -0,0 +1,12 @@
+import { fetchManager } from "@/lib/auth/fetchSessionData";
+import { WAREHOUSE_MANAGER } from "@/lib/constants/users/managerTypes";
+import React from "react";
+
+async function CreateWarehousePage() {
+  const { speciicManger } = await fetchManager({
+    managerNames: [WAREHOUSE_MANAGER],
+  });
+  return <div>CreateWarehousePage</div>;
+}
+
+export default CreateWarehousePage;

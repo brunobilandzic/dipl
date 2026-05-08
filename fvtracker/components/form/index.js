603544f9ecf4bbfb5c5f1c0e2461ce9092c14b88
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-08 14:08:20 +0200

    subm btn margin top

--- fvtracker/components/form/index.js
+++ fvtracker/components/form/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export const SubmitButton = ({ handleSubmit, label = "Spremi promjene" }) => {
   return (
     <div>
-      <div onClick={handleSubmit} className="btn submitButton">
+      <div onClick={handleSubmit} className="mt-4 btn submitButton">
         {label}
       </div>
     </div>

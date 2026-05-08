c29f5657836185be928e7e426395ff60be30675a
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-08 17:11:55 +0200

    create facility whole body

--- fvtracker/lib/production/facilities/create.js
+++ fvtracker/lib/production/facilities/create.js
@@ -1,6 +1,6 @@
 import { ProductionFacility } from "@/models/sectors/production/Facility";
 
-export const createFacility = async ({ name, description }) => {
-  const facility = await ProductionFacility.create({ name, description });
+export const createFacility = async (facilityData) => {
+  const facility = await ProductionFacility.create(facilityData);
   return facility;
 };

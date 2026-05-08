ba2cc4b3eddd513a110a962cb8c5fe1e31cd3184
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-08 13:43:06 +0200

    ditch in func imports

--- fvtracker/app/api/warehouses/route.js
+++ fvtracker/app/api/warehouses/route.js
@@ -5,11 +5,9 @@ export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get("id");
   if (id) {
-    const { getWarehouse } = await import("@/lib/warehouses/get");
     const warehouse = await getWarehouse({ id });
     return Response.json({ warehouse });
   } else {
-    const { getWarehouses } = await import("@/lib/warehouses/get");
     const warehouses = await getWarehouses();
     for (let wh of warehouses) {
       await wh.populate(warehousePopulateConfig);

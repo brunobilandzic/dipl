2cc53922205613bca9843406cb29700675d7cdf7
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-08 13:00:26 +0200

    submitWarehouseForm util func

--- fvtracker/lib/utils/storage/warehouse.js
+++ fvtracker/lib/utils/storage/warehouse.js
@@ -1,4 +1,5 @@
 import api from "@/lib/api";
+import handleError from "@/lib/constants/errors/client/handleError";
 import { WAREHOUSE_STOCK } from "@/lib/constants/warehouse";
 
 export const totalWarehouseStockQuantity = ({ warehouseStocks }) => {
@@ -22,4 +23,33 @@ export const submitWarehouseStock = async ({ warehouseStockData }) => {
     },
   );
   return res.data.newWarehouseStock;
+};
+
+export const submitWarehouseForm = async ({
+  isEdit,
+  form,
+  warehouseId,
+  dispatch,
+  router,
+}) => {
+  try {
+    if (isEdit) {
+      await api.put(`/warehouses/${warehouseId}`, form);
+    } else {
+      await api.post("/warehouses", form);
+    }
+    dispatch(fetchWarehouses());
+    router.push("/skladisne-jedinice");
+  } catch (error) {
+    console.error("Error submitting warehouse form:", error);
+    handleError(
+      {
+        ...error,
+        generalMessage: isEdit
+          ? "Došlo je do greške prilikom ažuriranja skladišta."
+          : "Došlo je do greške prilikom kreiranja skladišta.",
+      },
+      router,
+    );
+  }
 };

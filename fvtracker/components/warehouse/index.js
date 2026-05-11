bce416945c1db288cff8f5f57415b6ff79047f24
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-11 13:11:34 +0200

    List warehouse stocks

--- fvtracker/components/warehouse/index.js
+++ fvtracker/components/warehouse/index.js
@@ -3,6 +3,9 @@
 import React, { useState } from "react";
 import { FaCubesStacked } from "react-icons/fa6";
 import { useSelector } from "react-redux";
+import { List } from "../layout/preview/list";
+import { OptionButtons } from "../layout/buttons/options";
+import { ReturnButton } from "../layout/buttons/buttons";
 
 function WarehousePageComponent({ slug }) {
   const warehouses = useSelector((state) => state.warehouse.warehouses.items);
@@ -20,15 +23,46 @@ function WarehousePageComponent({ slug }) {
     },
   ];
 
-  console.log({ warehouse });
+  console.log({ stocksView });
 
   if (!warehouse) return <div>Warehouse not found</div>;
   if (stocksView) {
-    return <div>Stocks view for {warehouse.name}</div>;
+    return (
+      <div>
+        <WarehouseStocks
+          stocks={warehouse.stocks}
+          onCancel={() => setStocksView(false)}
+          warehouseName={warehouse.name}
+        />
+      </div>
+    );
   }
-  return <div>WarehousePageComponent</div>;
+  return (
+    <div>
+      <WarehouseOptions options={options} />
+    </div>
+  );
 }
 
-const WarehouseOptions = ({ options }) => {};
+const WarehouseOptions = ({ options }) => {
+  return <OptionButtons options={options} />;
+};
+
+const WarehouseStocks = ({ stocks, onCancel, warehouseName }) => {
+  return (
+    <div>
+      <List
+        title={`Zalihe na skladištu ${warehouseName}`}
+        customButtons={
+          <ReturnButton onClick={onCancel} returnLabel="Povratak na zalihe" />
+        }
+      >
+        {stocks.map((stock) => (
+          <div key={stock._id}>{stock?.product?.name}</div>
+        ))}
+      </List>
+    </div>
+  );
+};
 
 export default WarehousePageComponent;

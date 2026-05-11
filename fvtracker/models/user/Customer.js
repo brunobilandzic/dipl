ca4308d87dfda48468424ad25305eae2bee97da2
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-11 14:10:04 +0200

    customer model

---
+++ fvtracker/models/user/Customer.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const customerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  appUser: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "AppUser",
+    dafault: null,
+  },
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      default: [],
+    },
+  ],
+});
+
+export const Customer =
+  mongoose.models.Customer || mongoose.model("Customer", customerSchema);

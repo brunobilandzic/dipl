b492ae86e9aa4d3b1048d148f155e034d360203c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-11 12:34:58 +0200

    WarehousePage file

---
+++ fvtracker/app/(wareouse)/skladisne-jedinice/[slug]/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+async function WarehousePage({ params }) {
+  const { slug } = await params;
+  return (
+    <div>
+  
+    </div>
+  );
+}
+
+export default WarehousePage;

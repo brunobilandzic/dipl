af115471019a2297e1b5e7d812fddc07abb8a55a
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-11 13:34:11 +0200

    acc build whole product not just name

--- fvtracker/lib/utils/storage/acceptances.js
+++ fvtracker/lib/utils/storage/acceptances.js
@@ -2,7 +2,7 @@ export const buildAcceptances = ({ stocks }) => {
   const acceptances = [];
   stocks.forEach((stock) => {
     acceptances.push({
-      product: stock.product.name,
+      product: stock.product,
       warehouseAcceptanceProcesses: stock.warehouseAcceptanceProcesses,
     });
   });

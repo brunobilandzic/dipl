523975c2be1270aef47614580008e73a8b118d3c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-11 12:37:01 +0200

    rednder WarehousePageComponent

--- fvtracker/app/(wareouse)/skladisne-jedinice/[slug]/page.js
+++ fvtracker/app/(wareouse)/skladisne-jedinice/[slug]/page.js
@@ -1,10 +1,11 @@
+import WarehousePageComponent from "@/components/warehouse";
 import React from "react";
 
 async function WarehousePage({ params }) {
   const { slug } = await params;
   return (
     <div>
-  
+      <WarehousePageComponent slug={slug} />
     </div>
   );
 }

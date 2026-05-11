01d859089f5954a5732cfa138a80d4e1b9b349c9
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-11 12:41:34 +0200

    get wh in index by slug

--- fvtracker/components/warehouse/index.js
+++ fvtracker/components/warehouse/index.js
@@ -1,6 +1,15 @@
-import React from "react";
+"use client";
 
-function WarehousePageComponent() {
+import React from "react";
+import { useSelector } from "react-redux";
+
+function WarehousePageComponent({ slug }) {
+  const warehouses = useSelector((state) => state.warehouse.warehouses.items);
+  const warehouse = warehouses?.find((w) => w.slug === slug);
+
+  console.log({ warehouse });
+
+  if (!warehouse) return <div>Warehouse not found</div>;
   return <div>WarehousePageComponent</div>;
 }
 

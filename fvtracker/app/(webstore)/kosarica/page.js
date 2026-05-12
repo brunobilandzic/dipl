c19d0710eb86299cd7394dd40cb5e1e9ed905db9
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-12 18:19:18 +0200

    kosarica page

---
+++ fvtracker/app/(webstore)/kosarica/page.js
@@ -0,0 +1,12 @@
+import { CartPageComponent } from "@/components/webstore/cart";
+import React from "react";
+
+function CartPage() {
+  return (
+    <div>
+      <CartPageComponent />
+    </div>
+  );
+}
+
+export default CartPage;

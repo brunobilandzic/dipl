b8170a9000a9c2f8e4828489d511c334d30dfa97
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-12 13:10:28 +0200

    declare store reducer

--- fvtracker/store/index.js
+++ fvtracker/store/index.js
@@ -6,6 +6,7 @@ import cultivationReducer from "./cultivation";
 import generalManagerReducer from "./generalManager";
 import productionReducer from "./production/index.js";
 import warehouseReducer from "./warehouse";
+import storeReducer from "./store";
 
 const store = configureStore({
   reducer: {
@@ -16,6 +17,7 @@ const store = configureStore({
     generalManager: generalManagerReducer,
     production: productionReducer,
     warehouse: warehouseReducer,
+    store: storeReducer,
   },
 });
 

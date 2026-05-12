eb6cf6e4402de7e29043f5a006f962b09d146592
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-12 16:36:12 +0200

    add to cart quantity floating input

--- fvtracker/components/webstore/cart.js
+++ fvtracker/components/webstore/cart.js
@@ -1,18 +1,45 @@
-export const AddToCartQuantity = ({ quantity, setQuantity }) => {
+import { preventEvent } from "@/lib/utils/dev";
+import { useEffect, useState } from "react";
+import { FaShoppingCart } from "react-icons/fa";
+
+export const AddToCartQuantity = ({
+  quantity,
+  setQuantity,
+  submitQuantity,
+}) => {
   const onChange = (e) => {
     e.stopPropagation();
     e.preventDefault();
     const value = parseInt(e.target.value);
-    if (isNaN(value) || value < 1) {
-      setQuantity(1);
+    if (isNaN(value)) {
+      setQuantity(null);
     } else {
       setQuantity(value);
     }
   };
   return (
     <>
-      <div className="absolute w-4 top-0 left-0">
-        <input type="number" onChange={onChange} />
+      <div className="absolute border rounded-lg flex flex-col items-center gap-2 top-2 left-2  py-2 px-4 bg-slate-900 ">
+        <input
+          type="number"
+          value={quantity}
+          onChange={onChange}
+          onClick={(e) => e.stopPropagation()}
+          className="w-full"
+        />
+        <div
+          onClick={(e) => {
+            preventEvent(e);
+            submitQuantity(quantity);
+          }}
+          className="btn submitButton btnSm"
+        >
+          Dodaj
+        </div>
+      </div>
+    </>
+  );
+};
       </div>
     </>
   );

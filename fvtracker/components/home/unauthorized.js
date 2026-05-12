eae8fb4a22ec6b5f2429cdadcdd4ff29b265342c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-12 14:35:49 +0200

    render ProductList in unauth homepage

--- fvtracker/components/home/unauthorized.js
+++ fvtracker/components/home/unauthorized.js
@@ -2,8 +2,13 @@
 
 import { useSession } from "next-auth/react";
 import { useSelector } from "react-redux";
+import { ProductList } from "../webstore/products";
 
 export const UnathorizedHomepage = () => {
   const { data: session, status } = useSession();
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <ProductList />
+    </div>
+  );
 };

ff664ff38c20f7092aa08c58644d6388d37fb420
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-12 17:09:15 +0200

    cart local storage sh

--- fvtracker/lib/utils/webstore/cart.js
+++ fvtracker/lib/utils/webstore/cart.js
@@ -2,3 +2,29 @@ export const productInCart = ({ productId, cartItems }) => {
   const productItem = cartItems.find((item) => item.product.id === productId);
   return productItem ? productItem.quantity : null;
 };
+
+export const setCartToLocalStorege = ({ cart }) => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+export const getCartFromLocalStorage = () => {
+  const cart = localStorage.getItem("cart");
+  return cart ? JSON.parse(cart) : null;
+};
+
+export const fillCartRedux = ({ dispatch, useSelector }) => {
+  const cartItems = useSelector((state) => state.webstore.cart.items);
+  useEffect(() => {
+    if (cartItems.length > 0) {
+      return;
+    }
+    const localCart = getCartFromLocalStorage();
+    if (localCart && localCart.items) {
+      localCart.items.forEach((item) => {
+        dispatch(
+          addToCartRedux({ product: item.product, quantity: item.quantity }),
+        );
+      });
+    }
+  }, [dispatch]);
+};

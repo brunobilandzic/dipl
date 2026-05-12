40189e6a46bb4ce172507b1ae5a8d548d87efb38
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-12 12:59:49 +0200

    store redux slice for customers

---
+++ fvtracker/store/store.js
@@ -0,0 +1,44 @@
+import { sortItems } from "@/lib/utils/list";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: {
+    items: [],
+    filteredItems: [],
+  },
+  isLoading: false,
+};
+
+const productsSlice = createSlice({
+  name: "production",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(refreshProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(refreshProducts.fulfilled, (state, action) => {
+        state.products.items = action.payload;
+        state.products.filteredItems = sortItems({
+          items: action.payload,
+          sortBy: SORT_INIT_VALUE,
+        });
+        state.isLoading = false;
+      })
+      .addCase(refreshProducts.rejected, (state) => {
+        state.isLoading = false;
+      });
+  },
+});
+
+const refreshProducts = createAsyncThunk(
+  "production/refreshProducts",
+  async () => {
+    console.log("Fetching products...");
+    const res = await api.get("/products");
+    const data = res.data;
+    console.log({ data });
+    return res.data.products;
+  },
+);

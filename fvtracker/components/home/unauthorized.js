2ded0c73b90a1c22dfeb68f353eb85074305edec
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-12 14:32:27 +0200

    unauthorized home page

--- fvtracker/components/home/unauthorized.js
+++ fvtracker/components/home/unauthorized.js
@@ -1,6 +1,8 @@
+"use client";
+
 import { useSelector } from "react-redux";
 
-export const UnauthorizedHomePage = () => {
-  const managerModelName = useSelector((state) => state.auth.managerModelName);
+export const UnathorizedHomepage = () => {
+  const managerModelName = useSelector((state) => state.user.managerModelName);
   return <div>Homepage</div>;
 };

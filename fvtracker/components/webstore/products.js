abc7a1dd2c08d9b5c710ab3bf174109b052165d6
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-12 16:38:35 +0200

    add to cart btn delete from old file

--- fvtracker/components/webstore/products.js
+++ fvtracker/components/webstore/products.js
@@ -93,20 +93,3 @@ const ProductActions = ({ actions }) => {
     </div>
   );
 };
-
-const ProductActionComponent = ({ onClick, children }) => {
-  return (
-    <>
-      <div
-        className="text-xl hover:text-2xl hover:font-bold "
-        onClick={(e) => {
-          e.stopPropagation();
-          e.preventDefault();
-          onClick();
-        }}
-      >
-        {children}
-      </div>
-    </>
-  );
-};

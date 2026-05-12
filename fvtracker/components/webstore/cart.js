af3089b8bc4b1daf1924a17385eca778d4e826a0
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-12 16:40:10 +0200

    refaactor submit to outer func

--- fvtracker/components/webstore/cart.js
+++ fvtracker/components/webstore/cart.js
@@ -2,11 +2,7 @@ import { preventEvent } from "@/lib/utils/dev";
 import { useEffect, useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 
-export const AddToCartQuantity = ({
-  quantity,
-  setQuantity,
-  submitQuantity,
-}) => {
+export const AddToCartQuantity = ({ quantity, setQuantity, addToCart }) => {
   const onChange = (e) => {
     e.stopPropagation();
     e.preventDefault();
@@ -17,6 +13,12 @@ export const AddToCartQuantity = ({
       setQuantity(value);
     }
   };
+
+  const submitClick = (e) => {
+    preventEvent(e);
+    addToCart(quantity);
+  };
+
   return (
     <>
       <div className="absolute border rounded-lg flex flex-col items-center gap-2 top-2 left-2  py-2 px-4 bg-slate-900 ">
@@ -27,13 +29,7 @@ export const AddToCartQuantity = ({
           onClick={(e) => e.stopPropagation()}
           className="w-full"
         />
-        <div
-          onClick={(e) => {
-            preventEvent(e);
-            submitQuantity(quantity);
-          }}
-          className="btn submitButton btnSm"
-        >
+        <div onClick={submitClick} className="btn submitButton btnSm">
           Dodaj
         </div>
       </div>

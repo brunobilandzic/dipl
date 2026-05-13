2f802f65afbda96acf0258f85176ed549280d2e2
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-13 18:59:47 +0200

    Request base schema model

--- fvtracker/models/Base.js
+++ fvtracker/models/Base.js
@@ -1,6 +1,7 @@
 import { makeUrlFriendly } from "@/lib/utils/strings";
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
+import { PENDING, REQUEST_STATUSES } from "./sectors/interface/Request";
 
 const baseSchema = new Schema({
   name: {
@@ -24,6 +25,35 @@ const baseSchema = new Schema({
   },
 });
 
+const requestSchema = new Schema({
+  requestedBy: {
+    type: Schema.Types.ObjectId,
+    ref: "RootManager",
+    required: true,
+  },
+  requestedTo: {
+    type: Schema.Types.ObjectId,
+    ref: "RootManager",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: REQUEST_STATUSES,
+    default: PENDING,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  fullfilledAt: {
+    type: Date,
+    default: null,
+  },
+  slug: {
+    type: String,
+  },
+});
+
 baseSchema.index({ name: 1, slug: 1, __t: 1 }, { unique: true });
 baseSchema.pre("save", function () {
   this.updatedAt = new Date();
@@ -32,4 +62,12 @@ baseSchema.pre("save", function () {
   }
 });
 
+requestSchema.index({ slug: 1, __t: 1 }, { unique: true });
+requestSchema.pre("save", function () {
+  if (this.isModified("name") || this.isNew) {
+    this.slug = makeUrlFriendly(this.name);
+  }
+});
+
 export const Base = mongoose.models.Base || mongoose.model("Base", baseSchema);
+export const Request = mongoose.models.Request || mongoose.model("Request", requestSchema);

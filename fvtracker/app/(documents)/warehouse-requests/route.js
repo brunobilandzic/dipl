aa068c6d8c27e10b7dcd1f8432febd7049ea1dd1
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-13 19:44:11 +0200

    route error catch

--- fvtracker/app/(documents)/warehouse-requests/route.js
+++ fvtracker/app/(documents)/warehouse-requests/route.js
@@ -1,18 +1,22 @@
 import { fetchManager } from "@/lib/auth/fetchSessionData";
 import { FINANCIAL_MANAGER } from "@/lib/constants/users/managerTypes";
 
-export async function GET(request) { }
-export async function POST(request) { 
-    try {
-        const {specificManager, unauthorized} = await fetchManager({
-            managerNames:[FINANCIAL_MANAGER]
-        })
-        if(unauthorized){
-            return new Response(
-                JSON.stringify({ message: "Unauthorized" }),
-                { status: 401 }
-            );
-        }
-        
+export async function GET(request) {}
+export async function POST(request) {
+  try {
+    const { specificManager, unauthorized } = await fetchManager({
+      managerNames: [FINANCIAL_MANAGER],
+    });
+    if (unauthorized) {
+      return new Response(JSON.stringify({ message: "Unauthorized" }), {
+        status: 401,
+      });
     }
+  } catch (error) {
+    console.error("Error creating warehouse request:", error);
+    return new Response(
+      JSON.stringify({ message: "Greška prilikom izrade zahtjeva skladištu." }),
+      { status: 500 },
+    );
+  }
 }

401e8a3f28447bc91e04e034f48e58f8abdf225b
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-13 20:47:54 +0200

    move sendWarehouseRequest to file requests.js

--- fvtracker/lib/utils/documents/requests.js
+++ fvtracker/lib/utils/documents/requests.js
@@ -3,3 +3,20 @@ import { fetchWarehouseRequests } from "@/store/sales";
 export const fillWarehouseRequestsRedux = ({ dispatch }) => {
   dispatch(fetchWarehouseRequests());
 };
+
+export const sendWarehouseRequest = async ({
+  requestData,
+  dispatch,
+  router,
+}) => {
+  try {
+    const res = await api.post("/warehouse-requests", requestData);
+    return res.data;
+  } catch (error) {
+    console.error("Error sending warehouse request:", error);
+    handleError(
+      { ...error, customMessage: "Greška prilikom slanja zahtjeva skladištu." },
+      router,
+    );
+  }
+};

a082025bde352bbad63e20cdcba268d00acf3eb2
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-13 15:31:32 +0200

    OrderItem

--- fvtracker/components/store/financialManager/orders.js
+++ fvtracker/components/store/financialManager/orders.js
@@ -1,18 +1,40 @@
-"use client"
+"use client";
 import { List, ListItem } from "@/components/layout/preview/list";
+import { getName, showDate } from "@/lib/utils/display";
+import { useSelector } from "react-redux";
 
 export const OrdersList = () => {
   const orders = useSelector((state) => state.webstore.orders.filteredItems);
   return (
     <List title="Narudžbe">
       {orders?.map((order) => (
-        <OrderItem key={order._id} order={order} />
+        <OrderListItem key={order._id} order={order} />
       ))}
     </List>
   );
 };
 
-const OrderItem = ({ order }) => {
+const OrderListItem = ({ order }) => {
   console.log({ order });
-  return <ListItem>{order.number}</ListItem>;
+  return (
+    <ListItem>
+      <OrderItem order={order} />
+    </ListItem>
+  );
+};
+
+const OrderItem = ({ order }) => {
+  return (
+    <div>
+      <div className="listitemheader">Narudžba #{order.number} </div>
+      <div>
+        Kupac:{" "}
+        {getName({
+          name: order.customer.name,
+          surname: order.customer.surname,
+        })}
+      </div>
+      <div>{showDate(order.createdAt)}</div>
+    </div>
+  );
 };

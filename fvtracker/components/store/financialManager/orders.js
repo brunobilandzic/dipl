53bb25b713818249c4b65fe830c521ac93aee8a1
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-13 15:09:55 +0200

    basic OrdersList no list props

--- fvtracker/components/store/financialManager/orders.js
+++ fvtracker/components/store/financialManager/orders.js
@@ -1,4 +1,17 @@
+import { List, ListItem } from "@/components/layout/preview/list";
+
 export const OrdersList = () => {
-    const orders = useSelector((state) => state.webstore.orders.filteredItems);
-    
+  const orders = useSelector((state) => state.webstore.orders.filteredItems);
+  return (
+    <List title="Narudžbe">
+      {orders?.map((order) => (
+        <OrderItem key={order._id} order={order} />
+      ))}
+    </List>
+  );
+};
+
+const OrderItem = ({ order }) => {
+  console.log({ order });
+  return <ListItem>{order.number}</ListItem>;
 };

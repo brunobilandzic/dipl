910812b4c2755207cce2d7aa22cc2b5b6569ea24
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-13 20:59:39 +0200

    warehouseRequest state

--- fvtracker/components/financialManager/warehouseRequest.js
+++ fvtracker/components/financialManager/warehouseRequest.js
@@ -1,5 +1,15 @@
+import { useState } from "react";
 import { FormModal } from "../layout/modals/form";
 
 export const WarehouseRequestModal = ({ isOpen, onCancel, order }) => {
+  const initialWarehouseRequest = {
+    items: order.items,
+    orderNumber: order.number,
+    warehouseManagerId: null,
+    financialManagerId: null,
+  };
+  const [warehouseRequest, setWarehouseRequest] = useState(
+    initialWarehouseRequest,
+  );
   return <FormModal isOpen={isOpen} onCancel={onCancel}></FormModal>;
 };

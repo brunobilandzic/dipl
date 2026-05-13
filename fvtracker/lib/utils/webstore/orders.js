076fbf2e5e6becb14aa937cb63bcf4ab44218115
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-13 15:00:19 +0200

    fillOrdersRedux util implementatiopn

--- fvtracker/lib/utils/webstore/orders.js
+++ fvtracker/lib/utils/webstore/orders.js
@@ -1,5 +1,6 @@
 import api from "@/lib/api";
 import handleError from "@/lib/constants/errors/client/handleError";
+import { refreshOrdersThunk } from "@/store/webstore";
 
 export const sendOrder = async ({ customerData, cartItems }) => {
   try {
@@ -10,6 +11,18 @@ export const sendOrder = async ({ customerData, cartItems }) => {
     console.error("Greška pri slanju narudžbe:", error);
     handleError(
       { ...error, customMessage: "Greška pri slanju narudžbe" },
+      router,
+    );
+  }
+};
+
+export const fillOrdersRedux = async ({ dispatch }) => {
+  try {
+    dispatch(refreshOrdersThunk());
+  } catch (error) {
+    console.error("Greška pri dohvatanju narudžbi:", error);
+    handleError(
+      { ...error, customMessage: "Greška pri dohvatanju narudžbi" },
       router,
     );
   }

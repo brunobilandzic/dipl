17874a66e5e6cf4c33eca05112de7098077360ec
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-13 20:17:14 +0200

    wh req GET route check manager

--- fvtracker/app/api/(webstore)/warehouse-requests/route.js
+++ fvtracker/app/api/(webstore)/warehouse-requests/route.js
@@ -2,7 +2,27 @@ import { fetchManager } from "@/lib/auth/fetchSessionData";
 import { FINANCIAL_MANAGER } from "@/lib/constants/users/managerTypes";
 import { createWarehouseRequest } from "@/models/sectors/sales/warehouseRequests";
 
-export async function GET(request) {}
+export async function GET(request) {
+  console.log("Received GET request for warehouse requests");
+  try {
+    const { unauthorized } = await fetchManager({
+      managerNames: [FINANCIAL_MANAGER],
+    });
+    if (unauthorized) {
+      return new Response(JSON.stringify({ message: "Unauthorized" }), {
+        status: 401,
+      });
+    }
+  } catch (error) {
+    console.error("Error fetching warehouse requests:", error);
+    return new Response(
+      JSON.stringify({
+        message: "Greška prilikom dohvaćanja zahtjeva skladištu.",
+      }),
+      { status: 500 },
+    );
+  }
+}
 export async function POST(request) {
   try {
     const { unauthorized } = await fetchManager({

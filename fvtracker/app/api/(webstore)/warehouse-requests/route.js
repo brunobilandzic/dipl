e67c684abff58c2e25d4b4c5156e7a0622940f9b
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-14 17:00:52 +0200

    fill fin man id wh req

--- fvtracker/app/api/(webstore)/warehouse-requests/route.js
+++ fvtracker/app/api/(webstore)/warehouse-requests/route.js
@@ -31,16 +31,19 @@ export async function GET(request) {
 }
 export async function POST(request) {
   try {
-    const { unauthorized } = await fetchManager({
-      managerNames: [FINANCIAL_MANAGER],
-    });
+    const { unauthorized, specificManager: financialManager } =
+      await fetchManager({
+        managerNames: [FINANCIAL_MANAGER],
+      });
     if (unauthorized) {
       return Response.json(
         { message: "Nemate pravo pristupa" },
         { status: 403 },
       );
     }
+
     const requestData = await request.json();
+    requestData["financialManagerId"] = financialManager._id.toString();
     const warehouseRequest = await createWarehouseRequest(requestData);
     return Response.json({ message: "Zahtjev uspješno poslan skladištu." });
   } catch (error) {

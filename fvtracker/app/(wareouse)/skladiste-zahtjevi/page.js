05205038bb9ce8837031da55f635056743a052f2
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-14 19:31:34 +0200

    WarehouseRequestsPage

---
+++ fvtracker/app/(wareouse)/skladiste-zahtjevi/page.js
@@ -0,0 +1,12 @@
+import { WarehouseRequestList } from "@/components/financialManager/WarehouseRequestComponents";
+import React from "react";
+
+function WarehouseRequestsPage() {
+  return (
+    <div>
+      <WarehouseRequestList />
+    </div>
+  );
+}
+
+export default WarehouseRequestsPage;

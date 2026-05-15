b82580e967442a391eedde03e45cc7689c9ddfa7
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-15 16:55:12 +0200

    fin man remove unused schema fields

--- fvtracker/models/user/managers/FinancialManager.js
+++ fvtracker/models/user/managers/FinancialManager.js
@@ -9,18 +9,7 @@ const financialManagerSchema = new Schema({
     required: true,
   },
   orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order", default: [] }],
-  invoices: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "Invoice", default: [] },
-  ],
-  payments: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "Payment", default: [] },
-  ],
-  customers: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "Customer", default: [] },
-  ],
-  suppliers: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "Supplier", default: [] },
-  ],
+
 });
 
 export const FinancialManager =

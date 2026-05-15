a85a2466ea0f38c514a43ddf19d55332e7031790
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-15 16:23:18 +0200

    seed orders

---
+++ fvtracker/seed/sales/orders.js
@@ -0,0 +1,31 @@
+import { getCustomer } from "./customer";
+import { Product } from "@/models/sectors/production/Product";
+import { ordersSeedData } from "../data/sales/orders";
+import { Order } from "@/models/sectors/sales";
+
+export const createOrders = async () => {
+  await Order.deleteMany({}); // Clear existing orders
+  console.log("Creating orders...");
+
+  const customer = await getCustomer();
+  const items = await buildOrderItems();
+
+  for (const orderData of ordersSeedData) {
+    const newOrder = await Order.create({
+      ...orderData,
+      customer: customer._id,
+      items,
+    });
+  }
+};
+
+const buildOrderItems = async () => {
+  const product = await Product.findOne({}).select("name");
+
+  console.log(`Adding ${product.name} to order items...`);
+
+  return {
+    product: product._id,
+    quantity: 1,
+  };
+};

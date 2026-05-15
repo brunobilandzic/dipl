098a2b7abda5a6ecf63eba00ce95b9231430bd2e
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-15 17:45:18 +0200

    delete all documents sales seed

--- fvtracker/seed/sales/index.js
+++ fvtracker/seed/sales/index.js
@@ -1,10 +1,22 @@
+import { Order } from "@/models/sectors/sales";
 import { createOrders } from "./orders";
 import { createWarehouseRequests } from "./warehouseRequests";
+import { WarehouseRequest } from "@/models/documents/requests/WarehouseRequest";
+import { Customer } from "@/models/user/Customer";
 
 export default {
   seedSales: async () => {
+    await deleteDB();
     const orders = await createOrders();
     // create req for only one order
     await createWarehouseRequests({ orders: [orders[0]] });
   },
+};
+
+const deleteDB = async () => {
+  await Promise.all([
+    Order.deleteMany({}),
+    WarehouseRequest.deleteMany({}),
+    Customer.deleteMany({}),
+  ]);
 };

a54992413d27999e222b79225af613a48b2cfa91
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-18 15:17:25 +0200

    fillWarehouseRequest order item adjustment

--- fvtracker/lib/warehouses/warehouseRequests.js
+++ fvtracker/lib/warehouses/warehouseRequests.js
@@ -2,6 +2,7 @@ import { WarehouseRequest } from "@/models/documents/requests/WarehouseRequest";
 import { populateIngredientsConfig } from "../production/product/ingredients";
 import { Warehouse } from "@/models/sectors/storage/Warehouse";
 import { Shipment, ShipmentItem } from "@/models/sectors/sales/Shipment";
+import { getWarehouse } from "./get";
 
 export const getWarehouseRequestById = async (id) => {
   const request = await WarehouseRequest.findById(id);
@@ -51,19 +52,24 @@ export const fillWarehouseRequest = async ({
   warehouseRequest.populate([
     {
       path: "order",
+      populate: {
+        path: "items.product",
+      },
     },
   ]);
   warehouseRequest.shipments.push(shipment._id);
 
   for (const source of shipmentSources) {
     const { warehouseId, productName, quantity } = source;
-    const warehouse = await Warehouse.findById(warehouseId);
+
+    const warehouse = getWarehouse({ id: warehouseId });
     await warehouse.populate({
       path: "stocks",
       populate: {
         path: "product",
       },
     });
+
     const stock = warehouse.stocks.find(
       (whs) => whs.product.name == source.productName,
     );
@@ -73,17 +79,23 @@ export const fillWarehouseRequest = async ({
       );
       continue;
     }
+
     const product = stock.product;
+
+    const orderItem = warehouseRequest.order.items.find(
+      (oi) => oi.product.name === productName,
+    );
 
     const shipmentItem = new ShipmentItem({
       product: product._id,
       quantity,
       shipment: shipment._id,
       warehouseStock: stock._id,
+      orderItem: orderItem._id,
     });
 
+    orderItem.shipmentItems.push(shipmentItem._id);
     shipment.shipmentItems.push(shipmentItem._id);
-    product.shipmentItems.push(shipmentItem._id);
 
     stock.quantity -= quantity;
     if (stock.quantity < 0) {
@@ -94,7 +106,6 @@ export const fillWarehouseRequest = async ({
     }
     stock.shipmentItems.push(shipmentItem._id);
 
-    await product.save();
     await stock.save();
     await shipmentItem.save();
   }

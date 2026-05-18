acdf9586e28b8f09ddb5ef5b63d089171d1837bf
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-18 13:39:59 +0200

    route try catch

--- fvtracker/app/api/(webstore)/warehouse-requests/fill/route.js
+++ fvtracker/app/api/(webstore)/warehouse-requests/fill/route.js
@@ -3,14 +3,23 @@ import { WAREHOUSE_MANAGER } from "@/lib/constants/users/managerTypes";
 import { fillWarehouseRequest } from "@/lib/warehouses/warehouseRequests";
 
 export async function POST(request) {
-  const { specificManager, generalManager, unauthorized } = await fetchManager({
-    managerNames: [WAREHOUSE_MANAGER],
-  });
-  if (!specificManager || unauthorized) {
-    return Response.json({ message: "Unauthorized" }, { status: 403 });
-  }
-  const { warehouseRequestId, shipmentSources } = await request.json();
-  await fillWarehouseRequest({ warehouseRequestId, shipmentSources });
+  try {
+    const { specificManager, generalManager, unauthorized } =
+      await fetchManager({
+        managerNames: [WAREHOUSE_MANAGER],
+      });
+    if (!specificManager || unauthorized) {
+      return Response.json({ message: "Unauthorized" }, { status: 403 });
+    }
+    const { warehouseRequestId, shipmentSources } = await request.json();
+    await fillWarehouseRequest({ warehouseRequestId, shipmentSources });
 
-  return Response.json({ message: "Warehouse request filled successfully" });
+    return Response.json({ message: "Warehouse request filled successfully" });
+  } catch (error) {
+    console.error("Error filling warehouse request:", error);
+    return Response.json(
+      { message: "Error filling warehouse request" },
+      { status: 500 },
+    );
+  }
 }

b94dd14a7dfc6f79df942f4dfbab9d219e422ab9
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-18 13:35:31 +0200

    check warehouse manager ship route

--- fvtracker/app/api/(webstore)/warehouse-requests/fill/route.js
+++ fvtracker/app/api/(webstore)/warehouse-requests/fill/route.js
@@ -1,4 +1,15 @@
+import { fetchManager } from "@/lib/auth/fetchSessionData";
+import {
+  WAREHOUSE_MANAGER,
+} from "@/lib/constants/users/managerTypes";
+
 export async function POST(request) {
+  const { specificManager, generalManager, unauthorized } = await fetchManager({
+    managerNames: [WAREHOUSE_MANAGER],
+  });
+  if (!specificManager || unauthorized) {
+    return Response.json({ message: "Unauthorized" }, { status: 403 });
+  }
   const { warehouseRequestId, shipmentSources } = await request.json();
   console.log({ warehouseRequestId, shipmentSources });
 

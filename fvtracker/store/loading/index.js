e054ce4c359d818610572916fbe9da756abab913
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-18 16:51:36 +0200

    seed loading redux state

--- fvtracker/store/loading/index.js
+++ fvtracker/store/loading/index.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   isLoading: false,
+  seedLoading: false,
 };
 
 const loadingSlice = createSlice({
@@ -12,8 +13,13 @@ const loadingSlice = createSlice({
       console.log("Setting loading state to", action.payload);
       state.isLoading = action.payload;
     },
+    setSeedLoading: (state, action) => {
+      console.log("Setting seed loading state to", action.payload);
+      state.seedLoading = action.payload;
+    },
+
   },
 });
 
-export const { setLoading } = loadingSlice.actions;
+export const { setLoading, setSeedLoading } = loadingSlice.actions;
 export default loadingSlice.reducer;

e8c885ed46a6f6e433b988d636237072191bd932
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-22 20:25:21 +0200

    shimpment seed

--- fvtracker/seed/sales/shipment.js
+++ fvtracker/seed/sales/shipment.js
@@ -1,13 +1,81 @@
-import { WarehouseRequest } from "@/models/documents/requests/WarehouseRequest";
-import { Shipment } from "@/models/sectors/sales";
+import { warehouseRequestPopulateShipmentItems } from "@/lib/utils/storage/warehouse";
+import { fillWarehouseRequest } from "@/lib/warehouses/warehouseRequests";
+import { Warehouse } from "@/models/sectors/storage/Warehouse";
 
-export const createShipments = async ({}) => {
-  await Shipment.deleteMany({});
-  const warehouseRequests = await WarehouseRequest.find({}).select("_id");
-  for (const request of warehouseRequests) {
-    const shipment = new Shipment({
-      warehouseRequest: request._id,
+export const createShipments = async ({ warehouseRequests }) => {
+  for (const warehouseRequest of warehouseRequests) {
+    await warehouseRequest.populate(warehouseRequestPopulateShipmentItems);
+    const orderItems = warehouseRequest.order.items;
+    await fillWarehouseRequest({
+      warehouseRequestId: warehouseRequest._id,
+      shipmentSources: buildShipmentSources({
+        orderItems,
+        warehouseManagerId: warehouseRequest.warehouseManager,
+      }),
     });
-    await shipment.save();
   }
+};
+
+const buildShipmentSources = ({ orderItems, warehouseManagerId }) => {
+  const sources = [];
+
+  for (const oi of orderItems) {
+    const shipmentSource = {
+      productName: oi.product.name,
+      quantity: oi.quantity,
+    };
+    const warehouse = findWarehouseForShipmentSource({
+      shipmentSource,
+      warehouseManagerId,
+    });
+
+    console.log({ warehouse });
+
+    sources.push({
+      productName: oi.product.name,
+      quantity: oi.quantity,
+      warehouse,
+    });
+  }
+  return sources;
+};
+
+const findWarehouseForShipmentSource = async ({
+  shipmentSource,
+  warehouseManagerId,
+}) => {
+  const warehouses = await Warehouse.find({
+    warehouseManager: warehouseManagerId,
+  }).populate({
+    path: "stocks",
+    populate: {
+      path: "product",
+    },
+  });
+
+  const stocks = warehouses.reduce((acc, warehouse) => {
+    return [...acc, ...warehouse.stocks];
+  }, []);
+
+  const stocksWithQuantity = stocks.filter(
+    (stock) =>
+      stock.product.name === shipmentSource.productName &&
+      shipmentSource.quantity <= stock.quantity,
+  );
+
+  if (stocksWithQuantity.length === 0) {
+    console.log({
+      error: `Nema dovoljno zaliha za proizvod ${shipmentSource.productName}`,
+      shipmentSource,
+      stocks: stocks.map((s) => ({
+        product: s.product.name,
+        quantity: s.quantity,
+      })),
+    });
+    throw new Error(
+      `Nema dovoljno zaliha za proizvod ${shipmentSource.productName}`,
+    );
+  }
+
+  return stocksWithQuantity[0].warehouse;
 };

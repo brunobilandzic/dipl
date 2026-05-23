c6ddf6fd87bf4a2c162a577c72e1a74191e2d806
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-23 12:59:24 +0200

    prettify

--- fvtracker/components/warehouse/shipment/list.js
+++ fvtracker/components/warehouse/shipment/list.js
@@ -1,7 +1,9 @@
 import { useSelector } from "react-redux";
 
 export const ShipmentList = ({}) => {
-  const shipments = useSelector((state) => state.webstore.shipments.filteredItems);
+  const shipments = useSelector(
+    (state) => state.webstore.shipments.filteredItems,
+  );
   return (
     <div>
       {shipments.map((shipment) => (

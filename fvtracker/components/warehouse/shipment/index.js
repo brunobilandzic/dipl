ee12ea957a794165910bfe8ceda890fdd3d0fdfd
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-23 12:45:33 +0200

    ship index

--- fvtracker/components/warehouse/shipment/index.js
+++ fvtracker/components/warehouse/shipment/index.js
@@ -1,12 +1,22 @@
-import { showDate } from "@/lib/utils/display";
+"use client";
+
+import { ListItem } from "@/components/layout/preview/list";
+import { FINANCIAL_MANAGER } from "@/lib/constants/users/managerTypes";
+import { showDate, showDateTime } from "@/lib/utils/display";
 import { sortItems } from "@/lib/utils/list";
 import { notFound } from "next/navigation";
 import React from "react";
+import { useSelector } from "react-redux";
 
 function ShipmentPageComponent({ shipment }) {
   if (!shipment) {
     return notFound();
   }
+
+  const managerModelName = useSelector(
+    (state) => state.user.session.managerModelName,
+  );
+  console.log({ managerModelName });
 
   const { warehouseRequest, status, shipmentItems } = shipment;
   const { order } = warehouseRequest || {};
@@ -24,7 +34,10 @@ function ShipmentPageComponent({ shipment }) {
         <div>NARUDŽBA: {order?.number}</div>
         <div>STATUS: {shipment?.status}</div>
         <div className="mt-4 pl-12">
-          <ShipmentItemList shipmentItems={shipmentItems} />
+          <ShipmentItemList
+            managerModelName={managerModelName}
+            shipmentItems={shipmentItems}
+          />
         </div>
       </div>
     </div>
@@ -33,7 +46,7 @@ function ShipmentPageComponent({ shipment }) {
 
 export default ShipmentPageComponent;
 
-const ShipmentItemList = ({ shipmentItems }) => {
+const ShipmentItemList = ({ managerModelName, shipmentItems }) => {
   const sortedShipmentItems = sortItems({
     items: shipmentItems,
     sortBy: "createdAt",
@@ -42,34 +55,62 @@ const ShipmentItemList = ({ shipmentItems }) => {
   return (
     <div>
       {sortedShipmentItems?.map((si) => (
-        <ShipmentItem key={si._id} shipmentItem={si} />
+        <ShipmentItem
+          key={si._id}
+          shipmentItem={si}
+          managerModelName={managerModelName}
+        />
       ))}
     </div>
   );
 };
 
-const ShipmentItem = ({ shipmentItem }) => {
-  const { sources, receipt } = shipmentItem;
+const ShipmentItem = ({ shipmentItem, managerModelName }) => {
+  const { sources, receipt, createdAt, _id } = shipmentItem;
+
+  const actionOptions = [
+    ...(managerModelName === FINANCIAL_MANAGER && !receipt
+      ? [
+          {
+            label: "Izradi račun",
+            onClick: (e) => {
+              e.stopPropagation();
+              console.log("Creating receipt for shipment item", shipmentItem);
+            },
+            className: "submitButton",
+          },
+        ]
+      : []),
+  ];
   return (
-    <div>
+    <ListItem
+      key={_id}
+      actionOptions={actionOptions}
+      title={`Pošiljka napravljena ${showDateTime(createdAt)}`}
+    >
       <div>
-        <div>Pošiljka {showDate(shipmentItem.createdAt)}</div>
+        <strong>Stavke:</strong>
         <SourceList sources={sources} />
+        <div></div>
       </div>
-    </div>
+    </ListItem>
   );
 };
 
 const SourceList = ({ sources }) => {
   return (
-    <div className="flex flex-col w-full bg-yellow-900">
-      {sources.map((source) => (
-        <div className="border p-4 rounded-xl w-1/6 min-w-fit" key={source._id}>
-          <div>PROIZVOD: {source.product.name}</div>
-          <div>KOLIČINA: {source.quantity}</div>
-          <div>SKLADIŠTE: {source.warehouseStock.warehouse.name}</div>
-        </div>
-      ))}
+    <div className="flex flex-col w-full mt-2 gap-4">
+      {sources.map((source) => {
+        return (
+          <ListItem key={source._id}>
+            <div className=" p-4 rounded-xl w-1/6 min-w-fit" key={source._id}>
+              <div>PROIZVOD: {source.product.name}</div>
+              <div>KOLIČINA: {source.quantity}</div>
+              <div>SKLADIŠTE: {source.warehouseStock.warehouse.name}</div>
+            </div>
+          </ListItem>
+        );
+      })}
     </div>
   );
 };

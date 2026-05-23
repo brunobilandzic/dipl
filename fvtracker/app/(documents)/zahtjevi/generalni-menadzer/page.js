c98efc8ecc473a60060b7eddf8b7207f0fd514c7
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-23 18:28:27 +0200

    gen man req page find and pass

--- fvtracker/app/(documents)/zahtjevi/generalni-menadzer/page.js
+++ fvtracker/app/(documents)/zahtjevi/generalni-menadzer/page.js
@@ -3,6 +3,9 @@ import dbConnect from "@/lib/db/mongooseConnect";
 import { fetchAdmin } from "@/lib/auth/fetchSessionData";
 import { GeneralManagerRequest } from "@/models/documents/requests/RoleRequest";
 import { RoleRequestItem } from "@/components/generalManager/requests";
+import { ROLE_STATUSES } from "@/lib/constants/users";
+import { GeneralManagerRequestComponent } from "@/components/auth/genmanreq";
+import { sanitize } from "@/lib/utils/objects";
 
 async function GeneralManagerRequestsPage() {
   await dbConnect();
@@ -12,15 +15,37 @@ async function GeneralManagerRequestsPage() {
     return Response.json({ error: "Unauthorized" }, { status: 403 });
   }
 
-  const generalManagerRequest = await GeneralManagerRequest.findOne({
-    admin: admin._id,
-  }).populate("appUser", "name surname email");
+  const generalManagerRequest = await GeneralManagerRequest.findOne(
+    {},
+  ).populate([
+    {
+      path: "generalManager",
+      populate: {
+        path: "rootManager",
+        populate: {
+          path: "appUser",
+          select: "email name surname",
+        },
+      },
+    },
+  ]);
 
+  const approveRequest = async () => {
+    if (!generalManagerRequest) {
+      console.error("No General Manager Request found");
+      return;
+    }
+    generalManagerRequest.status = ROLE_STATUSES.APPROVED;
+    await generalManagerRequest.save();
+  };
+
+  const appUser = generalManagerRequest?.generalManager?.rootManager?.appUser;
+  const approved = generalManagerRequest?.status === ROLE_STATUSES.APPROVED;
   return (
     <div>
-      <div>
-        <div>{JSON.stringify(generalManagerRequest)}</div>
-      </div>
+      <GeneralManagerRequestComponent
+        request={sanitize(generalManagerRequest)}
+      />
     </div>
   );
 }

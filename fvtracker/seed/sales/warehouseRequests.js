8e81eb4aa6c5c747b8ed8a2334a200ec9c655a9f
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-23 09:11:10 +0200

    create wh req order state and mans push

--- fvtracker/seed/sales/warehouseRequests.js
+++ fvtracker/seed/sales/warehouseRequests.js
@@ -2,10 +2,15 @@ import { WarehouseRequest } from "@/models/documents/requests/WarehouseRequest";
 import { FinancialManager } from "@/models/user/managers/FinancialManager";
 import { WarehouseManager } from "@/models/user/managers/WarehouseManager";
 import { createShipments } from "./shipment";
+import { WAREHOUSE_REQUESTED } from "@/lib/constants/webstore/orders";
 
 export const createWarehouseRequests = async ({ orders }) => {
-  const financialManager = await FinancialManager.findOne({}).select("_id");
-  const warehouseManager = await WarehouseManager.findOne({}).select("_id");
+  const financialManager = await FinancialManager.findOne({}).select(
+    "_id warehouseRequests ",
+  );
+  const warehouseManager = await WarehouseManager.findOne({}).select(
+    "_id warehouseRequests ",
+  );
   const createdWarehouseRequests = [];
   for (const order of orders) {
     const warehouseRequest = new WarehouseRequest({
@@ -13,10 +18,21 @@ export const createWarehouseRequests = async ({ orders }) => {
       financialManager: financialManager._id,
       warehouseManager: warehouseManager._id,
     });
+
+    financialManager.warehouseRequests.push(warehouseRequest._id);
+    warehouseManager.warehouseRequests.push(warehouseRequest._id);
+
+    await financialManager.save();
+    await warehouseManager.save();
+
+    order.warehouseRequest = warehouseRequest._id;
+    order.state = WAREHOUSE_REQUESTED;
+    await order.save();
     await warehouseRequest.save();
     createdWarehouseRequests.push(warehouseRequest);
   }
 
   await createShipments({ warehouseRequests: createdWarehouseRequests });
+  console.log({ whrs: createdWarehouseRequests.length });
   return createdWarehouseRequests;
 };

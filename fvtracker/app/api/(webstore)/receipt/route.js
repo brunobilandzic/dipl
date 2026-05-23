3d589ee359b79a073db59b482015d76396fbb655
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-23 12:59:10 +0200

    create receipt route

--- fvtracker/app/api/(webstore)/receipt/route.js
+++ fvtracker/app/api/(webstore)/receipt/route.js
@@ -1,5 +1,7 @@
 import { fetchManager } from "@/lib/auth/fetchSessionData";
 import { FINANCIAL_MANAGER } from "@/lib/constants/users/managerTypes";
+import { Receipt } from "@/models/sectors/sales";
+import { ShipmentItem } from "@/models/sectors/sales/Shipment";
 
 export async function POST(req) {
   try {
@@ -10,6 +12,28 @@ export async function POST(req) {
     if (unauthorized) {
       return Response.json({ error: "Nema dozvole" }, { status: 403 });
     }
+
+    const { shipmentItemId } = await req.json();
+
+    const shipmentItem = await ShipmentItem.findById(shipmentItemId);
+
+    if (!shipmentItem) {
+      return Response.json(
+        { error: "Stavka pošiljke nije pronađena" },
+        { status: 404 },
+      );
+    }
+
+    const newReceipt = new Receipt({
+      shipmentItem: shipmentItem._id,
+    });
+
+    shipmentItem.receipt = newReceipt._id;
+
+    await newReceipt.save();
+    await shipmentItem.save();
+
+    return Response.json({ message: "Račun uspješno kreiran" });
   } catch (error) {
     console.error("Error creating receipt:", error);
     return Response.json({

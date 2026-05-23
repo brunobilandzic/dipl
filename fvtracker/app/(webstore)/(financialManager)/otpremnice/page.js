5464a07dba0dadffaa4c40c1c5e24cb59a3cbfc8
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-23 11:04:18 +0200

    shipments page, renders wh req list

---
+++ fvtracker/app/(webstore)/(financialManager)/otpremnice/page.js
@@ -0,0 +1,12 @@
+import { WarehouseRequestList } from "@/components/warehouse/warehouseRequest/WarehouseRequestComponents";
+import React from "react";
+
+function ShipmentsPage() {
+  return (
+    <div>
+      <WarehouseRequestList />
+    </div>
+  );
+}
+
+export default ShipmentsPage;

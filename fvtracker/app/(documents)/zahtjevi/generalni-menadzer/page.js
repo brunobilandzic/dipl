d35a97938cb026be296ea68daa506428902dc62e
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-23 19:13:41 +0200

    case no reqt

--- fvtracker/app/(documents)/zahtjevi/generalni-menadzer/page.js
+++ fvtracker/app/(documents)/zahtjevi/generalni-menadzer/page.js
@@ -2,10 +2,7 @@ import React from "react";
 import dbConnect from "@/lib/db/mongooseConnect";
 import { fetchAdmin } from "@/lib/auth/fetchSessionData";
 import { GeneralManagerRequest } from "@/models/documents/requests/RoleRequest";
-import { RoleRequestItem } from "@/components/generalManager/requests";
-import { ROLE_STATUSES } from "@/lib/constants/users";
 import { GeneralManagerRequestComponent } from "@/components/auth/genmanreq";
-import { sanitize } from "@/lib/utils/objects";
 
 async function GeneralManagerRequestsPage() {
   await dbConnect();
@@ -13,6 +10,18 @@ async function GeneralManagerRequestsPage() {
 
   if (unauthorized) {
     return Response.json({ error: "Unauthorized" }, { status: 403 });
+  }
+
+  const count = await GeneralManagerRequest.countDocuments();
+  if (count === 0) {
+    return (
+      <div className="p-4">
+        <h1 className="text-2xl font-bold mb-4">
+          Zahtevi za generalnog menadžera
+        </h1>
+        <p className="">Trenutno nema zahteva za generalnog menadžera.</p>
+      </div>
+    );
   }
 
   return (

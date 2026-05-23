acfdba0cbe377d3594ae89215889f3668daa9afa
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-23 11:04:45 +0200

    ShipmentPageComponent declaration shipment log

---
+++ fvtracker/components/warehouse/shipment/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ShipmentPageComponent({ shipment }) {
+  console.log({ shipmentPage: shipment });
+  return <div>ShipmentPageComponent</div>;
+}
+
+export default ShipmentPageComponent;

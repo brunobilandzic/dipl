68f884c74537c09b8397c758dbc745b444630e45
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-23 18:28:39 +0200

    approve req route

--- fvtracker/app/api/(admin)/genman-requests/route.js
+++ fvtracker/app/api/(admin)/genman-requests/route.js
@@ -1,3 +1,7 @@
+import { fetchAdmin } from "@/lib/auth/fetchSessionData";
+import { ROLE_STATUSES } from "@/lib/constants/users";
+import { GeneralManagerRequest } from "@/models/documents/requests/RoleRequest";
+
 export async function GET(request) {
   try {
     const { unauthorized, admin } = await fetchSessionData({
@@ -16,6 +20,28 @@ export async function GET(request) {
     console.error("Error fetching general manager requests:", error);
     return Response.json(
       { error: "Greška prilikom dohvatanja zahteva za generalnog menadžera" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(request) {
+  try {
+    const { unauthorized, admin } = await fetchAdmin({
+      requireAdmin: true,
+    });
+    if (unauthorized) {
+      return Response.json({ error: "Unauthorized" }, { status: 403 });
+    }
+    const request = await GeneralManagerRequest.findOne();
+    request.status = ROLE_STATUSES.APPROVED;
+    await request.save();
+    console.log({ request });
+    return Response.json({ message: "General manager request approved" });
+  } catch (error) {
+    console.error("Error approving general manager request:", error);
+    return Response.json(
+      { error: "Greška prilikom odobravanja zahteva za generalnog menadžera" },
       { status: 500 },
     );
   }

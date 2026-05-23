65a381ecfe814c427606ad88381a0fd2d87b5f80
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-23 12:07:45 +0200

    db connect sanitize shipment

--- fvtracker/app/(webstore)/(financialManager)/otpremnice/[id]/page.js
+++ fvtracker/app/(webstore)/(financialManager)/otpremnice/[id]/page.js
@@ -1,12 +1,17 @@
 import ShipmentPageComponent from "@/components/warehouse/shipment";
+import dbConnect from "@/lib/db/mongooseConnect";
+import { getShipmentById } from "@/lib/shipment/get";
+import { sanitize } from "@/lib/utils/objects";
 import React from "react";
 
 async function ShipmentPage({ params }) {
+  await dbConnect();
   const { id } = await params;
   const shipment = await getShipmentById({ id });
+  console.log();
   return (
     <div>
-      <ShipmentPageComponent shipment={shipment} />
+      <ShipmentPageComponent shipment={sanitize(shipment)} />
     </div>
   );
 }

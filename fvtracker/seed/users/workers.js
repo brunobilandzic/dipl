190de54bd62230e83f15e1561842617ebb97269e
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-24 17:18:41 +0200

    workers seed gile

---
+++ fvtracker/seed/users/workers.js
@@ -0,0 +1,20 @@
+import { AppUser } from "@/models/user/AppUser";
+import { workersJson } from "../data/appUsers";
+
+export const seedWorkers = async () => {
+  for (const workerData of workersJson) {
+    const appUser = new AppUser(workerData);
+    await appUser.save();
+    console.log(`Seeded worker: ${appUser.username}`);
+
+    switch (workerData.modelName) {
+      case "CultivationWorker":
+        const cultivationWorker = new CultivationWorker({
+          appUser: appUser._id,
+        });
+        await cultivationWorker.save();
+        console.log(`Created CultivationWorker for ${appUser.username}`);
+        break;
+    }
+  }
+};

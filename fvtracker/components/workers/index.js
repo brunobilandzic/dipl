cfc862914b852fe5db1edb4dfa4928b1c827e1d7
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-24 21:47:00 +0200

    w comp use rdx

--- fvtracker/components/workers/index.js
+++ fvtracker/components/workers/index.js
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 export const WorkersPageComponent = ({ managerModelName }) => {
-  const workers = useSelector((state) => state.workers.workers);
+  const workers = useSelector((state) => state.workers);
 
   useEffect(() => {
     if (workers) return;

511a48feb1dbe4a11ee582ee1483e63e9a337b18
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-24 21:45:56 +0200

    workers slice

---
+++ fvtracker/store/workers.js
@@ -0,0 +1,60 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+  filteredItems: [],
+  isLoading: false,
+};
+
+const workersSlice = createSlice({
+  name: "workers",
+  initialState,
+  reducers: {
+    setWorkers: (state, action) => {
+      state.items = action.payload;
+    },
+    setFilteredWorkers: (state, action) => {
+      state.filteredItems = action.payload;
+    },
+    setLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWorkers.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchWorkers.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.filteredItems = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchWorkers.rejected, (state, action) => {
+        state.isLoading = false;
+      });
+  },
+});
+
+// thunks
+
+export const fetchWorkers = createAsyncThunk(
+  "workers/fetchWorkers",
+  async (managerModelName, { dispatch }) => {
+    // Simulate an API call
+    const response = await api.get(`/workers`, {
+      params: {
+        managerModelName,
+      },
+    });
+    return response.data;
+  },
+);
+
+export const { setWorkers, setFilteredWorkers, setLoading, setError } =
+  workersSlice.actions;
+
+export default workersSlice.reducer;

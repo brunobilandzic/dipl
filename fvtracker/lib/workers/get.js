549f6d5d881ca115c402147e6681f2a9fbbe78a6
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-25 22:38:37 +0200

    populate if manager model name cult manager

--- fvtracker/lib/workers/get.js
+++ fvtracker/lib/workers/get.js
@@ -1,9 +1,23 @@
+import { CULTIVATION_MANAGER } from "../constants/users/managerTypes.js";
 import Worker from "../models/Worker.js";
 
-export const getWorkers = async ({ rootManagerId }) => {
+export const getWorkers = async ({ rootManagerId, managerModelName }) => {
   const workers = await Worker.find({
     rootManager: rootManagerId,
   }).populate("manager");
 
-  return workers;
+  switch (managerModelName) {
+    case CULTIVATION_MANAGER:
+      await workers.populate([
+        {
+          path: "plantages",
+        },
+        {
+          path: "harvests",
+        },
+      ]);
+      return workers;
+    default:
+      return workers;
+  }
 };

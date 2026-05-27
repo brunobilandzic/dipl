393c5d4ac33924ec7dfa7908726a5111b3ee2451
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-27 18:29:12 +0200

    WorkerListPage

---
+++ fvtracker/app/(workers)/radnici/lista/page.js
@@ -0,0 +1,12 @@
+import { WorkersList } from "@/components/workers/manage/list";
+import React from "react";
+
+function WorkerListPage() {
+  return (
+    <div>
+      <WorkersList />
+    </div>
+  );
+}
+
+export default WorkerListPage;

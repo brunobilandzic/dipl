74ce57690e9e4a4825f7e5a43d69e71e0e72791d
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-27 19:04:55 +0200

    show comp on worker create page

---
+++ fvtracker/app/(workers)/radnici/zaposlie/page.js
@@ -0,0 +1,12 @@
+import { CreateWorker } from "@/components/workers/manage/create";
+import React from "react";
+
+function CreateWorkerPage() {
+  return (
+    <div>
+      <CreateWorker />
+    </div>
+  );
+}
+
+export default CreateWorkerPage;

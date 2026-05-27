3c72e8296d1efa2d7a17ec96b8eebafaaf5a6bc9
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-27 19:14:24 +0200

    on create item create worker

--- fvtracker/components/workers/index.js
+++ fvtracker/components/workers/index.js
@@ -1,22 +1,25 @@
 "use client";
 
 import { LoadingFullScreen } from "@/components/layout/loading";
-import { fetchWorkers } from "@/lib/utils/workers/api";
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { List, ListItem } from "../layout/preview/list";
+import { useRouter } from "next/navigation";
 
 export const WorkersPageComponent = ({ managerModelName }) => {
   const workersState = useSelector((state) => state.workers);
   const { items: workers, isLoading } = workersState;
+  const router = useRouter();
 
   if (!workers) return <LoadingFullScreen />;
 
   return (
     <div>
-      <div>Vaš sektor ima zaposlena {workers.length} radnika.</div>
       <div>
-        <List title="Radnici">
+        <List
+          title="Radnici"
+          addLabel="Dodaj radnika"
+          onCreateItem={() => router.push("/radnici/izradi")}
+        >
           {workers.map((worker) => (
             <WorkerItem key={worker._id} worker={worker} />
           ))}

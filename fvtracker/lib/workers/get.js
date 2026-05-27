92326ea253ac20b3db9244e1f5d625f0ada2f57c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-27 20:28:31 +0200

    populate workers

--- fvtracker/lib/workers/get.js
+++ fvtracker/lib/workers/get.js
@@ -1,10 +1,11 @@
 import { CULTIVATION_MANAGER } from "../constants/users/managerTypes.js";
 import { Worker } from "@/models/user/workers";
+import populateConfig from "./populate";
 
 export const getWorkers = async ({ rootManagerId, managerModelName }) => {
   const workers = await Worker.find({
     manager: rootManagerId,
-  }).populate("manager");
+  }).populate(populateConfig);
   console.log({ rootManagerId, managerModelName });
   console.log("Fetched workers for rootManagerId:", rootManagerId, { workers });
   for (const worker of workers) {

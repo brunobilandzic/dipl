f74595004a2717ecf6cc70de6e4587d1696d62fc
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-27 20:47:13 +0200

    populate cultivation worker

--- fvtracker/lib/workers/populate.js
+++ fvtracker/lib/workers/populate.js
@@ -1,11 +1,29 @@
+import { populateConfigCropVariety } from "../cultivation/populate";
+
 export default [
   {
     path: "appUser",
   },
   {
     path: "harvestWorks",
+    populate: [
+      {
+        path: "harvestingBatchItem",
+        populate: { path: "cropVariety", populate: populateConfigCropVariety },
+      },
+    ],
   },
   {
     path: "plantageWorks",
+    populate: {
+      path: "plantage",
+      select: "cultivation plantageItems",
+      populate: [
+        {
+          path: "cultivation",
+          select: "name",
+        },
+      ],
+    },
   },
 ];

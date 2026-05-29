f28b1525aff1d76a668415ab653f937064d738c8
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-29 12:26:20 +0200

    delete workers before seed

--- fvtracker/seed/users/workers.js
+++ fvtracker/seed/users/workers.js
@@ -2,12 +2,15 @@ import { AppUser } from "@/models/user/AppUser";
 import { workersJson } from "../data/appUsers";
 import { CultivationWorker } from "@/models/user/workers/CultivationWork";
 import { CultivationManager } from "@/models/user/managers/CultivationManager";
+import dbConnect from "@/lib/db/mongooseConnect";
+import { Worker } from "@/models/user/workers";
 
 export const seedWorkers = async () => {
+  await dbConnect();
+  await Worker.deleteMany({});
   for (const workerData of workersJson) {
     const appUser = new AppUser(workerData);
     await appUser.save();
-    console.log(`Seeded worker: ${appUser.username}`);
 
     switch (workerData.modelName) {
       case "CultivationWorker":

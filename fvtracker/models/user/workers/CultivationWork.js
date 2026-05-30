312290864b376716e7be5232a36e44d156e58a80
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-30 11:38:19 +0200

    bring baxk platn hravest works

--- fvtracker/models/user/workers/CultivationWork.js
+++ fvtracker/models/user/workers/CultivationWork.js
@@ -19,6 +19,107 @@ const cultivationWorkerSchema = new Schema({
   ],
 });
 
+const plantageWorkSchema = new Schema(
+  {
+    plantingPlanItem: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "PlantingPlanItem",
+      required: true,
+    },
+    cultivation: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Cultivation",
+    },
+    relativeCoords: {
+      type: String,
+      default: null,
+      validate: (coords) => {
+        if (coords && !utils.strings.testCoordinates(coords)) {
+          throw new Error(
+            "Invalid cell coordinates format. Expected format: 'x,y'",
+          );
+        }
+      },
+    },
+  },
+  { timestamps: true },
+);
+
+plantageWorkSchema.pre("save", async function () {
+  if (this.isNew) {
+    const plantingPlanItem = await mongoose
+      .model("PlantingPlanItem")
+      .findById(this.plantingPlanItem)
+      .populate("cropVariety");
+    if (plantingPlanItem) {
+      plantingPlanItem.plantageWorks.push(this._id);
+      await plantingPlanItem.save();
+    } else
+      throw new Error(
+        "PlantageWork must be associated with a valid PlantingPlanItem",
+      );
+    const cultivation = await mongoose
+      .model("Cultivation")
+      .findById(this.cultivation);
+
+    if (cultivation) {
+      cultivation.plantageWorks.push(this._id);
+      await cultivation.save();
+    } else {
+      throw new Error(
+        "PlantageWork must be associated with a valid Cultivation",
+      );
+    }
+
+    const cultivationWorker = await mongoose
+      .model("CultivationWorker")
+      .findById(this.worker);
+    if (cultivationWorker) {
+      cultivationWorker.plantageWorks.push(this._id);
+      await cultivationWorker.save();
+    } else {
+      throw new Error(
+        "PlantageWork must be associated with a valid CultivationWorker",
+      );
+    }
+  }
+});
+
+plantageWorkSchema.pre("deleteMany", async function () {
+  const ids = await PlantageWork.find(this.getFilter()).distinct("_id");
+  await mongoose
+    .model("PlantingPlanItem")
+    .updateMany(
+      { plantageWorks: { $in: ids } },
+      { $pull: { plantageWorks: { $in: ids } } },
+    );
+});
+
+const harvestWorkSchema = new Schema(
+  {
+    cultivation: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Cultivation",
+    },
+    harvestingPlanItem: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "HarvestingPlanItem",
+    },
+    relativeCoords: {
+      type: String,
+      default: null,
+      validate: (coords) => {
+        if (coords && !utils.strings.testCoordinates(coords)) {
+          throw new Error(
+            "Invalid cell coordinates format. Expected format: 'x,y'",
+          );
+        }
+      },
+    },
+  },
+  { timestamps: true },
+);
+
 export const CultivationWorker =
   mongoose.models.CultivationWorker ||
   Worker.discriminator("CultivationWorker", cultivationWorkerSchema);

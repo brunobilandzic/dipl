05c2aee789d24fc9dd41b3e1ab9873c1df61309b
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-30 19:09:41 +0200

    seed prod worker

--- fvtracker/seed/users/workers.js
+++ fvtracker/seed/users/workers.js
@@ -2,6 +2,8 @@ import { AppUser } from "@/models/user/AppUser";
 import { workersJson } from "../data/appUsers";
 import { CultivationWorker } from "@/models/user/workers/CultivationWork";
 import { CultivationManager } from "@/models/user/managers/CultivationManager";
+import { ProductionWorker } from "@/models/user/workers/ProductionWork";
+import { ProductionManager } from "@/models/user/managers/ProductionManager";
 import dbConnect from "@/lib/db/mongooseConnect";
 import { Worker } from "@/models/user/workers";
 
@@ -23,6 +25,20 @@ export const seedWorkers = async () => {
         await cultivationWorker.save();
         console.log(`Created CultivationWorker for ${appUser.username}`);
         break;
+      case "ProductionWorker":
+        const productionManager = await ProductionManager.findOne();
+        const productionWorker = new ProductionWorker({
+          appUser: appUser._id,
+          manager: productionManager.rootManager,
+          hourlyRate: workerData.hourlyRate,
+        });
+        await productionWorker.save();
+        console.log(`Created ProductionWorker for ${appUser.username}`);
+        break;
+      default:
+        console.warn(
+          `Unknown worker type ${workerData.modelName} for user ${appUser.username}`,
+        );
     }
   }
 };

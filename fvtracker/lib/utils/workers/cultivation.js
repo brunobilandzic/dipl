73bd55c6991e67a4b386b867fa233ca6100fff9d
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-30 17:43:14 +0200

    sum hours no log

--- fvtracker/lib/utils/workers/cultivation.js
+++ fvtracker/lib/utils/workers/cultivation.js
@@ -1,5 +1,4 @@
 export const worksCoordsSum = ({ works, plant = false }) => {
-  console.log("Calculating works coords sum", { works });
   return works.reduce((sum, work) => {
     const coords = plant ? work.plantedCoords : work.harvestedCoords;
     const coordsCount = coords ? coords.length : 0;

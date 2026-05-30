02cd6d85a4fadfc0491020617e8f2f01fcbd201e
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-30 19:18:03 +0200

    products page send worker

--- fvtracker/app/(production)/proizvodi/page.js
+++ fvtracker/app/(production)/proizvodi/page.js
@@ -1,10 +1,29 @@
+import { UnathorizedPage } from "@/components/auth/unAuthorized";
 import ProductList from "@/components/production/products/list";
+import { PRODUCTION_MANAGER } from "@/lib/constants/users/managerTypes";
 import React from "react";
+import { fetchManagerWorker } from "@/lib/utils/auth";
+import { managerMorkerMap } from "@/lib/constants/users/managerWorkerMap";
+import { sanitize } from "@/lib/utils/objects";
 
-const ProductsPage = () => {
+const ProductsPage = async () => {
+  let {
+    specificManager: productionManager,
+    worker: productionWorker,
+    unauthorized,
+  } = await fetchManagerWorker({
+    managerNames: [PRODUCTION_MANAGER],
+    workerType: managerMorkerMap[PRODUCTION_MANAGER],
+  });
+
+  if (unauthorized) {
+    return <UnathorizedPage />;
+  }
   return (
     <div>
-      <ProductList />
+      <ProductList
+        worker={productionWorker ? sanitize(productionWorker) : null}
+      />
     </div>
   );
 };

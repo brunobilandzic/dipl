e38f1d05e2a88797b6265493f712d288604b8da8
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-30 17:43:27 +0200

    fetchManagerWorker

--- fvtracker/app/api/cultivation/harvest/new-harvest/route.js
+++ fvtracker/app/api/cultivation/harvest/new-harvest/route.js
@@ -1,12 +1,26 @@
-import { fetchSessionSpecificManager } from "@/lib/auth/fetchSessionData";
+import { fetchManagerWorker } from "@/lib/auth/fetchSessionData";
 import { CULTIVATION_MANAGER } from "@/lib/constants/users/managerTypes";
+import { managerMorkerMap } from "@/lib/constants/users/managerWorker";
 import { harvestCells } from "@/lib/cultivation/harvest";
 import dbConnect from "@/lib/db/mongooseConnect";
 
 export async function POST(req) {
   try {
     await dbConnect();
-    await fetchSessionSpecificManager({ managerName: CULTIVATION_MANAGER });
+    const {
+      specificManager: cultvationManager,
+      worker,
+      unauthorized,
+    } = await fetchManagerWorker({
+      managerNames: [CULTIVATION_MANAGER],
+      workerType: managerMorkerMap[CULTIVATION_MANAGER],
+    });
+    if (unauthorized) {
+      return Response.json(
+        { success: false, error: "Nemate pravo izrade novog zasada" },
+        { status: 403 },
+      );
+    }
     const body = await req.json();
     const newHarvest = body;
     console.log({ newHarvest });

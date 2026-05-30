49cbb4662dd1c39ba1dc6caa25d68520d9ecc3f7
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-30 20:24:26 +0200

    worker component to chooose wich worker type comp to show

--- fvtracker/components/workers/index.js
+++ fvtracker/components/workers/index.js
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 import { showDate } from "@/lib/utils/display";
 import { workPay } from "@/lib/utils/workers/pay";
 import { worksCoordsSum } from "@/lib/utils/workers/cultivation";
+import { CULTIVATION_MANAGER } from "@/lib/constants/users/managerTypes";
 
 export const WorkersPageComponent = ({ managerModelName }) => {
   const workersState = useSelector((state) => state.workers);
@@ -15,7 +16,7 @@ export const WorkersPageComponent = ({ managerModelName }) => {
 
   if (!workers) return <LoadingFullScreen />;
 
-  console.log("showing workers", { workers });
+  console.log("showing workers", { workers }, { managerModelName });
 
   return (
     <div>
@@ -27,7 +28,10 @@ export const WorkersPageComponent = ({ managerModelName }) => {
         >
           {workers.map((worker) => (
             <WorkerItem key={worker._id} worker={worker}>
-              <CultivationWork worker={worker} />
+              <WorkerContent
+                worker={worker}
+                managerModelName={managerModelName}
+              />
             </WorkerItem>
           ))}
         </List>
@@ -49,30 +53,42 @@ const WorkerItem = ({ worker, children }) => {
           </div>
           <div>{worker.hourlyRate} $/h</div>
         </div>
-        <div>
-          {workPay({
-            hourlyRate: worker.hourlyRate,
-            works: [...worker.plantageWorks, ...worker.harvestWorks],
-          })}{" "}
-          $ ukupno
-        </div>
-        <div>{worker.plantageWorks.length} sadnji</div>
-        <div>{worker.harvestWorks.length} berbi</div>
+
         {children}
       </div>
     </ListItem>
   );
 };
 
-const CultivationWork = ({ worker }) => {
+function WorkerContent({ worker, managerModelName }) {
+  console.log({ managerModelName });
+  switch (managerModelName) {
+    case CULTIVATION_MANAGER:
+      console.log("render for cult manager....");
+      return <CultivationWorker worker={worker} />;
+    default:
+      return null;
+  }
+}
+
+const CultivationWorker = ({ worker }) => {
   return (
     <>
+      <div>
+        {workPay({
+          hourlyRate: worker.hourlyRate,
+          works: [...worker.plantageWorks, ...worker.harvestWorks],
+        })}{" "}
+        $ ukupno
+      </div>
       <div>
         Zasađeno: {worksCoordsSum({ works: worker.plantageWorks, plant: true })}
       </div>
       <div>
         Ubrano: {worksCoordsSum({ works: worker.harvestWorks, plant: false })}
       </div>
+      <div>{worker.plantageWorks.length} sadnji</div>
+      <div>{worker.harvestWorks.length} berbi</div>
     </>
   );
 };

9be972321720676eda21963da53c125449902156
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-05-31 15:47:14 +0200

    ProductionWorker pay

--- fvtracker/lib/utils/workers/pay.js
+++ fvtracker/lib/utils/workers/pay.js
@@ -1,7 +1,15 @@
-export const workPay = ({ hourlyRate, works }) => {
+export const workPayCultivation = ({ hourlyRate, works }) => {
   let totalPay = 0;
   for (const work of works) {
     totalPay += work.hoursWorked * hourlyRate;
+  }
+  return totalPay;
+};
+
+export const workPayProduction = ({ hourlyRate, processes }) => {
+  let totalPay = 0;
+  for (const process of processes) {
+    totalPay += process.quantity * hourlyRate;
   }
   return totalPay;
 };

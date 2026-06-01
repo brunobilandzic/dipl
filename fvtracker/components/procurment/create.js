035d7b202ce838962bd244d238dd6ed0e65773d6
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-01 18:21:49 +0200

    item proc handlers

--- fvtracker/components/procurment/create.js
+++ fvtracker/components/procurment/create.js
@@ -6,10 +6,25 @@ export const CreateProcurment = () => {
     description: "",
     items: [],
   });
+  const emptyItem = { name: "", quantity: 0, price: 0 };
   const handleChange = (e) => {
     const { name, value } = e.target;
     setProcurmentData((prev) => ({ ...prev, [name]: value }));
   };
+  const addItem = () => {
+    setProcurmentData((prev) => ({
+      ...prev,
+      items: [...prev.items, emptyItem],
+    }));
+  };
+  const handleItemChange = (index, field, value) => {
+    setProcurmentData((prev) => {
+      const newItems = [...prev.items];
+      newItems[index][field] = value;
+      return { ...prev, items: newItems };
+    });
+  };
+
   return (
     <div>
       <h1>Create Procurment</h1>

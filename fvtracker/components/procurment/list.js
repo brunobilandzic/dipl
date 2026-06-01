b1471dc4347e9ff5a67ac1d6516d2c59d622404f
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-01 18:07:08 +0200

    redirect list create procurment

--- fvtracker/components/procurment/list.js
+++ fvtracker/components/procurment/list.js
@@ -1,15 +1,17 @@
 "use client";
 
 import { List, ListItem } from "@/components/layout/preview/list";
+import { useRouter } from "next/navigation";
 import { useSelector } from "react-redux";
 
 export const ProcurmentList = () => {
   const procurments = useSelector((state) => state.procurments.filteredItems);
   console.log({ procurments });
+  const router = useRouter();
 
   return (
     <>
-      <List title="Nabavke">
+      <List title="Nabavke" onCreateItem={() => router.push("/nabava/izradi")}>
         {procurments.map((procurment) => (
           <ProcurmentListItem key={procurment._id} procurment={procurment} />
         ))}

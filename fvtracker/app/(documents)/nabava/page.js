cba37df49e6d8a7d07f7f2f8bec0accc61b8ef38
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-01 18:03:54 +0200

    render procurment list

---
+++ fvtracker/app/(documents)/nabava/page.js
@@ -0,0 +1,12 @@
+import { ProcurmentList } from "@/components/procurment/list";
+import React from "react";
+
+function ProcurmentsPage() {
+  return (
+    <div>
+      <ProcurmentList />
+    </div>
+  );
+}
+
+export default ProcurmentsPage;

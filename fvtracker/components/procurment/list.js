960d09432bc076f3db1ea49d5e85c161b942f35c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-01 17:59:17 +0200

    ProcurmentList skeleton

---
+++ fvtracker/components/procurment/list.js
@@ -0,0 +1,27 @@
+import { List, ListItem } from " @/components/layout/preview/list";
+import { ListItem } from "../layout/preview/list";
+
+export const ProcurmentList = () => {
+  const procurments = useSelector((state) => state.procurments.filteredItems);
+  console.log({ procurments });
+
+  return (
+    <>
+      <List title="Nabavke">
+        {procurments.map((procurment) => (
+          <ProcurmentListItem key={procurment._id} procurment={procurment} />
+        ))}
+      </List>
+    </>
+  );
+};
+
+const ProcurmentListItem = ({ procurment }) => {
+  return (
+    <>
+      <ListItem>
+        <p>Nabavka: {procurment._id}</p>
+      </ListItem>
+    </>
+  );
+};

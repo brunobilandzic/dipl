6868ac958dc0d46b681396e3e5ade91b58b61a8f
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-01 17:50:38 +0200

    procurments route only manager auth

--- fvtracker/app/api/(managers)/procurments/route.js
+++ fvtracker/app/api/(managers)/procurments/route.js
@@ -1,13 +1,19 @@
-import { fetchManagerWorker } from "@/lib/auth/fetchSessionData";
+import { fetchManager, fetchManagerWorker } from "@/lib/auth/fetchSessionData";
+import { MANAGER_TYPES } from "@/lib/constants/users/managerTypes";
 import { getProcurments } from "@/lib/procurments/get";
 
 export async function GET(req) {
-  const { specificManager, worker, unauthorized } = await fetchManagerWorker();
-  if (unauthorized) {
+  const { specificManager, gerneralManager, unauthorized } = await fetchManager(
+    { managerNames: MANAGER_TYPES },
+  );
+  if (unauthorized || (!specificManager && !gerneralManager)) {
     return Response.json({ message: "Unauthorized" }, { status: 401 });
   }
 
-  const rootManagerId = specificManager ? specificManager._id : worker.manager;
+  const rootManagerId = specificManager
+    ? specificManager.rootManager._id
+    : gerneralManager._id;
+  //if gen or fin man, fetch all, later
   const procurments = await getProcurments(rootManagerId);
-  return Response.json(procurments, { status: 200 });
+  return Response.json({ procurments }, { status: 200 });
 }

948d8e343f7175cbfa6b733db4f02c8dc63bee03
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-01 18:15:37 +0200

    create proc route

--- fvtracker/app/api/(managers)/procurments/route.js
+++ fvtracker/app/api/(managers)/procurments/route.js
@@ -1,5 +1,6 @@
 import { fetchManager, fetchManagerWorker } from "@/lib/auth/fetchSessionData";
 import { MANAGER_TYPES } from "@/lib/constants/users/managerTypes";
+import { createProcurment } from "@/lib/procurments/create";
 import { getProcurments } from "@/lib/procurments/get";
 
 export async function GET(req) {
@@ -16,4 +17,27 @@ export async function GET(req) {
   //if gen or fin man, fetch all, later
   const procurments = await getProcurments(rootManagerId);
   return Response.json({ procurments }, { status: 200 });
+}
+
+export async function POST(req) {
+  const { specificManager, gerneralManager, unauthorized } = await fetchManager(
+    { managerNames: MANAGER_TYPES },
+  );
+  if (unauthorized || (!specificManager && !gerneralManager)) {
+    return Response.json(
+      { message: "Nije dozvoljena izrada nabavke." },
+      { status: 401 },
+    );
+  }
+  const { newProcurmentData } = await req.json();
+  console.log("Received new procurment data:", newProcurmentData);
+  const rootManagerId = specificManager
+    ? specificManager.rootManager._id
+    : gerneralManager._id;
+
+  const procurment = await createProcurment({
+    ...newProcurmentData,
+    manager: rootManagerId,
+  });
+  return Response.json({ procurment }, { status: 201 });
 }

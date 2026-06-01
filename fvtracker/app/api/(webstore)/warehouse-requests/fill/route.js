f11a357b72fb3d530cc6677833e71ffc184bbaea
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-01 11:40:02 +0200

    sendworker id from body to fiill w req

--- fvtracker/app/api/(webstore)/warehouse-requests/fill/route.js
+++ fvtracker/app/api/(webstore)/warehouse-requests/fill/route.js
@@ -1,20 +1,31 @@
-import { fetchManager } from "@/lib/auth/fetchSessionData";
+import { fetchManager, fetchManagerWorker } from "@/lib/auth/fetchSessionData";
 import { WAREHOUSE_MANAGER } from "@/lib/constants/users/managerTypes";
 import { fillWarehouseRequest } from "@/lib/warehouses/warehouseRequests";
+import { managerWorkerMap } from "@/lib/constants/users/managerWorker";
 
 export async function POST(request) {
   try {
-    const { specificManager, generalManager, unauthorized } =
-      await fetchManager({
-        managerNames: [WAREHOUSE_MANAGER],
-      });
-    if (!specificManager || unauthorized) {
-      return Response.json({ message: "Unauthorized" }, { status: 403 });
+    let {
+      specificManager,
+      worker: warehouseWorker,
+      unauthorized,
+    } = await fetchManagerWorker({
+      managerNames: [WAREHOUSE_MANAGER],
+      workerType: managerWorkerMap[WAREHOUSE_MANAGER],
+    });
+
+    if (unauthorized) {
+      return Response.json(
+        { message: "Nemate pravo izrade otpremnica" },
+        { status: 403 },
+      );
     }
-    const { warehouseRequestId, shipmentSources } = await request.json();
+    const { warehouseRequestId, shipmentSources, workerId } =
+      await request.json();
     const result = await fillWarehouseRequest({
       warehouseRequestId,
       shipmentSources,
+      workerId,
     });
 
     console.log(result);

c6d9ef1a60ee1e100385b3b6d17082685d72985f
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-02 10:53:37 +0200

    all procs for fin and gen mans

--- fvtracker/app/api/(managers)/procurments/route.js
+++ fvtracker/app/api/(managers)/procurments/route.js
@@ -1,7 +1,11 @@
 import { fetchManager, fetchManagerWorker } from "@/lib/auth/fetchSessionData";
-import { MANAGER_TYPES } from "@/lib/constants/users/managerTypes";
+import {
+  FINANCIAL_MANAGER,
+  MANAGER_TYPES,
+} from "@/lib/constants/users/managerTypes";
 import { createProcurment } from "@/lib/procurments/create";
-import { getProcurments } from "@/lib/procurments/get";
+import { getAllProcurments, getProcurments } from "@/lib/procurments/get";
+import { GeneralManager } from "@/models/user/managers/GeneralManager";
 
 export async function GET(req) {
   const { specificManager, gerneralManager, unauthorized } = await fetchManager(
@@ -10,12 +14,20 @@ export async function GET(req) {
   if (unauthorized || (!specificManager && !gerneralManager)) {
     return Response.json({ message: "Unauthorized" }, { status: 401 });
   }
+  let procurments;
+  if (
+    specificManager.rootManager?.managerModelName == FINANCIAL_MANAGER ||
+    GeneralManager
+  ) {
+    procurments = await getAllProcurments();
+    return Response.json({ procurments }, { status: 200 });
+  }
 
   const rootManagerId = specificManager
     ? specificManager.rootManager._id
     : gerneralManager._id;
   //if gen or fin man, fetch all, later
-  const procurments = await getProcurments(rootManagerId);
+  procurments = await getProcurments(rootManagerId);
   return Response.json({ procurments }, { status: 200 });
 }
 

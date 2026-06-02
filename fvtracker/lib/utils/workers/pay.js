20283a14ad653fe22738a647c4cc72455fe0671f
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-02 18:11:55 +0200

    payWorker simple api util func

--- fvtracker/lib/utils/workers/pay.js
+++ fvtracker/lib/utils/workers/pay.js
@@ -1,3 +1,7 @@
+import { setLoading } from "@/store/loading";
+import api from "@/lib/api";
+import { handleError } from "@/lib/constants/errors/client/handleError";
+
 export const workPayCultivation = ({ hourlyRate, works }) => {
   let totalPay = 0;
   for (const work of works) {
@@ -12,4 +16,19 @@ export const workPayProduction = ({ hourlyRate, processes }) => {
     totalPay += process.quantity * hourlyRate;
   }
   return totalPay;
+};
+
+export const payWorker = async ({ workerId, amount, dispatch }) => {
+  try {
+    dispatch(setLoading(true));
+    const res = await api.post("/pay", { workerId, amount });
+  } catch (error) {
+    console.error("Error paying worker:", error);
+    handleError({
+      ...error,
+      generalMessage: "Došlo je do greške pri isplati radnika.",
+    });
+  } finally {
+    dispatch(setLoading(false));
+  }
 };

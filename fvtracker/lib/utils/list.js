95fbcc79a5dff46084268b6b7ed8b143228f4c3c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-02 20:23:30 +0200

    workers list type init filters

--- fvtracker/lib/utils/list.js
+++ fvtracker/lib/utils/list.js
@@ -56,6 +56,14 @@ export const initFilters = (listType) => {
           value: "",
         },
       ];
+    case "workers":
+      return [
+        {
+          type: "nameSearch",
+          placeholder: "Pretraži radnike...",
+          value: "",
+        },
+      ];
   }
 };
 

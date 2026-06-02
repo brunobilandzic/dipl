77332a3515ba755015b7b4cd1bc9d0430ef20767
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-02 12:29:20 +0200

    populate created proc

--- fvtracker/lib/procurments/create.js
+++ fvtracker/lib/procurments/create.js
@@ -1,7 +1,9 @@
 import { Procurment } from "@/models/documents/Procurment";
+import populateProcurmenConfig from "./populate";
 
 export const createProcurment = async (procurmentData) => {
   const procurment = new Procurment(procurmentData);
+  await procurment.populate(populateProcurmenConfig);
   await procurment.save();
   return procurment;
 };

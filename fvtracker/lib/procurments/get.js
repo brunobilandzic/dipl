324a0f72c3a5c4811f50f5b575f58516f9899649
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-02 10:46:10 +0200

    get all procs

--- fvtracker/lib/procurments/get.js
+++ fvtracker/lib/procurments/get.js
@@ -1,5 +1,10 @@
 import { Procurment } from "@/models/documents/Procurment";
+import populateConfigProcurments from "./populate";
 
 export const getProcurments = async (rootManagerId) => {
   return await Procurment.find({ manager: rootManagerId });
+};
+
+export const getAllProcurments = async () => {
+  return await Procurment.find().populate(populateConfigProcurments);
 };

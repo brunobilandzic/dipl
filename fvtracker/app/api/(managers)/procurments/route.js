f6709d3fff7d23b41c6d2cdae74ab84cb3ca5d3f
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-03 11:12:53 +0200

    procurments root manager filter

--- fvtracker/app/api/(managers)/procurments/route.js
+++ fvtracker/app/api/(managers)/procurments/route.js
@@ -11,15 +11,15 @@ import { GeneralManager } from "@/models/user/managers/GeneralManager";
 
 export async function GET(req) {
   try {
-    const { specificManager, gerneralManager, unauthorized } =
+    const { specificManager, generalManager, unauthorized } =
       await fetchManager({ managerNames: MANAGER_TYPES });
-    if (unauthorized || (!specificManager && !gerneralManager)) {
+    if (unauthorized || (!specificManager && !generalManager)) {
       return Response.json({ message: "Unauthorized" }, { status: 401 });
     }
     let procurments;
     if (
       specificManager.rootManager?.managerModelName == FINANCIAL_MANAGER ||
-      GeneralManager
+      generalManager
     ) {
       procurments = await getAllProcurments();
       return Response.json({ procurments }, { status: 200 });
@@ -27,7 +27,7 @@ export async function GET(req) {
 
     const rootManagerId = specificManager
       ? specificManager.rootManager._id
-      : gerneralManager._id;
+      : generalManager._id;
     //if gen or fin man, fetch all, later
     procurments = await getProcurments(rootManagerId);
     return Response.json({ procurments }, { status: 200 });

26da8830c42762448622d8c02210338af840485d
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-03 14:07:37 +0200

    EmploymentRequest model

---
+++ fvtracker/models/user/workers/EmploymentRequest.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+import {
+  EMPLOYMENT_STATUS_PENDING,
+  EMPLOYMENT_STATUSES,
+} from "@lib/constants/users/workers";
+
+const employmentRequestSchema = new Schema(
+  {
+    worker: { type: Schema.Types.ObjectId, ref: "Worker", required: true },
+    status: {
+      type: String,
+      enum: EMPLOYMENT_STATUSES,
+      default: EMPLOYMENT_STATUS_PENDING,
+    },
+  },
+  { timestamps: true },
+);
+
+export const EmploymentRequest =
+  mongoose.models.EmploymentRequest ||
+  mongoose.model("EmploymentRequest", employmentRequestSchema);

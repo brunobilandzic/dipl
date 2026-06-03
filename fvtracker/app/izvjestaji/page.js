39eecbdba219e52600586baba650ddfbdf6869aa
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-03 18:50:13 +0200

    reports page

---
+++ fvtracker/app/izvjestaji/page.js
@@ -0,0 +1,12 @@
+import { ReportsDashboard } from "@/components/reports/dashboard";
+import React from "react";
+
+function ReportsPage() {
+  return (
+    <div>
+      <ReportsDashboard />
+    </div>
+  );
+}
+
+export default ReportsPage;

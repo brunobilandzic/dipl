567d348c7f751452ee4b9334392370f21e4b47a1
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-04 17:49:33 +0200

    getshipmentSourcesCount for ship items

--- fvtracker/lib/utils/webstore/shipments.js
+++ fvtracker/lib/utils/webstore/shipments.js
@@ -8,3 +8,14 @@ export const getshipmentSourcesCountProducts = (products) =>
       ),
     0,
   );
+
+export const getshipmentSourcesCount = (shipmentItems) =>
+  shipmentItems?.reduce(
+    (total, item) =>
+      total +
+      item.sources.reduce(
+        (sourceTotal, source) => sourceTotal + source.quantity,
+        0,
+      ),
+    0,
+  );

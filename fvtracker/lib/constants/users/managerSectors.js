96de46f6cf504e560557e7590edb710db5e017b4
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-04 21:09:15 +0200

    manager model name sdector croatian map

---
+++ fvtracker/lib/constants/users/managerSectors.js
@@ -0,0 +1,13 @@
+import {
+  CULTIVATION_MANAGER,
+  FINANCIAL_MANAGER,
+  PRODUCTION_MANAGER,
+  WAREHOUSE_MANAGER,
+} from "./managerTypes";
+
+export default {
+  [CULTIVATION_MANAGER]: "Uzgoj i berba",
+  [PRODUCTION_MANAGER]: "Proizvodnja",
+  [WAREHOUSE_MANAGER]: "Skladište",
+  [FINANCIAL_MANAGER]: "Financije",
+};

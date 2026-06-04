44cba1167111eb3df4664d242d4c3bc371068a31
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-04 12:21:47 +0200

    render all reports for fin manager toooo

--- fvtracker/app/izvjestaji/page.js
+++ fvtracker/app/izvjestaji/page.js
@@ -2,7 +2,10 @@ import { UnathorizedPage } from "@/components/auth/unAuthorized";
 import { AllReports } from "@/components/reports";
 import { ReportsDashboard } from "@/components/reports/dashboard";
 import { fetchManagerWorker } from "@/lib/auth/fetchSessionData";
-import { MANAGER_TYPES } from "@/lib/constants/users/managerTypes";
+import {
+  FINANCIAL_MANAGER,
+  MANAGER_TYPES,
+} from "@/lib/constants/users/managerTypes";
 import React from "react";
 
 async function ReportsPage() {
@@ -15,7 +18,15 @@ async function ReportsPage() {
     unauthorized,
   });
   if (unauthorized) return <UnathorizedPage />;
-  if (generalManager) return <AllReports />;
+  if (
+    generalManager ||
+    specificManager?.rootManager?.managerModelName === FINANCIAL_MANAGER
+  ) {
+    console.log(
+      "Rendering AllReports for financial manager or general manager",
+    );
+    return <AllReports />;
+  }
 }
 
 export default ReportsPage;

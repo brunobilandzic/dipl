f218c47f38b9b56f90155e174e64b44f888848d5
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-04 14:28:45 +0200

    ProductionManagerReports stocks and facilities

--- fvtracker/components/reports/production/manager..js
+++ fvtracker/components/reports/production/manager..js
@@ -1,5 +1,43 @@
-import { ReportSector } from "../dashboard";
+import { useSelector } from "react-redux";
+import { ReportItem, ReportSection, ReportSector } from "../dashboard";
 
 export const ProductionManagerReports = ({}) => {
-  return <ReportSector title="Proizvodnja"></ReportSector>;
+  const products = useSelector((state) => state.production?.products.items);
+  const productionStockQuantity = products?.reduce(
+    (total, product) =>
+      total +
+      product.productionStocks.reduce(
+        (stockTotal, stock) => stockTotal + stock.quantity,
+        0,
+      ),
+    0,
+  );
+  const warehouseStockQuantity = products?.reduce(
+    (total, product) =>
+      total +
+      product.warehouseStocks.reduce(
+        (stockTotal, stock) => stockTotal + stock.quantity,
+        0,
+      ),
+    0,
+  );
+  const facilities = useSelector((state) => state.production?.facilities.items);
+  console.log({ facilities });
+  return (
+    <ReportSector title="Proizvodnja">
+      <ReportSection title="Proizvodi">
+        <ReportItem count={products?.length || 0} description="Proizvoda" />
+        <ReportItem
+          count={productionStockQuantity + warehouseStockQuantity}
+          description="Proizvedeno"
+        />
+
+        <ReportItem count={warehouseStockQuantity} description="Skladišta" />
+      </ReportSection>
+      <ReportSection title="Postrojenja">
+        <ReportItem count={facilities?.length || 0} description="Postrojenja" />
+        <ReportItem count={productionStockQuantity} description="Proizvoda" />
+      </ReportSection>
+    </ReportSector>
+  );
 };

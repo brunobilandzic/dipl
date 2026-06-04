4ee9cdcc7883124f1523465e5ed2310a4526588b
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-04 10:17:25 +0200

    dashboard and item wrappers

--- fvtracker/components/reports/dashboard.js
+++ fvtracker/components/reports/dashboard.js
@@ -1,9 +1,14 @@
 "use client";
 
-export const ReportsDashboard = () => {
+export const ReportsDashboard = ({ children }) => {
   return (
     <div>
-      <h1>Reports Dashboard</h1>
+      <h1 className="text-3xl font-extrabold">Reports Dashboard</h1>
+      <div className="grid grid-cols-4">{children}</div>
     </div>
   );
+};
+
+export const ReportItem = ({ children }) => {
+  return <div className="bg-white rounded-lg shadow p-4">{children}</div>;
 };

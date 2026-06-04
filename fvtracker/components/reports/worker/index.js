65c2b8002fcbd792b11d62cf3450de55d1e653e6
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-04 21:10:18 +0200

    worker report get sectorName

--- fvtracker/components/reports/worker/index.js
+++ fvtracker/components/reports/worker/index.js
@@ -1,6 +1,8 @@
+import managerSectors from "@/lib/constants/users/managerSectors";
 import { useSelector } from "react-redux";
 
-const WorkersReport = ({ managerModelName }) => {
+export const WorkersReport = ({ managerModelName }) => {
   const workers = useSelector((state) => state.workers.items);
   if (!workers || workers.length === 0) return null;
+  const sectorName = managerSectors[managerModelName] || "Sektor";
 };

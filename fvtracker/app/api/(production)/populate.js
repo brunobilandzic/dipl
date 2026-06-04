fddfeca3eb35ab4aa68ab248b420f49d194a79f4
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-04 14:06:46 +0200

    correct populate products

--- fvtracker/app/api/(production)/populate.js
+++ fvtracker/app/api/(production)/populate.js
@@ -1,33 +1,36 @@
-export const populateProductsConfig = [
-  {
-    path: "ingredients",
-    select: "cropVariety quantity",
-    populate: {
-      path: "cropVariety",
-      select: "name cropType",
+export const populateProductsConfig = {
+  path: "products",
+  populate: [
+    {
+      path: "ingredients",
+      select: "cropVariety quantity",
       populate: {
-        path: "cropType",
-        select: "name",
+        path: "cropVariety",
+        select: "name cropType",
+        populate: {
+          path: "cropType",
+          select: "name",
+        },
       },
     },
-  },
-  {
-    path: "productionStocks",
-    populate: [
-      {
-        path: "facility",
-      },
-    ],
-  },
-  {
-    path: "warehouseStocks",
-    populate: [
-      {
-        path: "warehouse",
-      },
-      {
-        path: "warehouseAcceptanceProcesses",
-      },
-    ],
-  },
-];
+    {
+      path: "productionStocks",
+      populate: [
+        {
+          path: "facility",
+        },
+      ],
+    },
+    {
+      path: "warehouseStocks",
+      populate: [
+        {
+          path: "warehouse",
+        },
+        {
+          path: "warehouseAcceptanceProcesses",
+        },
+      ],
+    },
+  ],
+};

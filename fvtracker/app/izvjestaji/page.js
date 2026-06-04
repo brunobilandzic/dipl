e3d4d2ce3494408710db03a1073ba0e9633f1707
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-04 11:06:50 +0200

    reports page gen man render all reports

--- fvtracker/app/izvjestaji/page.js
+++ fvtracker/app/izvjestaji/page.js
@@ -1,12 +1,21 @@
+import { UnathorizedPage } from "@/components/auth/unAuthorized";
+import { AllReports } from "@/components/reports";
 import { ReportsDashboard } from "@/components/reports/dashboard";
+import { fetchManagerWorker } from "@/lib/auth/fetchSessionData";
+import { MANAGER_TYPES } from "@/lib/constants/users/managerTypes";
 import React from "react";
 
-function ReportsPage() {
-  return (
-    <div>
-      <ReportsDashboard />
-    </div>
-  );
+async function ReportsPage() {
+  const { specificManager, worker, generalManager, unauthorized } =
+    await fetchManagerWorker({ managerNames: MANAGER_TYPES });
+  console.log({
+    specificManager,
+    worker,
+    generalManager,
+    unauthorized,
+  });
+  if (unauthorized) return <UnathorizedPage />;
+  if (generalManager) return <AllReports />;
 }
 
 export default ReportsPage;

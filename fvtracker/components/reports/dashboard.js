24c5290ee675c85c32cf69a9ec0dd671ca803d8d
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-04 10:52:41 +0200

    report dashboard -> sector -> section -> item comp tree

--- fvtracker/components/reports/dashboard.js
+++ fvtracker/components/reports/dashboard.js
@@ -1,17 +1,36 @@
 "use client";
 
-export const ReportsDashboard = ({ children }) => {
+export const ReportsDashboard = ({ children, managerModelName }) => {
   return (
     <div>
       <h1 className="text-3xl font-extrabold">Reports Dashboard</h1>
-      <div className="grid grid-cols-4">{children}</div>
+      <div className="flex gap-8">{children}</div>
+    </div>
+  );
+};
+
+export const ReportSector = ({ title, children }) => {
+  return (
+    <div className="mb-8">
+      <h2 className="text-2xl font-bold mb-4">{title}</h2>
+      <ReportSection title={title}>{children}</ReportSection>
+    </div>
+  );
+};
+
+export const ReportSection = ({ title, children }) => {
+  return (
+    <div className="mb-8">
+      <h2 className="text-2xl font-bold mb-4">{title}</h2>
+      <div className="grid grid-cols-2 gap-4">{children}</div>
     </div>
   );
 };
 
 export const ReportItem = ({ children, count, description, title, stats }) => {
+  console.log("report item");
   return (
-    <div className="bg-white rounded-lg shadow p-4">
+    <div className="bg-yellow-100 rounded-lg shadow p-4">
       <div className="text-xl font-bold">{title}</div>
       <div className="text-xl font-bold">{count}</div>
       <div className="text-gray-500">{description}</div>

6a404a5c2511e89007b2627a109b6d33568ff1f9
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-04 20:42:44 +0200

    remove user provider

--- fvtracker/app/providers.js
+++ fvtracker/app/providers.js
@@ -1,20 +1,15 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from "next-themes";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "@/store";
-import UserProviders from "@/lib/providers/user";
 import CustomProviders, { LoadingProvider } from "@/lib/providers/custom";
 
 export default function Providers({ children }) {
   return (
     <SessionProvider>
       <ReduxProvider store={store}>
-        
-          <UserProviders>
-            <CustomProviders>{children}</CustomProviders>
-          </UserProviders>
+        <CustomProviders>{children}</CustomProviders>
       </ReduxProvider>
     </SessionProvider>
   );

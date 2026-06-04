42e70a8cfacc8f2a999d81adae3f6adfb5502cd0
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-04 12:22:29 +0200

    ReportSector full width, item min heighjts

--- fvtracker/components/reports/dashboard.js
+++ fvtracker/components/reports/dashboard.js
@@ -2,7 +2,7 @@
 
 export const ReportsDashboard = ({ children, managerModelName }) => {
   return (
-    <div>
+    <div className="w-full">
       <h1 className="text-3xl font-extrabold">Izvještaji</h1>
       <div className="flex gap-8">{children}</div>
     </div>
@@ -11,7 +11,7 @@ export const ReportsDashboard = ({ children, managerModelName }) => {
 
 export const ReportSector = ({ title, children }) => {
   return (
-    <div className="mb-8 mt-4">
+    <div className="w-full mb-8 mt-4">
       <h2 className="text-2xl font-bold mb-4">Sektor: {title}</h2>
       {children}
     </div>
@@ -22,14 +22,14 @@ export const ReportSection = ({ title, children }) => {
   return (
     <div className="mb-8">
       <h2 className="text-xl mb-4">{title}</h2>
-      <div className="grid grid-cols-4 gap-4">{children}</div>
+      <div className="grid grid-cols-6 gap-4">{children}</div>
     </div>
   );
 };
 
 export const ReportItem = ({ children, count, description, title, stats }) => {
   return (
-    <div className="bg-gray-100 rounded-lg shadow p-4  hover:bg-gray-200">
+    <div className="bg-gray-100 rounded-lg shadow p-4 min-h-22  hover:bg-gray-200">
       <div className="text-xl font-extrabold">{title}</div>
       <div className="text-xl font-extrabold">{count}</div>
       <div className="text-gray-600">{description}</div>

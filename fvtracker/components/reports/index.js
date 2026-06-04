0309af24819dd60913482bb54368dbc474752918
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-04 16:29:11 +0200

    reder warehouse report

--- fvtracker/components/reports/index.js
+++ fvtracker/components/reports/index.js
@@ -3,12 +3,14 @@
 import { CultivationManagerReports } from "./cultivation/manager";
 import { ReportsDashboard } from "./dashboard";
 import { ProductionManagerReports } from "./production/manager.";
+import { WarehouseReport } from "./warehouse/manager";
 
 export const AllReports = ({}) => {
   return (
     <ReportsDashboard>
       <CultivationManagerReports />
       <ProductionManagerReports />
+      <WarehouseReport />
     </ReportsDashboard>
   );
 };

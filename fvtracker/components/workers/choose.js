122d0c05e5f50879d37a242b3d7a777ac10c89b2
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-07 20:13:22 +0200

    choose only employed workers

--- fvtracker/components/workers/choose.js
+++ fvtracker/components/workers/choose.js
@@ -1,12 +1,16 @@
+import { EMPLOYMENT_STATUS_EMPLOYED } from "@/lib/constants/users/workers";
 import { AppSelect } from "../form/inputs";
 
 export const ChooseWorker = ({ workers, onChoose }) => {
+  const workerOptions = workers.filter(
+    (w) => w.employmentRequest.status === EMPLOYMENT_STATUS_EMPLOYED,
+  );
   return (
     <AppSelect
       label="Radnik"
       name="workerId"
       onChange={onChoose}
-      options={workers.map((worker) => ({
+      options={workerOptions.map((worker) => ({
         label: `${worker.appUser.name} ${worker.appUser.surname}`,
         value: worker._id,
       }))}

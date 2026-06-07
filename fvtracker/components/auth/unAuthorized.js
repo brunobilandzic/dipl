27f37d8f15e9eb1ebed014ebad4ac90a97cf44ce
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-07 19:05:06 +0200

    un auth page return

--- fvtracker/components/auth/unAuthorized.js
+++ fvtracker/components/auth/unAuthorized.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
 export function UnathorizedPage({ message, path = "/autorizacija" }) {
-  <div className="flex flex-col items-center justify-center h-screen gap-4">
-    <h1 className="text-2xl font-bold">Nedovoljno ovlasti</h1>
-    <p>{message || "Nemate pristup ovoj stranici."}</p>
-    <Link href={path} className="text-blue-500 hover:underline">
-      Povratak na autorizaciju
-    </Link>
-  </div>;
+  return (
+    <div className="flex flex-col items-center justify-start h-screen gap-4">
+      <h1 className="text-2xl font-bold">Nedovoljno ovlasti</h1>
+      <p>{message || "Nemate pristup ovoj stranici."}</p>
+      <Link href={path} className=" btn submitButton">
+        Povratak na autorizaciju
+      </Link>
+    </div>
+  );
 }

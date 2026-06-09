bb1b248da43f1a23d2901f370f1a4eb175c4221b
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-09 13:28:17 +0200

    loadig full screen fixed ,inset

--- fvtracker/components/layout/loading.js
+++ fvtracker/components/layout/loading.js
@@ -12,7 +12,7 @@ export function Loading() {
 
 export const LoadingFullScreen = () => {
   return (
-    <div className="h-screen w-full z-50 flex items-center justify-center  text-gray-600">
+    <div className="h-screen w-full z-50 flex items-center justify-center  text-gray-600 bg-[var(--background)] opacity-70 fixed inset-0">
       <ClipLoader size={100} color="currentColor" />
     </div>
   );

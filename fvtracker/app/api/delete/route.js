3ad817874006c7cf855755aff40e59a0d3de10c5
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-09 19:14:55 +0200

    create admin afret deleting db

--- fvtracker/app/api/delete/route.js
+++ fvtracker/app/api/delete/route.js
@@ -2,12 +2,14 @@
 
 import { deleteDB } from "@/lib/db/delete";
 import dbConnect from "@/lib/db/mongooseConnect";
+import { createAdmin } from "@/seed/users/admin";
 
 export async function DELETE(req) {
   try {
     await dbConnect();
     // Logic to delete the database goes here
     const success = await deleteDB();
+    await createAdmin();
     console.log("Database deleted successfully.");
     return Response.json(
       { message: "Database deleted successfully.", success: true },

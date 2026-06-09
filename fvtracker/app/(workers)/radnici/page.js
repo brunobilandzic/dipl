fa8b683466fab37d6f064c19844ec4814ff45892
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-09 16:22:33 +0200

    WorkersPage check for admin

--- fvtracker/app/(workers)/radnici/page.js
+++ fvtracker/app/(workers)/radnici/page.js
@@ -1,13 +1,20 @@
 import { WorkersPageComponent } from "@/components/workers";
-import { fetchSessionManagerModelName } from "@/lib/auth/fetchSessionData";
+import {
+  fetchAdmin,
+  fetchSessionManagerModelName,
+} from "@/lib/auth/fetchSessionData";
 import React from "react";
 
 async function WorkersPage() {
   const { managerModelName } = await fetchSessionManagerModelName();
+  const { admin } = await fetchAdmin();
 
   return (
     <div>
-      <WorkersPageComponent managerModelName={managerModelName} />
+      <WorkersPageComponent
+        isAdmin={!!admin}
+        managerModelName={managerModelName}
+      />
     </div>
   );
 }

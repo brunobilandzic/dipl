d5255533e36f56066aa448bebc947411c13bad26
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-09 13:43:14 +0200

    no ggogle auth

--- fvtracker/auth.js
+++ fvtracker/auth.js
@@ -12,10 +12,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   session: { strategy: "jwt", maxAge: 30 * 24 * 60 * 60 },
   secret: process.env.AUTH_SECRET,
   providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
     Credentials({
       name: "Credentials",
       credentials: {

e8c426e145e7bc6eca347e1ea08abbee6cf24ade
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-10 13:40:34 +0200

    gen man avoid fetch manager

--- fvtracker/app/api/cultivation/fields/general-manager/route.js
+++ fvtracker/app/api/cultivation/fields/general-manager/route.js
@@ -1,12 +1,15 @@
-import { fetchManager } from "@/lib/auth/fetchSessionData";
+import {
+  fetchManager,
+  isAuthorizedGeneralManager,
+} from "@/lib/auth/fetchSessionData";
 import { Field } from "@/models/sectors/cultivation/Field";
 import dbConnect from "@/lib/db/mongooseConnect";
 import fieldsPopulate from "../populate";
 
 export async function GET(req) {
   await dbConnect();
-  const { generalMaager, unauuthorized } = await fetchManager({});
-  if (unauuthorized) {
+  const isGeneralManager = await isAuthorizedGeneralManager();
+  if (!isGeneralManager) {
     return Response.json({ message: "Unauthorized" }, { status: 403 });
   }
   const fields = await Field.find().populate(fieldsPopulate);

eb015622524020aa1a54c5b73abf98e2b4057780
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-10 13:34:58 +0200

    filter employment req infrastructure

--- fvtracker/components/workers/employmentRequests.js
+++ fvtracker/components/workers/employmentRequests.js
@@ -10,7 +10,7 @@ import {
 import { handleStatusChange } from "@/lib/utils/workers/employment";
 import { useRouter } from "next/navigation";
 import { useMemo, useState } from "react";
-import { initFilters } from "@/lib/utils/list";
+import { filterItems, initFilters } from "@/lib/utils/list";
 
 export const EmploymentRequestsPageComponent = () => {
   const employmentRequests = useSelector(
@@ -19,11 +19,26 @@ export const EmploymentRequestsPageComponent = () => {
   const dispatch = useDispatch();
   const router = useRouter();
 
+  const EMPTY_ARRAY = [];
   const initialFilters = useMemo(
     () => initFilters("employmentRequests"),
     [employmentRequests],
   );
   const [filters, setFilters] = useState(initialFilters);
+  const allEmploymentRequests = useMemo(
+    () => employmentRequests ?? EMPTY_ARRAY,
+    [employmentRequests],
+  );
+  const filteredEmploymentRequests = useMemo(
+    () =>
+      filterItems({
+        _items: allEmploymentRequests,
+        filters,
+      }),
+    [allEmploymentRequests, filters],
+  );
+
+  console.log({ employmentRequests, filteredEmploymentRequests });
 
   return (
     <List
@@ -31,20 +46,24 @@ export const EmploymentRequestsPageComponent = () => {
       filters={filters}
       setFilters={setFilters}
     >
-      {employmentRequests.map((request) => (
-        <EmploymentRequestsItem
-          key={request._id}
-          request={request}
-          dispatch={dispatch}
-          router={router}
-        />
-      ))}
+      {filteredEmploymentRequests.map((request) => {
+        console.log({ request });
+        return (
+          <EmploymentRequestsItem
+            key={request._id}
+            request={request}
+            dispatch={dispatch}
+            router={router}
+          />
+        );
+      })}
     </List>
   );
 };
 
 const EmploymentRequestsItem = ({ request, dispatch, router }) => {
   const { worker, status, _id } = request;
+  if (!worker) return null;
 
   const actionOptions = [
     ...(status === EMPLOYMENT_STATUS_PENDING ||
@@ -94,9 +113,9 @@ const EmploymentRequestsItem = ({ request, dispatch, router }) => {
   const getClassName = () => {
     switch (status) {
       case EMPLOYMENT_STATUS_EMPLOYED:
-        return "ring-1 ring-green-300";
+        return "ring-2 ring-green-300";
       case EMPLOYMENT_STATUS_UNEMPLOYED:
-        return "ring-1 ring-red-300";
+        return "ring-2 ring-red-300";
       default:
         return "";
     }
@@ -113,9 +132,7 @@ const EmploymentRequestsItem = ({ request, dispatch, router }) => {
             <span className="font-semibold">{getName(worker.appUser)}</span> -{" "}
             {worker.appUser.username}
           </div>
-          <div>
-            {worker.appUser.username} {status}
-          </div>
+          <div className="text-sm text-gray-500"> {status}</div>
         </div>
       </ListItem>
     </>

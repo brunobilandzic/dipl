b42d1535b287dcd490ba5bdca66484802a6005bc
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-10 15:34:33 +0200

    check exists gen man

--- fvtracker/seed/users/generalManager.js
+++ fvtracker/seed/users/generalManager.js
@@ -8,36 +8,42 @@ import { GENERAL_MANAGER } from "@/lib/constants/users/managerTypes";
 import { ROLE_STATUSES } from "@/lib/constants/users";
 import { GeneralManagerRequest } from "@/models/documents/requests/RoleRequest";
 
-export const createGeneralManager = async ({ approve =true }) => {
+export const createGeneralManager = async ({ approve = true }) => {
   await dbConnect();
+  let generalManager = await GeneralManager.findOne()
+    .select("generalManagerRequest")
+    .populate("generalManagerRequest");
+  if (generalManager) {
+    generalManager.generalManagerRequest.status = ROLE_STATUSES.APPROVED;
+    generalManager.generalManagerRequest.save();
+  } else {
+    const generalManagerData = appUsersJsonArray.find(
+      (user) => user.username === GENERAL_MANAGER_USERNAME,
+    );
 
-  const generalManagerData = appUsersJsonArray.find(
-    (user) => user.username === GENERAL_MANAGER_USERNAME,
-  );
+    const generalManagerAppUser = new AppUser(generalManagerData);
+    await generalManagerAppUser.save();
 
-  const generalManagerAppUser = new AppUser(generalManagerData);
-  await generalManagerAppUser.save();
+    generalManager = new GeneralManager({
+      appUser: generalManagerAppUser._id,
+    });
+    await generalManager.save();
+    const request = await GeneralManagerRequest.findOne();
+    approve ? (request.status = ROLE_STATUSES.APPROVED) : null;
+    await request.save();
 
-  const generalManager = new GeneralManager({
-    appUser: generalManagerAppUser._id,
-  });
-  await generalManager.save();
-  const request = await GeneralManagerRequest.findOne();
-  approve ? (request.status = ROLE_STATUSES.APPROVED) : null;
-  await request.save();
-
-  await generalManager.save();
-
-  const rootManager = new RootManager({
-    appUser: generalManagerAppUser._id,
-    managerModelName: GENERAL_MANAGER,
-    generalManager: generalManager._id,
-  });
-  await rootManager.save();
-  generalManagerAppUser.rootManager = rootManager._id;
-  await generalManagerAppUser.save();
-  generalManager.rootManager = rootManager._id;
-  await generalManager.save();
+    await generalManager.save();
+    const rootManager = new RootManager({
+      appUser: generalManagerAppUser._id,
+      managerModelName: GENERAL_MANAGER,
+      generalManager: generalManager._id,
+    });
+    await rootManager.save();
+    generalManagerAppUser.rootManager = rootManager._id;
+    await generalManagerAppUser.save();
+    generalManager.rootManager = rootManager._id;
+    await generalManager.save();
+  }
 
   return generalManager;
 };

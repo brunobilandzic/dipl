865bcc05e3a22a9b1455cf5ba2f66e25ad87e7d9
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-11 16:14:49 +0200

    ManagerWorkers title workers count

--- fvtracker/components/generalManager/managerWorkers.js
+++ fvtracker/components/generalManager/managerWorkers.js
@@ -1,14 +1,18 @@
 export const ManagerWorkers = ({ workers, paySum }) => {
   return (
     <>
-      <div>Zarađeno ukupno: {paySum || 0} €</div>
       <div>
-        Plaće ukupno:{" "}
-        {workers.reduce((total, worker) => total + worker.hourlyRate, 0)} €/h
-      </div>
-      <div>
-        Isplaćeno:{" "}
-        {workers.reduce((total, worker) => total + worker.payedAmount, 0)} €
+        <div className="italic mt-2">Radnici</div>
+        <div className="">Zaposleno radnika: {workers.length}</div>
+        <div>Zarađeno ukupno: {paySum || 0} €</div>
+        <div>
+          Plaće ukupno:{" "}
+          {workers.reduce((total, worker) => total + worker.hourlyRate, 0)} €/h
+        </div>
+        <div>
+          Isplaćeno:{" "}
+          {workers.reduce((total, worker) => total + worker.payedAmount, 0)} €
+        </div>
       </div>
     </>
   );

83248f30e210c0144cba788685dfa4061d232061
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-11 14:01:15 +0200

    worker translate

--- fvtracker/lib/constants/users/workers.js
+++ fvtracker/lib/constants/users/workers.js
@@ -7,3 +7,14 @@ export const EMPLOYMENT_STATUSES = [
   EMPLOYMENT_STATUS_UNEMPLOYED,
   EMPLOYMENT_STATUS_PENDING,
 ];
+
+export const WORKER_TRANSLATION = {
+  [EMPLOYMENT_STATUS_EMPLOYED]: "Zaposlen",
+  [EMPLOYMENT_STATUS_UNEMPLOYED]: "Nezaposlen",
+  [EMPLOYMENT_STATUS_PENDING]: "Na čekanju",
+
+  WarehouseWorker: "Radnik skladišta",
+  CultivationWorker: "Radnik u uzgoju",
+  ProductionWorker: "Radnik u proizvodnji",
+  FinancialWorker: "Radnik u financijama",
+};

af5c412a5a2fcd6bd2cd86943f217e542758e60e
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-11 20:28:48 +0200

    dont import ermoved action

--- fvtracker/components/production/products/stock/CreateProductionStock.js
+++ fvtracker/components/production/products/stock/CreateProductionStock.js
@@ -13,7 +13,7 @@ import { getAvailableFacilities } from "@/lib/utils/production/facilities";
 import { ChooseWorker } from "@/components/workers/choose";
 import { PRODUCTION_MANAGER } from "@/lib/constants/users/managerTypes";
 import { checkValue } from "@/lib/utils/formValidation";
-import { addProductStock, editProductStocks } from "@/store/production";
+import { editProductStocks } from "@/store/production";
 
 export const CreateProductionStock = ({
   product,

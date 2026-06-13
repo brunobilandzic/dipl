f1071c5ba13bb3edcf097576312923aa4f182c25
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-13 18:26:15 +0200

    fetchManagerWorker check admin

--- fvtracker/lib/auth/fetchSessionData.js
+++ fvtracker/lib/auth/fetchSessionData.js
@@ -203,15 +203,16 @@ export async function fetchWorker({ workerType }) {
 }
 
 export async function fetchManagerWorker({ managerNames = [], workerType }) {
-  const { specificManager, generalManager, unauthorized } = await fetchManager({
-    managerNames,
-    worker: true,
-  });
+  const { specificManager, generalManager, unauthorized, isAdmin } =
+    await fetchManager({
+      managerNames,
+      worker: true,
+    });
   if (unauthorized) {
     return { unauthorized: true };
   }
-  if (!unauthorized && (specificManager || generalManager)) {
-    return { specificManager, generalManager, unauthorized: false };
+  if (!unauthorized && (specificManager || generalManager || isAdmin)) {
+    return { specificManager, generalManager, unauthorized: false, isAdmin };
   }
   const { worker, unauthorized: workerUnauthorized } = await fetchWorker({
     workerType,

19fac022493062fa251e6e5141d8ef671b94885c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-16 16:11:25 +0200

    profile extract session data

--- fvtracker/app/(auth)/profil/page.js
+++ fvtracker/app/(auth)/profil/page.js
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client";
+
+import { useSession } from "next-auth/react";
+import React from "react";
 
 function ProfilePage() {
-  return (
-    <div>ProfilePage</div>
-  )
+  const { data: session } = useSession();
+  console.log(session);
+
+  const { email, employed, workerType, maangerModelName, isAdmin, roleStatus } =
+    session?.user || {};
+  return <div>ProfilePage</div>;
 }
 
-export default ProfilePage
+export default ProfilePage;

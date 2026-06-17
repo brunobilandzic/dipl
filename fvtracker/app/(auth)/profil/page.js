0a1c15a07109d17663dfce8129c4879fb55245c6
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-17 15:12:03 +0200

    rofile check unatuhs

--- fvtracker/app/(auth)/profil/page.js
+++ fvtracker/app/(auth)/profil/page.js
@@ -2,22 +2,33 @@
 
 import { LoadingFullScreen } from "@/components/layout/loading";
 import { useSession } from "next-auth/react";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 function ProfilePage() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+  const router = useRouter();
   console.log(session);
 
   const { email, employed, workerType, maangerModelName, isAdmin, roleStatus } =
     session?.user || {};
 
-  if (session) {
+  if (status === "loading") {
     return (
       <div>
         <LoadingFullScreen />
       </div>
     );
   }
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/uloga-nije-odobrena");
+    }
+  }, [status, router]);
+
+  if (!session) router.push("/uloga-nije-odobrena");
+
   return <div>ProfilePage</div>;
 }
 

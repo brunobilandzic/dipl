e410785a3a99d7394c7227650a2df213309ee258
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-17 16:26:25 +0200

    calc cultivatedVarieties

--- fvtracker/lib/utils/workers/cultivation.js
+++ fvtracker/lib/utils/workers/cultivation.js
@@ -5,3 +5,29 @@ export const worksCoordsSum = ({ works, plant = false }) => {
     return sum + coordsCount;
   }, 0);
 };
+
+export const cultivatedVarieties = ({ works, plant = false }) => {
+  const varietiesMap = new Map();
+  works.forEach((work) => {
+    const plantName = plant
+      ? `${work.plantingPlantItem.cropVariety.cropType} - ${work.plantingPlantItem.cropVariety.name}`
+      : `${work.plantingPlantItem.cropVariety.name} - ${work.plantingPlantItem.cropVariety.name}`;
+    if (!varietiesMap.has(plantName)) {
+      varietiesMap.set(plantName, 0);
+    }
+    const quantityPerCell = plant
+      ? work.plantingPlantItem.cropVariety.quantityPerCell
+      : work.harvestingPlanItem.cropVariety.quantityPerCell;
+    const coordsCount = plant
+      ? work.plantedCoords.length
+      : work.harvestedCoords.length;
+    varietiesMap.set(
+      plantName,
+      varietiesMap.get(plantName) + coordsCount * quantityPerCell,
+    );
+  });
+  return Array.from(varietiesMap.entries()).map(([name, quantity]) => ({
+    name,
+    quantity,
+  }));
+};

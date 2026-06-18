7239af6f7fecd15de7f7901426f1ff2372b3993c
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-18 12:18:28 +0200

    render profile page if auth

--- fvtracker/app/page.js
+++ fvtracker/app/page.js
@@ -1,6 +1,18 @@
-import { UnathorizedHomepage } from "@/components/home/unauthorized";
+"use client";
 
-export default async function Home() {
+import { UnathorizedHomepage } from "@/components/home/unauthorized";
+import { LoadingFullScreen } from "@/components/layout/loading";
+import { useSession } from "next-auth/react";
+import ProfilePage from "./(auth)/profil/page";
+
+export default function Home() {
+  const { data: session, status } = useSession();
+  if (status === "loading") {
+    return <LoadingFullScreen />;
+  }
+
+  if (status == "authenticated") return <ProfilePage />;
+
   return (
     <>
       <div>

d11fb9d3502fbaa0cf4ae61cb84721cf75276815
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-18 10:57:57 +0200

    harvest cells lib adjust to all in add planted cvs

--- fvtracker/lib/cultivation/harvest/index.js
+++ fvtracker/lib/cultivation/harvest/index.js
@@ -1,8 +1,53 @@
 import { PlantedCropVariety } from "@/models/sectors/cultivation/Crops";
 import { getHarvestingPlanById } from "./plan";
 import { HarvestWork } from "@/models/user/workers/CultivationWork";
+import { HarvestingPlan } from "@/models/documents/plans/HarvestingPlan";
+import { populateConfigCropVariety } from "../populate";
 
 export async function harvestCells({
+  cultivationId,
+  cropVarietyId,
+  toHarvestCells,
+  harvestingPlanId,
+  workerId,
+  quality,
+}) {
+  const harvestingPlan = await HarvestingPlan.findById(
+    harvestingPlanId,
+    "items harvestingBatch",
+  ).populate([
+    {
+      path: "items",
+      select: "cropVariety quantity plantedCropVarieties",
+      populate: { path: "cropVariety", populate: populateConfigCropVariety },
+    },
+  ]);
+  const harvestingPlanItem = harvestingPlan.items.find(
+    (item) => item.cropVariety._id === cropVarietyId,
+  );
+  if (!harvestingPlanItem) {
+    throw new Error("Stavka plana nije pronađena.");
+  }
+
+  const plantedCropVarieties = await PlantedCropVariety.find(
+    { relativeCoords: { $in: toHarvestCells } },
+    "relativeCoods _id",
+  );
+
+  await harvestingPlan.harvestingBatch.addPlantedCropVarieties({
+    harvestingPlanItem,
+    plantedCropVarietiesIds: plantedCropVarieties.map((pcv) => pcv._id),
+    cropVarietyId,
+    quality,
+    workerId,
+    cultivationId,
+    harvestedCoords: toHarvestCells,
+  });
+
+  return plantedCropVarieties;
+}
+
+export async function harvestCells_bup({
   cultivationId,
   cropVarietyId,
   toHarvestCells,

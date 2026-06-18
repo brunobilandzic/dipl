f39bc454d81246ac0ccbfb26e4e4da67454c5694
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-18 12:25:33 +0200

    move cult worker info to file

--- fvtracker/components/workers/profile.js
+++ fvtracker/components/workers/profile.js
@@ -3,16 +3,11 @@
 import { useSelector } from "react-redux";
 
 import { LoadingFullScreen } from "../layout/loading";
-import { WORKER_TRANSLATION } from "@/lib/constants/users/workers";
 import {
   shipmentItemsProductSum,
   shipmentItemsShipmentItemsProductQuantity,
 } from "@/lib/utils/workers/warehouse";
 import { workerTotalPay } from "@/lib/utils/workers/pay";
-import {
-  cultivatedVarieties,
-  worksCoordsSum,
-} from "@/lib/utils/workers/cultivation";
 
 export const WorkerProfile = () => {
   const workerType = useSelector((state) => state.user.session?.workerType);
@@ -74,41 +69,6 @@ export const WarehouseWorkerInfo = ({ shipmentItems }) => {
             </li>
           ))}
         </ul>
-      </div>
-    </div>
-  );
-};
-
-export const CultivationWorkerInfo = ({ plantageWorks, harvestWorks }) => {
-  console.log({
-    plantageWorks,
-    harvestWorks,
-  });
-
-  const cultivatedVarietiesPlanted = cultivatedVarieties({
-    works: plantageWorks,
-    plant: true,
-  });
-  const cultivatedVarietiesHarvested = cultivatedVarieties({
-    works: harvestWorks,
-    plant: false,
-  });
-
-  console.log({
-    cultivatedVarietiesPlanted,
-    cultivatedVarietiesHarvested,
-  });
-
-  return (
-    <div>
-      <div>Obavljeno sadnji: {plantageWorks.length}</div>
-      <div>Obavljeno žetvi: {harvestWorks.length}</div>
-      <div>
-        Ukupno posađeno ćelija:{" "}
-        {worksCoordsSum({ works: plantageWorks, plant: true })}
-      </div>
-      <div>
-        Ukupno požnjeveno ćelija: {worksCoordsSum({ works: harvestWorks })}
       </div>
     </div>
   );

1d9e11328ba59a0f0cfb6255f1a3606f618a86f4
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-18 12:36:04 +0200

    worker profile separate file

---
+++ fvtracker/components/auth/profile/worker.js
@@ -0,0 +1,116 @@
+"use client";
+
+import { useSelector } from "react-redux";
+import { LoadingFullScreen } from "../layout/loading";
+import {
+  shipmentItemsProductSum,
+  shipmentItemsShipmentItemsProductQuantity,
+} from "@/lib/utils/workers/warehouse";
+import { workerTotalPay } from "@/lib/utils/workers/pay";
+
+export const WorkerProfile = () => {
+  const workerType = useSelector((state) => state.user.session?.workerType);
+  const worker = useSelector((state) => state.workers.worker);
+
+  if (!worker) return <LoadingFullScreen />;
+  return (
+    <>
+      <WorkerCommonInfo
+        hourlyRate={worker.hourlyRate}
+        payedAmount={worker.payedAmount}
+        totalPay={workerTotalPay(worker).totalPay}
+        totalHours={workerTotalPay(worker).totalHours}
+      />
+      <WorkerSectorInfo workerType={workerType}>
+        {workerType === "WarehouseWorker" && (
+          <WarehouseWorkerInfo shipmentItems={worker.shipmentItems} />
+        )}
+      </WorkerSectorInfo>
+    </>
+  );
+};
+
+const CommonProfilePage = ({
+  email,
+  employed,
+  workerType,
+  managerModelName,
+  isAdmin,
+  roleStatus,
+  name,
+  username,
+}) => {
+  const profileRole = () => {
+    if (isAdmin) {
+      return "Administrator";
+    } else if (workerType) {
+      return (
+        <>
+          <div>{WORKER_TRANSLATION[workerType]}</div>
+          <div>{employed ? "Zaposlen" : "Nezaposlen"}</div>
+        </>
+      );
+    } else if (managerModelName) {
+      return (
+        <>
+          <div>{MANAGER_TRANSLATION[managerModelName]}</div>
+          <div>
+            Zahtjev za ulogu:{" "}
+            {roleStatus === ROLE_STATUSES.APPROVED ? "Odobren" : "Nije odobren"}
+          </div>
+        </>
+      );
+    }
+  };
+  return (
+    <div>
+      <div className="text-2xl font-bold underline">{name}</div>
+      <div>{profileRole()}</div>
+      <div>Korisničko ime: {username}</div>
+      <div>Email: {email}</div>
+    </div>
+  );
+};
+
+export const WorkerCommonInfo = ({
+  hourlyRate,
+  payedAmount,
+  totalPay,
+  totalHours,
+}) => {
+  return (
+    <div>
+      <div className="flex flex-col  my-4">
+        <p>Zarađeno: {totalPay} €</p>
+        <p>Ukupno sati: {totalHours}</p>
+        <p>Satnica: {hourlyRate} €/h</p>
+        <p>Isplaćemo: {payedAmount} €</p>
+      </div>
+    </div>
+  );
+};
+
+export const WorkerSectorInfo = ({ workerType, children }) => {
+  return <div>{children}</div>;
+};
+
+export const WarehouseWorkerInfo = ({ shipmentItems }) => {
+  const shippedPrducts =
+    shipmentItemsShipmentItemsProductQuantity(shipmentItems);
+  return (
+    <div>
+      <div>Učinjeno otpremnica: {shipmentItems.length}</div>
+      <div>Odaslano: {shipmentItemsProductSum(shipmentItems)} proizvoda</div>
+      <div>
+        <h4>Proizvodi:</h4>
+        <ul className="list-disc list-inside">
+          {shippedPrducts.map((product) => (
+            <li key={`${product.name}-${product.quantity}`}>
+              {product.name}: {product.quantity}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};

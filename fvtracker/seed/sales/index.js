ee6cd5f962e8f1edcef961e31ebb847cabab2ea3
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-19 20:47:21 +0200

    checkpoints needed no mor bug is fixed

--- fvtracker/seed/sales/index.js
+++ fvtracker/seed/sales/index.js
@@ -1,4 +1,4 @@
-import { Order } from "@/models/sectors/sales";
+import { Order, Receipt } from "@/models/sectors/sales";
 import { createOrders } from "./orders";
 import { createWarehouseRequests } from "./warehouseRequests";
 import { WarehouseRequest } from "@/models/documents/requests/WarehouseRequest";
@@ -7,6 +7,7 @@ import { Customer } from "@/models/user/Customer";
 export default {
   seedSales: async () => {
     await deleteDB();
+    console.log("seeding orders...");
     const orders = await createOrders();
     console.log("seeding orders completed");
     // create req for only one order
@@ -18,24 +19,6 @@ export default {
 };
 
 const deleteDB = async () => {
-  console.log("[deleteDB] 1 start");
-
-  console.log("[deleteDB] 2 deleting orders");
-  await Order.deleteMany({});
-
-  console.log("[deleteDB] 3 deleting warehouse requests");
-  await WarehouseRequest.deleteMany({});
-
-  console.log("[deleteDB] 4 deleting customers");
-  await Customer.deleteMany({});
-
-  console.log("[deleteDB] 5 deleting receipts");
-  await Receipt.deleteMany({});
-
-  console.log("[deleteDB] 6 done");
-};
-
-const deleteDB_bup = async () => {
   console.log(
     "Clearing existing orders, warehouse requests, customers, and receipts...",
   );

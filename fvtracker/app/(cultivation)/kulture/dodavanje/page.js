814257d4fcecf7b0802772eebb94ffce6990d573
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-21 14:15:38 +0200

    render create crops comp in kreiraj page

---
+++ fvtracker/app/(cultivation)/kulture/dodavanje/page.js
@@ -0,0 +1,12 @@
+import CreateCropType from "@/components/cultivation/crops/create";
+import React from "react";
+
+function CreateCropTypePage() {
+  return (
+    <div>
+      <CreateCropType />
+    </div>
+  );
+}
+
+export default CreateCropTypePage;

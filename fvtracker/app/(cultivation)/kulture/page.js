ff48e06198158cc8c8af6a09ace76d2fc910b48b
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-21 14:08:39 +0200

    crops list page

---
+++ fvtracker/app/(cultivation)/kulture/page.js
@@ -0,0 +1,12 @@
+import CropTypesList from "@/components/cultivation/crops/list";
+import React from "react";
+
+function CropsPage() {
+  return (
+    <div>
+      <CropTypesList />
+    </div>
+  );
+}
+
+export default CropsPage;

c4674b0c0c00d0aaae56f7a5e0d0d222ae026853
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-06-23 10:53:38 +0200

    check existing role request on approve

--- fvtracker/app/api/(managers)/(generalManager)/general-manager/role-requests/route.js
+++ fvtracker/app/api/(managers)/(generalManager)/general-manager/role-requests/route.js
@@ -2,6 +2,7 @@ import {
   fetchAdmin,
   fetchSessionSpecificManager,
 } from "@/lib/auth/fetchSessionData";
+import { ROLE_STATUSES } from "@/lib/constants/users";
 import { GENERAL_MANAGER } from "@/lib/constants/users/managerTypes";
 import dbConnect from "@/lib/db/mongooseConnect";
 import { RoleRequest } from "@/models/documents/requests/RoleRequest";
@@ -15,6 +16,33 @@ export async function PUT(req) {
       await fetchSessionSpecificManager({ managerName: GENERAL_MANAGER });
     }
     const { requestId, response } = await req.json();
+    const roleRequest = await RoleRequest.findById(requestId);
+    if (!roleRequest) {
+      return Response.json(
+        { error: "Zahtjev nije pronađen." },
+        { status: 404 },
+      );
+    }
+    await roleRequest.populate("rootManager", "managerModelName");
+    const roleRequests = await RoleRequest.find().populate(
+      "rootManager",
+      "managerModelName",
+    );
+    const someApprovedRequest = roleRequests.some(
+      (rr) =>
+        rr.rootManager.managerModelName ===
+          roleRequest.rootManager.managerModelName &&
+        rr.status === ROLE_STATUSES.APPROVED &&
+        response === ROLE_STATUSES.APPROVED,
+    );
+    if (someApprovedRequest) {
+      return Response.json(
+        {
+          error: `Menadžer ${roleRequest.rootManager.managerModelName} s odobrenom ulogom već postoji.`,
+        },
+        { status: 400 },
+      );
+    }
     await RoleRequest.updateOne(
       { _id: requestId },
       { status: response, respondedAt: new Date() },

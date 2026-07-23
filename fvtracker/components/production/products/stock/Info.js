93398700a5fdcec2cec76563106fa79774106ca1
Bruno Bilandžić <bruno.bilandzic@outlook.com>
2026-07-23 14:50:49 +0200

    product stock processes n icons

--- fvtracker/components/production/products/stock/Info.js
+++ fvtracker/components/production/products/stock/Info.js
@@ -1,5 +1,7 @@
 import { productsAllProductionStocksSum } from "@/lib/utils/production/stocks";
 import { totalWarehouseStockQuantity } from "@/lib/utils/storage/warehouse";
+import { FaCubes } from "@react-icons/all-files/fa/FaCubes";
+import { FaToolbox, FaWarehouse } from "react-icons/fa6";
 
 export const ProductItemStocksInfo = ({
   productionStocks,
@@ -11,22 +13,39 @@ export const ProductItemStocksInfo = ({
   const totalWarehouseQuantity = totalWarehouseStockQuantity({
     warehouseStocks,
   });
+  const totalProcesses = (productionStocks || []).reduce(
+    (acc, stock) => acc + (stock.productionProcesses?.length || 0),
+    0,
+  );
 
   return (
     <>
       {" "}
       <div className="stockquantity flex items-center gap-4">
-        <StockQuantity label="Pogoni" quantity={totalProductionQuantity} />
-        <StockQuantity label="Skladišta" quantity={totalWarehouseQuantity} />
+        <StockQuantity
+          label="Pogoni"
+          quantity={totalProductionQuantity}
+          Icon={FaCubes}
+        />
+        <StockQuantity
+          label="Skladišta"
+          quantity={totalWarehouseQuantity}
+          Icon={FaWarehouse}
+        />
+        <StockQuantity
+          label="Procesi"
+          quantity={totalProcesses}
+          Icon={FaToolbox}
+        />
       </div>
     </>
   );
 };
 
-const StockQuantity = ({ label, quantity }) => {
+const StockQuantity = ({ label, quantity, Icon }) => {
   return (
-    <div>
-      <span>{label}:</span> {quantity || 0}
+    <div className="flex flex-col gap-2 items-center" title={label}>
+      <div>{quantity || 0}</div> <Icon />
     </div>
   );
 };
